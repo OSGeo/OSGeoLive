7e961c90686e540b6a7fbf78b0c17ea4c1d616a3
Stefan Hansen <stefan.hansen@lisasoft.com>
2009-08-28 02:29:33 +0000

    bigger, better...
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@1794 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_udig.sh
+++ bin/install_udig.sh
@@ -1,28 +1,98 @@
- #!/bin/sh
- cd /usr/lib
- wget http://udig.refractions.net/files/downloads/branches/udig-1.2-M6.linux.gtk.x86.tar.gz
- tar -xzf udig-1.2-M6.linux.gtk.x86.tar
+#!/bin/sh
  
- rm udig-1.2-M6.linux.gtk.x86.tar
+## Setup things... ##
  
- #In udig.sh replace ./udig_internals with
- #DATA_ARG=false
- #
- #for ARG in $@ 
- #do
- #        if [ $ARG = "-data" ]; then DATA_ARG=true; fi
- #done
- #
- #if $DATA_ARG; then 
- #        /usr/lib/udig/udig_internal $@
- #else
- #        /usr/lib/udig/udig_internal -data $HOME/uDigWorkspace $@
- #fi
- mkdir /tmp/udig_downloads/udig-data
- cd /tmp/udig_downloads/udig-data
- wget http://udig.refractions.net/docs/data-v1_1.zip
- unzip data-v1_1.zip
- rm data-v1_1.zip
- mv -rf /tmp/udig_downloads/udig-data /usr/local/share
- rm -rf /tmp/udig_downloads
+# check required tools are installed
+if [ ! -x "`which wget`" ] ; then
+   echo "ERROR: wget is required, please install it and try again" 
+   exit 1
+fi
+# create tmp folders
+mkdir /tmp/udig_downloads
+cd /tmp/udig_downloads
+
+
+## Install Application ##
+
+# get udig
+if [ -f "udig-1.2-M6.linux.gtk.x86.tar.gz" ]
+then
+   echo "udig-1.2-M6.linux.gtk.x86.tar.gz has already been downloaded."
+else
+   wget http://udig.refractions.net/files/downloads/branches/udig-1.2-M6.linux.gtk.x86.tar.gz
+fi
+# unpack it and copy it to /usr/lib
+tar -xzf udig-1.2-M6.linux.gtk.x86.tar.gz -C /usr/lib
+
+
+## Configure Application ##
+
+# Download modified startup script for udig
+if [ -f "udig.sh" ]
+then
+   echo "udig.sh has already been downloaded."
+else
+   wget https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/udig-conf/udig.sh
+fi
+# copy it into the udig folder
+cp udig.sh /usr/lib/udig
+
+# create link to startup script
+ln -s /usr/lib/udig/udig.sh /usr/bin/udig
+
+# Download desktop icon
+if [ -f "uDig.desktop" ]
+then
+   echo "uDig.desktop has already been downloaded."
+else
+   wget https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/udig-conf/uDig.desktop
+fi
+# copy it into the udig folder
+cp uDig.desktop $HOME/Desktop
+
+
+## Sample Data ##
+
+# Download udig's sample data
+if [ -f "data-v1_1.zip" ]
+then
+   echo "data-v1_1.zip has already been downloaded."
+else
+   wget http://udig.refractions.net/docs/data-v1_1.zip
+fi
+#unzip the file into /usr/local/share/udig-data
+mkdir /usr/local/share/udig-data
+unzip data-v1_1.zip -d /usr/local/share/udig-data
+
+
+## Documentation ##
+
+# Download udig's documentation
+if [ -f "udig-1.2-M5.html" ]
+then
+   echo "udig-1.2-M5.html has already been downloaded."
+else
+   wget http://udig.refractions.net/files/downloads/branches/udig-1.2-M5.html
+fi
+
+if [ -f "uDigWalkthrough1.pdf" ]
+then
+   echo "uDigWalkthrough1.pdf has already been downloaded."
+else
+   wget http://udig.refractions.net/docs/uDigWalkthrough1.pdf
+fi
+
+if [ -f "uDigWalkthrough2.pdf" ]
+then
+   echo "uDigWalkthrough2.pdf has already been downloaded."
+else
+   wget http://udig.refractions.net/docs/uDigWalkthrough2.pdf
+fi
+
+#copy into /usr/local/share/udig-docs
+mkdir /usr/local/share/udig-docs
+cp udig-1.2-M5.html /usr/local/share/udig-docs
+cp uDigWalkthrough1.pdf /usr/local/share/udig-docs
+cp uDigWalkthrough1.pdf /usr/local/share/udig-docs
+
  
