6b93b6fedc4b8f2d961b4f616386b83f88d3de5d
Jorge Sanz <jsanz@gvsig.com>
2009-09-04 10:52:28 +0000

    adding gvSIG script and resources to the liveDVD
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@1856 7da46fd8-bd2a-0410-a152-cf9810cf5b10

---
+++ bin/install_gvsig.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+# Copyright (c) 2009 The Open Source Geospatial Foundation.
+# Licensed under the GNU LGPL.
+# 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+
+# About:
+# =====
+# This script will install gvSIG 1.9rc1 (BN1242) using
+# a deb package. It will also download the gvSIG manual
+
+# Running:
+# =======
+# Go to Applications -> Accesories -> gvSIG or
+# just double-click on Desktop gvSIG Icon file
+
+
+# check required tools are installed
+if [ ! -x "`which wget`" ] ; then
+   echo "ERROR: wget is required, please install it and try again" 
+   exit 1
+fi
+
+# create tmp folders
+TMP=/tmp/gvsig_downloads
+
+if [ ! -d $TMP ] ; then
+   mkdir cd $TMP
+fi
+
+cd $TMP
+
+# get deb package
+GVSIG_PACKAGE="gvsig_1.9.0RC1_i386.deb"
+GVSIG_PATH="http://gvsig-desktop.forge.osor.eu/downloads/people/iver"
+
+if [ -f $GVSIG_PACKAGE ]
+then
+   echo "$GVSIG_PACKAGE has already been downloaded."
+else
+   wget $GVSIG_PATH/$GVSIG_PACKAGE
+fi
+
+
+# install dependencies
+apt-get install libstdc++5 libgdal1-1.5.0
+dpkg -i $GVSIG_PACKAGE
+
+# place a gvSIG icon on desktop
+cp /usr/share/applications/gvsig.desktop /home/user/Desktop
+
+
+# download documentation
+GVSIG_DOCS=/usr/local/share/gvsig
+GVSIG_MAN=gvSIG-1_1-man-v1-en.pdf
+
+if [ ! -d $GVSIG_DOCS ] ; then
+   mkdir cd $GVSIG_DOCS
+fi
+
+if [ -f "$GVSIG_MAN" ]
+then
+   echo "$GVSIG_MAN has already been downloaded."
+else
+   wget ftp://downloads.gvsig.org/gva/descargas/manuales/$GVSIG_MAN
+fi
+
+cp $GVSIG_MAN $GVSIG_DOCS
+
+echo "Done!"
+
