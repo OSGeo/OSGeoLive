dc4dfc3157b2cf8936612a50ca684c716775e200
Hamish Bowman <hamish_b yahoo com>
2009-09-07 12:09:10 +0000

    don't use --force-yes. It will break more things than it fixes.
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@1887 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_qgis.sh
+++ bin/install_qgis.sh
@@ -33,7 +33,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160
 apt-get update
 
 #Install packages
-apt-get --assume-yes --force-yes install qgis=1.2.0-1~jaunty1 qgis-common qgis-plugin-grass python-qgis python-qgis-common qgis-plugin-grass-common libgdal1-1.6.0-grass gpsbabel grass-doc
+apt-get --assume-yes install qgis=1.2.0-1~jaunty1 qgis-common qgis-plugin-grass python-qgis python-qgis-common qgis-plugin-grass-common libgdal1-1.6.0-grass gpsbabel grass-doc
 
 #Make sure old qt uim isn't installed
 apt-get remove uim-qt uim-qt3
