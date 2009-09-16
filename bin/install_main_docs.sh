65c24933e0849812dc69ec247c5a02fe7d2cc6d5
vmische <vmische>
2009-09-16 21:27:59 +0000

    Updates docs install script.
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2059 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_main_docs.sh
+++ bin/install_main_docs.sh
@@ -24,29 +24,38 @@
 
 # Requires: nothing
 
-mkdir /usr/share/livedvd-docs/
 
 OSGEO_SVN="https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk"
 DEST="/usr/share/livedvd-docs"
 
+mkdir -p $DEST
+
 #base
-FILES="index.html banner.bmp jquery.js arramagong.css"
+FILES="banner.png arramagong.css"
 
 for ITEM in $FILES ; do
    wget -r "$OSGEO_SVN/doc/$ITEM"  --output-document "$DEST/$ITEM"
 done
+# index page start
+wget -q -O - "$OSGEO_SVN/doc/index_pre.html" \
+    >> "$DEST/index.html"
 
 
 # apps
-APPS="gpsdrive grass mapfish udig"
+APPS="geokettle gpsdrive grass mapfish udig"
 
 for ITEM in $APPS ; do
-   wget -r "$OSGEO_SVN/doc/descriptions/${ITEM}_definition.html" \
-        --output-document "$DEST/${ITEM}_definition.html"
+   wget -q -O - "$OSGEO_SVN/doc/descriptions/${ITEM}_definition.html" \
+       >> "$DEST/index.html"
 
    wget -r "$OSGEO_SVN/doc/descriptions/${ITEM}_description.html" \
         --output-document "$DEST/${ITEM}_description.html"
 done
+
+# index page end
+wget -q -O - "$OSGEO_SVN/doc/index_post.html" \
+    >> "$DEST/index.html"
+
 
 
 # FIXME
@@ -54,7 +63,6 @@ echo "install_main_docs.sh FIXME: The Firefox home page shoud be set to file:///
 
 # edit ~user/.mozilla/firefox/$RANDOM.default/prefs.js:
 #   user_pref("browser.startup.homepage", "file:///usr/share/livedvd-docs/index.html");
-
 
 PREFS_FILE=`find ~user/.mozilla/firefox/ | grep -w default/prefs.js | head -n 1`
 if [ -n "$PREFS_FILE" ] ; then
