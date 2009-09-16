e39a353fc2e548ca612f8763bf71966989e7901d
Hamish Bowman <hamish_b yahoo com>
2009-09-16 17:56:24 +0000

    use loops and control variables; add exisiting docs; attempt to set homepage
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2043 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_main_docs.sh
+++ bin/install_main_docs.sh
@@ -25,11 +25,40 @@
 # Requires: nothing
 
 mkdir /usr/share/livedvd-docs/
-wget -r https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc/index.html --output-document /usr/share/livedvd-docs/index.html
-wget -r https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc/banner.bmp --output-document /usr/share/livedvd-docs/banner.bmp
-wget -r https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc/jquery.js --output-document /usr/share/livedvd-docs/jquery.js
-wget -r https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/doc/arramagong.css --output-document /usr/share/livedvd-docs/arramagong.css
+
+OSGEO_SVN="https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk"
+DEST="/usr/share/livedvd-docs"
+
+#base
+FILES="index.html banner.bmp jquery.js arramagong.css"
+
+for ITEM in $FILES ; do
+   wget -r "$OSGEO_SVN/doc/$ITEM"  --output-document "$DEST/$ITEM"
+done
+
+
+# apps
+APPS="gpsdrive grass mapfish udig"
+
+for ITEM in $APPS ; do
+   wget -r "$OSGEO_SVN/doc/descriptions/${ITEM}_definition.html" \
+        --output-document "$DEST/${ITEM}_definition.html"
+
+   wget -r "$OSGEO_SVN/doc/descriptions/${ITEM}_description.html" \
+        --output-document "$DEST/${ITEM}_description.html"
+done
+
 
 # FIXME
 echo "install_main_docs.sh FIXME: The Firefox home page shoud be set to file:///usr/share/livedvd-docs/index.html"
 
+# edit ~user/.mozilla/firefox/$RANDOM.default/prefs.js:
+#   user_pref("browser.startup.homepage", "file:///usr/share/livedvd-docs/index.html");
+
+
+PREFS_FILE=`find ~user/.mozilla/firefox/ | grep -w default/prefs.js | head -n 1`
+if [ -n "$PREFS_FILE" ] ; then
+   mv "$PREFS_FILE" "$PREFS_FILE~"
+   sed -e 's+\(homepage", "\)[^"]*+\1file:///usr/share/livedvd-docs/index.html+' \
+      "$PREFS_FILE~" > "$PREFS_FILE"
+fi
