95a4e37af8b79bdccf55b76d7dc12caa242c9cbb
camerons <cameron.shorter@gmail.com>
2009-09-19 20:28:25 +0000

    update disk usage comments
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2140 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/package.sh
+++ bin/package.sh
@@ -39,31 +39,38 @@ VM="arramagong-gisvm-${VERSION}"
 echo "===================================================================="
 echo "Starting package.sh for version: {$VERSION}"
 echo "===================================================================="
-echo Disk Usage1:, package.sh start, `df | grep "Filesystem" | sed -e "s/  */,/g"`
-echo Disk Usage2:, package.sh start, `df | grep " /$" | sed -e "s/  */,/g"`
+echo Disk Usage1:, package.sh start, `df | grep "Filesystem" | sed -e "s/  */,/g"`, date
+echo Disk Usage2:, package.sh start, `df | grep " /$" | sed -e "s/  */,/g"`, date
+
+echo Disk Usage3:, package.sh start, `df -s ${VM_DIR}/${VM}` , `date`
+
 echo "Start package.sh. Packaging ${VM_DIR}/${VM}"
 date
 
 # Install 7zip
 apt-get install p7zip-full
 
-# Remove non-core VM files, except *.vmx and *.vmdk
+echo "Remove non-core VM files, except *.vmx and *.vmdk"
 cd "${VM_DIR}/${VM}"
 
 for FILE  in `ls | grep -v "\.vmdk$" | grep -v "\.vmx$"` ; do
   rm -fr $FILE
 done
 
+echo Disk Usage3:, after removing non-core VM files, `du -hs ${VM_DIR}/${VM}` , `date`
 
 # Shrink
 echo "Shrink the image"
 vmware-vdiskmanager -k *.vmdk
 
+echo Disk Usage3:, after shrinking the image, `du -hs ${VM_DIR}/${VM}` , `date`
+
 # Compress
 echo "Compress the image using 7z"
 cd "${VM_DIR}"
-pwd
 7z a -mx=9 "${PACKAGE_NAME}-${VERSION}.7z" "${PACKAGE_NAME}-${VERSION}" 
+
+echo Disk Usage3:, after 7zipping the image, `du -hs ${PACKAGE_NAME}-${VERSION}.7z` , `date`
 
 # if the image is greater than 2 Gig, we need to split the image, as the OSGeo
 # server isn't configured to accept files of a greater size.
@@ -72,11 +79,31 @@ pwd
 echo "Split the image"
 split -b 1500M  ${PACKAGE_NAME}-${VERSION}.7z  ${PACKAGE_NAME}-${VERSION}.7z
 
-# Checksums
-echo "Create checksums"
-md5sum ${PACKAGE_NAME}-${VERSION}*.7z*
+mv  ${PACKAGE_NAME}-${VERSION}.7z.aa  ${PACKAGE_NAME}-${VERSION}.7z.1
+mv  ${PACKAGE_NAME}-${VERSION}.7z.ab  ${PACKAGE_NAME}-${VERSION}.7z.2
+mv  ${PACKAGE_NAME}-${VERSION}.7z.ac  ${PACKAGE_NAME}-${VERSION}.7z.3
+mv  ${PACKAGE_NAME}-${VERSION}.7z.ad  ${PACKAGE_NAME}-${VERSION}.7z.4
+mv  ${PACKAGE_NAME}-${VERSION}.7z.ae  ${PACKAGE_NAME}-${VERSION}.7z.5
+mv  ${PACKAGE_NAME}-${VERSION}.7z.af  ${PACKAGE_NAME}-${VERSION}.7z.6
+
+echo "Add the following lines to"
+echo "  https://camerons@svn.osgeo.org/osgeo/livedvd/gisvm/trunk/download/index.html"
+echo 
+
+echo '<h3>Arramagong GISVM and LiveDVD - ${VERSION}'
+echo '<ul>'
+for FILE in ${PACKAGE_NAME}-${VERSION}.7z* ; do
+  MD5SUM=`md5sum ${FILE}`
+  SIZE=`du -h ${FILE} | cut -f1`
+  echo '<li>'
+  echo '  <a href="${FILE}">$FILE</a>'
+  echo '  (${SIZE})'
+  echo '  mdsum: $MD5SUM '
+  echo '</li>'
+done
+echo '</ul>'
+echo
 
 echo Disk Usage1:, package.sh end, `df | grep "Filesystem" | sed -e "s/  */,/g"`
-echo Disk Usage2:, package.sh end, `df | grep " /$" | sed -e "s/  */,/g"`
+echo Disk Usage2:, package.sh end, `df | grep " /$" | sed -e "s/  */,/g"`, date
 echo Finished package.sh
-date
