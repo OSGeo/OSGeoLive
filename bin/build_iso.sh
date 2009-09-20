c3d5de752d045f116a45ec393e10e2a79b1285b5
camerons <cameron.shorter@gmail.com>
2009-09-20 20:28:16 +0000

    incorporate iso2.sh into build_iso.sh and some updates
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2190 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/build_iso.sh
+++ bin/build_iso.sh
@@ -2,7 +2,8 @@
 #################################################
 # 
 # Purpose: Creating an ISO from an existing system
-# Author:  Stefan Hansen <shansen@lisasoft.com>
+# Author:  Stefan Hansen
+# Author:  Cameron Shorter
 #
 #################################################
 # Copyright (c) 2009 Open Geospatial Foundation
@@ -21,9 +22,11 @@
 # web page "http://www.fsf.org/licenses/lgpl.html".
 ##################################################
 
-# This script is expected to be run as root. See README is this directory
+# This script is expected to be run as root inside the arramagong LiveDVD
+# after it has been installed. See README is this directory
 # for instuctions.
-#   sudo iso1.sh
+#
+#   sudo build_iso.sh
 
 
 cd $HOME
@@ -62,15 +65,67 @@ echo cp -rp --parents * ${WORK}/rootfs/etc/skel
 cp -rp --parents * ${WORK}/rootfs/etc/skel
 #done
 
-# cp the script we nee to run in chroot 
+# cp the script we need to run in chroot 
 wget https://svn.osgeo.org/osgeo/livedvd/scripts/trunk/LiveDVDFromScratch/inchroot.sh
 mv inchroot.sh $WORK/rootfs/tmp/
+chmod a+x $WORK/rootfs/tmp/inchroot.sh
 
 # mount proc and dev into work
 mount -o bind /dev/ ${WORK}/rootfs/dev
 mount -t proc proc ${WORK}/rootfs/proc
 
-#get into chroot
-chroot ${WORK}/rootfs /bin/bash
+#get into chroot and execute the inchroot.sh
+chroot ${WORK}/rootfs /tmp/inchroot.sh
 
-# continue with running inchroot.sh and then exit chroot...
+#########################################################
+# The following is derived from Stefan's iso2.sh script
+#########################################################
+
+cd $HOME
+#export WORK=/tmp/work
+#export CD=/tmp/cd
+#export FORMAT=squashfs
+#export FS_DIR=casper
+export IMAGE_VERSION=Alpha5
+
+#Copy the kernel, the updated initrd
+sudo cp -vp ${WORK}/rootfs/boot/vmlinuz-$(uname -r) ${CD}/casper/vmlinuz
+sudo cp -vp ${WORK}/rootfs/boot/initrd.img-$(uname -r) ${CD}/casper/initrd.gz
+
+# Only used if ubiquity is installed
+#sudo chroot ${WORK}/rootfs dpkg-query -W --showformat='${Package} ${Version}\n' | sudo tee ${CD}/${FS_DIR}/filesystem.manifest
+
+#sudo cp -v ${CD}/${FS_DIR}/filesystem.manifest ${CD}/${FS_DIR}/filesystem.manifest-desktop
+
+#REMOVE='ubiquity casper user-setup discover1 xresprobe os-prober libdebian-installer4'
+
+#for i in $REMOVE 
+#do
+#	sudo sed -i "/${i}/d" ${CD}/${FS_DIR}/filesystem.manifest-desktop
+#done
+
+#unmount things we mounted before
+sudo umount ${WORK}/rootfs/proc
+sudo umount ${WORK}/rootfs/sys
+sudo umount ${WORK}/rootfs/dev
+
+#create the squashed fs from the directory tree in $CD
+sudo mksquashfs ${WORK}/rootfs ${CD}/${FS_DIR}/filesystem.${FORMAT} -noappend -no-duplicates
+
+sudo mkdir ${CD}/isolinux
+sudo cp /usr/lib/syslinux/isolinux.bin ${CD}/isolinux/
+
+sudo mkdir ${CD}/install
+sudo mkdir ${CD}/install/memtest
+sudo cp /boot/memtest86+.bin ${CD}/install/memtest
+sudo cp /boot/sbm.img ${CD}/install/
+
+sudo rm -rf ${CD}/boot
+
+#Copy your win installer into $CD/win!!!!! and isolinux-config into $CD/isolinux. An example can be found in the addcd-folder of the svn 
+
+#make the iso
+cd $CD && find . -type f -print0 | xargs -0 sudo md5sum | sudo tee ${CD}/md5sum.txt
+
+sudo mkisofs -r -V "Arramagong_v$IMAGE_VERSION" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o /tmp/Arramagong_v$IMAGE_VERSION.iso .
+
