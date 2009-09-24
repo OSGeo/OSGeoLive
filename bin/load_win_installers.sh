a42e0cefa9ac026afd8d793d43dccd0e10b65cde
Hamish Bowman <hamish_b yahoo com>
2009-09-24 14:37:24 +0000

    add two small ones
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2307 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/load_win_installers.sh
+++ bin/load_win_installers.sh
@@ -43,6 +43,8 @@ for URL in \
   http://maptools.org/dl/ms4w/ms4w_3.0_beta7.zip \
   http://download.osgeo.org/mapguide/releases/2.0.2/MgServerSetup-2.0.2.3011.exe \
   http://maptiler.googlecode.com/files/maptiler-1.0-beta1-setup.exe \
+  http://www.gpsbabel.org/plan9.php?dl=gpsbabel-1.3.6.zip \
+  http://gpsvp.googlecode.com/files/gpsVPxp_0.4.18.zip \
 ; do
   wget -c --progress=dot:mega ${URL}
 done;
@@ -57,9 +59,5 @@ done;
   # WinGRASS
   # MapGuide Open Source
   # MapTiler
-
-  ### Possibles:
   # GpsBabel + GUI (1.2mb)
-  #    http://www.gpsbabel.org/plan9.php?dl=gpsbabel-1.3.6.zip \
   # gpsVP: (500kb)  http://code.google.com/p/gpsvp/
-  #    http://gpsvp.googlecode.com/files/gpsVPxp_0.4.18.zip \
