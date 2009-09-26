01e1298aaccf118abe7013132baf770851e0df22
Hamish Bowman <hamish_b yahoo com>
2009-09-26 12:28:40 +0000

    sync comments
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2355 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/load_mac_installers.sh
+++ bin/load_mac_installers.sh
@@ -50,7 +50,7 @@ cd "$TMP"
 #    pgRouting-1.0.3-3(PG8.3).dmg
 #    PostGIS-1.4.0-4(PG8.3).dmg
 #    Qgis-1.3.0-1-Leopard.dmg
-#    Qgis-1.3.0-2-Snow.dmg
+#    Qgis-1.3.0-1-Snow.dmg
 #
 
 BASE_URL="http://www.kyngchaos.com/files/software/unixport"
