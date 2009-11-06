b9ccfc8c1c04127e4689900623edcc3565810553
Hamish Bowman <hamish_b yahoo com>
2009-11-06 20:09:43 +0000

    declutter udig
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2717 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_udig.sh
+++ bin/install_udig.sh
@@ -83,7 +83,7 @@ if [ -f "udig.sh" ]
 then
    echo "udig.sh has already been downloaded."
 else
-   wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/udig-conf/udig.sh
+   wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/app-conf/udig/udig.sh
 fi
 # copy it into the udig folder
 cp udig.sh $UDIG_FOLDER
@@ -96,7 +96,7 @@ if [ -f "uDig.desktop" ]
 then
    echo "uDig.desktop has already been downloaded."
 else
-   wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/udig-conf/uDig.desktop
+   wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/app-conf/udig/uDig.desktop
 fi
 # copy it into the udig folder
 cp uDig.desktop $USER_HOME/Desktop
