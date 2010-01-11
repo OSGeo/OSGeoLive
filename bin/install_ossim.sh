3b80f5d75ebaffc26bfc5db3b75c1f45d14076bb
Hamish Bowman <hamish_b yahoo com>
2010-01-11 16:17:50 +0000

    update karmic repos
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2889 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_ossim.sh
+++ bin/install_ossim.sh
@@ -13,8 +13,8 @@ USER_HOME="/home/$USER_NAME"
 
 #Add repositories
 
-wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/sources.list.d/ubuntugis-karmic.list \
-     --output-document=/etc/apt/sources.list.d/ubuntugis-karmic.list
+wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/sources.list.d/ubuntugis.list \
+     --output-document=/etc/apt/sources.list.d/ubuntugis.list
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160
 
