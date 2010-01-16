d9602dd3fe7d18b2071a648b74c518c92a5c65ba
Klokan Petr Pridal <klokan@klokan.cz>
2010-01-16 16:37:13 +0000

    Windows installer for MapTiler 1.0 beta2 in the download script
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2928 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/load_win_installers.sh
+++ bin/load_win_installers.sh
@@ -44,7 +44,7 @@ for URL in \
   http://mirror.aarnet.edu.au/pub/grass/grass64/binary/mswindows/native/WinGRASS-6.4.0SVN-r39271-1-Setup.exe \
   http://maptools.org/dl/ms4w/ms4w_3.0_beta7.zip \
   http://download.osgeo.org/mapguide/releases/2.0.2/MgServerSetup-2.0.2.3011.exe \
-  http://maptiler.googlecode.com/files/maptiler-1.0-beta1-setup.exe \
+  http://maptiler.googlecode.com/files/maptiler-1.0-beta2-setup.exe \
   http://www.gpsbabel.org/plan9.php?dl=gpsbabel-1.3.6.zip \
   http://gpsvp.googlecode.com/files/gpsVPxp_0.4.18.zip \
   http://www.saig.es/descargas/dloads/kosmo/kosmo_2_0_beta_windows_jre_2009_09_07.zip \
