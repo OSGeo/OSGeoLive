87c991d67a6cfd14799ff20ad52ddafb49309cc4
Hamish Bowman <hamish_b yahoo com>
2010-01-25 15:48:51 +0000

    add sample data, desktop icon, menu entry
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@2951 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_opencpn.sh
+++ bin/install_opencpn.sh
@@ -14,10 +14,10 @@
 
 # About:
 # =====
-# This script will install the OpenCpn GPS navigation software
+# This script will install the OpenCPN GPS navigation software
 #    written by H.Bowman <hamish_b  yahoo com>
-#    OpenCpn homepage: http://www.opencpn.org/
-#    OpenCpn license: GPL
+#    OpenCPN homepage: http://www.opencpn.org/
+#    OpenCPN license: GPLv2
 #
 # Description: OpenCPN is an OpenSource Chart PLotter/Navigator
 #     OpenCPN supports:
@@ -29,22 +29,29 @@
 #       Unlimited Route/Mark creation
 #
 
+# live disc's username is "user"
+USER_NAME="user"
+USER_HOME="/home/$USER_NAME"
+
 
 TMP_DIR=/tmp/opencpn
 
 cd "$TMP_DIR"
 
-wget  -c --progress=dot:mega \
+wget -c --progress=dot:mega \
   "http://downloads.sourceforge.net/project/opencpn/opencpn/1.3.6/opencpn_1.3.6_i386.deb"
 
 
-DEPS="libwxgtk2.8-0"
-#FIXME 
+# dpkg -I <packagename.deb>
 # Depends: libc6 (>= 2.4), libgcc1 (>= 1:4.1.1-21), libgl1-mesa-glx | libgl1,
 #  libglib2.0-0 (>= 2.12.0), libglu1-mesa | libglu1, libgtk2.0-0 (>= 2.12.0),
 #  libstdc++6 (>= 4.1.1-21), libwxbase2.8-0 (>= 2.8.7.1), libwxgtk2.8-0 (>= 2.8.7.1),
 #  zlib1g (>= 1:1.2.3.3.dfsg-1)
- 
+
+DEPS="libgl1-mesa-glx libglu1-mesa \
+      libglib2.0-0 libgtk2.0-0 libstdc++6 \
+      libwxbase2.8-0 libwxgtk2.8-0 zlib1g"
+
 apt-get --assume-yes install $DEPS
 
 
@@ -60,20 +67,162 @@ fi
 # RNC raster (BSB format)
 # ENC vector (S-57 format)
 #
-# TODO
+# (Sorry Australians, complain to your hydrographic office that encrypting
+#  safety-critical data for the purposes of "DRM" is fundamentally stupid.
+#  They should rely on well-established cryptographically signed methods
+#  to ensure that the data has not been corrupted or tampered with instead.
+#  So we use USA's NOAA data for our examples instead of Sydney Harbour.)
+
+# send users to these websites as part of the min-tutorial:
+# http://www.nauticalcharts.noaa.gov/mcd/Raster/download_agreement.htm
+# http://www.nauticalcharts.noaa.gov/mcd/enc/download_agreement.htm
+# find the chart numbers you want then,
+#   http://www.charts.noaa.gov/RNCs/
+
+DATADIR="/usr/local/share/opencpn"
+mkdir -p "$DATADIR/charts/BSB_ROOT"
+mkdir -p "$DATADIR/charts/ENC_ROOT"
 
 
-#### pre-set data paths
-# TODO
+## "Copying of the NOAA RNCs® to any other server or location for further
+##  distribution is discouraged unless the following guidelines are followed:
+##  1) this User Agreement is displayed and accepted by anyone accessing the
+##  NOAA RNCs®, and 2) a reference to this Web site is included so that anyone
+##  accessing the NOAA RNCs® is advised of their origin."
+
+# [ temp enable for testing:
+#   move to user-init'd run-time script "opencpn_fetch_charts.sh" ]
+ACCEPTED_TERMS=true
+
+if [ "$ACCEPTED_TERMS" = "true" ] ; then
+
+   ### Raster BSB charts ###
+   # New York Harbor:
+   CHARTS="
+    12300
+    12326
+    12327
+    12334
+    12335
+    12358
+    12401
+    12402
+    13006"
+
+    for CHART in $CHARTS ; do
+      wget -c --progress=dot:mega "http://www.charts.noaa.gov/RNCs/$CHART.zip"
+      wget -c -nv "http://www.charts.noaa.gov/RNCs/${CHART}_RNCProdCat.xml"
+    done
+
+    cd "$DATADIR/charts"
+
+    for CHART in $CHARTS ; do
+       unzip "$TMP_DIR/$CHART.zip"
+       cp "$TMP_DIR/${CHART}_RNCProdCat.xml" BSB_ROOT/
+    done
+
+
+   cd "$TMP_DIR"
+
+   ### Vector S-57 charts ###
+   # New York Harbor and Long Island Sound:
+   CHARTS="
+    US2EC03M
+    US3NY01M
+    US4NY13M
+    US4NY1AM
+    US4NY1GM
+    US5CN10M
+    US5CN11M
+    US5CN12M
+    US5NY12M
+    US5NY14M
+    US5NY15M
+    US5NY16M
+    US5NY18M
+    US5NY19M
+    US5NY1BM
+    US5NY1CM
+    US5NY1DM
+    US5NY50M"
+
+    for CHART in $CHARTS ; do
+      wget -c --progress=dot:mega "http://www.charts.noaa.gov/ENCs/$CHART.zip"
+      wget -c -nv "http://www.charts.noaa.gov/ENCs/${CHART}_ENCProdCat.xml"
+    done
+
+    cd "$DATADIR/charts"
+
+    for CHART in $CHARTS ; do
+       unzip -u -o "$TMP_DIR/$CHART.zip"
+       cp "$TMP_DIR/${CHART}_ENCProdCat.xml" ENC_ROOT/
+    done
+
+    cd "$TMP_DIR"
+fi
+
+
+#### pre-set config file with data paths and initial position
+# GPX tracks, etc:
+mkdir "$DATADIR/gpx"
+# GRIB weather data downloads:
+mkdir "$DATADIR/grib"
+# seed config file:
+mkdir "$USER_HOME/.opencpn"
+
+cat << EOF > "$USER_HOME/.opencpn/opencpn.conf"
+[Directories]
+InitChartDir=$DATADIR/charts
+GPXIODir=$DATADIR/gpx
+GRIBDirectory=$DATADIR/grib
+[ChartDirectories]
+ChartDir1=$DATADIR/charts/BSB_ROOT
+ChartDir2=$DATADIR/charts/ENC_ROOT
+[Settings/GlobalState]
+VPLatLon="   40.58,  -71.02"
+VPScale=0.00135726
+OwnShipLatLon="   40.58,   -71.02"
+nBoundaryStyle=79
+EOF
+#S57DataLocation=/usr/src/cvs/opencpn/data/s57data
+
 
 #### install icon on desktop/menus
-# TODO
+wget -nv -c "http://opencpn.cvs.sourceforge.net/viewvc/*checkout*/opencpn/opencpn/data/opencpn.png"
+cp opencpn.png /usr/share/icons/
+
+cat << EOF > /usr/share/applications/opencpn.desktop
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=OpenCPN
+Comment=GPS Navigation. You need to setup Gpsd manually
+Categories=Application;Education;Geography;
+Exec=/usr/local/bin/opencpn
+Icon=exec
+Terminal=false
+EOF
+
+cp /usr/share/applications/opencpn.desktop "$USER_HOME/Desktop/"
+chown -R $USER_NAME.$USER_NAME "$USER_HOME/Desktop/opencpn.desktop"
+
+# add menu item
+if [ ! -e /usr/share/menu/opencpn ] ; then
+   cat << EOF > /usr/share/menu/opencpn
+?package(opencpn):needs="x11"\
+  section="Applications/Science/Geoscience"\
+  longtitle="OpenCPN Ocean Navigator"\
+  title="OpenCPN" command="opencpn"\
+  icon="/usr/share/icons/opencpn.png"
+EOF
+
+   update-menus
+fi
 
 
 #### install help manual
 # PDF version of:
 # http://opencpn.org/docwiki
 #  ?
-#
-# TODO
+# just point to /usr/local/share/opencpn/doc/help.html
 
