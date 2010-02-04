a01a98f3b8dddaa40300d99147e847eef5e237fb
Jorge Sanz <jsanz@gvsig.com>
2010-02-04 16:03:14 +0000

    FIX: polishing the gvSIG install script
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3057 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_gvsig.sh
+++ bin/install_gvsig.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# Copyright (c) 2009 The Open Source Geospatial Foundation.
+#!/bin/bash
+# Copyright (c) 2009-2010 The Open Source Geospatial Foundation.
 # Licensed under the GNU LGPL.
 # 
 # This library is free software; you can redistribute it and/or modify it
@@ -14,29 +14,41 @@
 
 # About:
 # =====
-# This script will install gvSIG 1.9rc1 (BN1242) using
+# This script will install gvSIG 1.9 (BN1253) using
 # a deb package. It will also download the gvSIG manual
 
 # Running:
 # =======
-# Go to Applications -> Accesories -> gvSIG or
-# just double-click on Desktop gvSIG Icon file
-
+# sudo ./install_gvsig.sh
 
 # Changelog:
 # ===========
 # 2010-01-04: adapting the script to 1.9 stable release (jsanz@osgeo.org)
 #   * Adapted dependencies
-#   * Changed to the "with-jre" version because the 9.10 version
+#   * Changed to the "with-jre" version because the Xubuntu 9.10 version
 #     doesn't have the packages of Java 1.5
 
 
 # echo "ERROR: the gvsig.deb pacakge needs to be updated to work with Ubuntu 9.10. Aborting."
 # exit 1
 
+# live disc's username is "user"
+USER_NAME="user"
+USER_HOME="/home/$USER_NAME"
+GVSIG_PACKAGE="gvsig-withjre_1.9.0-1253_i386.deb"
+GVSIG_PATH="http://gvsig-desktop.forge.osor.eu/downloads/people/iver"
+GVSIG_DOCS="/usr/local/share/gvsig"
+GVSIG_MAN="gvSIG-1_1-man-v1-en.pdf"
 
-# install dependencies
-apt-get --assume-yes install libgdal1-1.5.0
+# load user dirs to have the  $XDG_DESKTOP_DIR variable
+if [ -f "$USER_HOME/.config/user-dirs.dirs" ]
+then
+   source $USER_HOME/.config/user-dirs.dirs
+   USER_DESKTOP=$XDG_DESKTOP_DIR
+   echo "\n\n$USER_DESKTOP\n\n"
+else
+   USER_DESKTOP="USER_HOME/Desktop"
+fi
 
 # check required tools are installed
 if [ ! -x "`which wget`" ] ; then
@@ -44,28 +56,17 @@ if [ ! -x "`which wget`" ] ; then
    exit 1
 fi
 
-
-# live disc's username is "user"
-USER_NAME="user"
-USER_HOME="/home/$USER_NAME"
-
-# load user dirs to have the  $XDG_DESKTOP_DIR variable
-source "$USER_HOME/.config/user-dirs.dirs"
-USER_DESKTOP=$XDG_DESKTOP_DIR
+# install dependencies
+apt-get --assume-yes install libgdal1-1.5.0
 
 # create tmp folders
 TMP="/tmp/build_gvsig"
-
 if [ ! -d $TMP ] ; then
    mkdir "$TMP"
 fi
-
 cd "$TMP"
 
 # get deb package with the jre "gvsig-withjre"
-GVSIG_PACKAGE="gvsig-withjre_1.9.0-1253_i386.deb"
-GVSIG_PATH="http://gvsig-desktop.forge.osor.eu/downloads/people/iver"
-
 if [ -f "$GVSIG_PACKAGE" ]
 then
    echo "$GVSIG_PACKAGE has already been downloaded."
@@ -76,46 +77,42 @@ fi
 # install the deb package
 dpkg -i "$GVSIG_PACKAGE"
 
+
 if [ $? -ne 0 ] ; then
    echo "ERROR: gvsig package failed to install"
    exit 1
 fi
 
-
 # place a gvSIG icon on desktop
 # NOTE: maybe this icon will be placed at other folder
-cp /usr/share/applications/gvsig.desktop $USER_DESKTOP
-
+if [ -d $USER_DESKTOP ] ; then
+   echo "Copying icon to desktop at $USER_DESKTOP"
+   cp /usr/share/applications/gvsig.desktop $USER_DESKTOP
+fi
 
 # download documentation 
 # note: at this time (January 2010) the last updated version of the
 #       gvSIG manual is for the 1.1.2 version of gvSIG
-GVSIG_DOCS=/usr/local/share/gvsig
-GVSIG_MAN=gvSIG-1_1-man-v1-en.pdf
-
-
 if [ -f "$GVSIG_MAN" ]
 then
    echo "$GVSIG_MAN has already been downloaded."
 else
-   wget --progress=dot:mega "http://forge.osor.eu/docman/view.php/89/329/gvSIG-1_1-man-v1-en.pdf"
+   wget -c --progress=dot:mega "http://forge.osor.eu/docman/view.php/89/329/gvSIG-1_1-man-v1-en.pdf"
 fi
-
 
 if [ ! -d "$GVSIG_DOCS" ] ; then
    mkdir -p "$GVSIG_DOCS"
 fi
-
 cp "$GVSIG_MAN" "$GVSIG_DOCS"
 
 
 # download gvSIG sample project
-mkdir "$USER_HOME/gvSIG"
+if [ ! -d "$USER_HOME/gvSIG" ] ; then
+   mkdir -p "$USER_HOME/gvSIG"
+fi
 wget --progress=dot:binary http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/app-data/gvsig/sample-project.gvp \
      --output-document=sample-project.gvp
-
 cp sample-project.gvp "$USER_HOME/gvSIG/"
-
 chown -R $USER_NAME:$USER_NAME "$USER_HOME/gvSIG"
 
 
