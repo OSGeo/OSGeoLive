6c19def5b623f4f4839fd7c50a87330375a9d75a
Alex Mandel <tech@wildintellect.com>
2010-02-06 02:44:12 +0000

    Attempt 2 for language script
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3067 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_language.sh
+++ bin/install_language.sh
@@ -28,28 +28,29 @@
 PACKAGES=""
 
 #TODO: make an alternate list base on OSGeo chapters
+#TODO: figure out how to do inline comment with name of language, possibly store list in a different file
 for CODE in \
-  zh \ #Chinese(Mandarin) 
-  sp \ #Spanish 
-  en \ #English 
-  hi \ #Hindi/Urdu
-  ar \ #Arabic 
-  bn \ #Bengali
-  pt \ #Portuguese 
-  ru \ #Russian
-  ja \ #Japanese
-  de \ #German
-  jv \ #Javanese
-  pa \ #Punjabi pa
+  zh \
+  sp \
+  en \
+  hi \
+  ar \
+  bn \
+  pt \
+  ru \
+  ja \
+  de \
+  jv \
+  pa \
 # no language code for Wu may not be a written language
-  fr \ #French 
-  te \ #Telugu
-  vi \ #Vietnamese
-  mr \ #Marathi
-  ko \ #Korean
-  ta \ #Tamil
-  it \ #Italian
-  tr \ #Turkish
+  fr \
+  te \
+  vi \
+  mr \
+  ko \
+  ta \
+  it \
+  tr \
 ; do
   #Currently simulates in order to test for packages and how much space it will take
 PACKAGES="$PACKAGES language-support-$code" 
