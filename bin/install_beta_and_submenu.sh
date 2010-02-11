5ad9a4de8af0b6fb969dc181dadb6d8489ffc1a3
jldominguez <jldominguez>
2010-02-11 17:10:11 +0000

    improved header
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3108 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_beta_and_submenu.sh
+++ bin/install_beta_and_submenu.sh
@@ -1,23 +1,43 @@
 #!/bin/sh
-
+# Copyright (c) 2009 The Open Source Geospatial Foundation.
+# Licensed under the GNU LGPL.
+# 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+#
+# About:
+# =====
 # This script installs beta packages and creates new submenu.
 # Valid for Xubuntu 9.10
 # 
 # I believe menu descriptions have different paths and structure in 
 # each Linux distribution. After some tests, I don't think the command
 # "?package(name):needs..." and "update-menus" are enough to create
-# a new submenu, so this script
-# will have to be adapted in future versions of the live DVD.
-# 
-# #################################################
+# a new submenu, so this script will have to be adapted in future
+# versions of the live DVD.
 # 
 # INSTRUCTIONS:
 # 
-# Copy your install script to the folder ./beta_software  
+# Copy your beta software install script to the folder ./beta_software  
 # Also create in that folder your .desktop file using
 # the existing one as template. Your script should not create two .desktop files,
 # only the one inside the folder ./beta_software
 # Add a call to your install sh script here (under "for SCRIPT in \")
+#
+# Running:
+# =======
+#
+# sudo ./install_beta_and_submenu.sh
+#
+# --------------- Start: ---------------------
+
 
 echo "===================================================================="
 echo "Starting installation of beta software..."
