3e8ff1e704c268b35ea7520954a217f65d47a319
Hamish Bowman <hamish_b yahoo com>
2010-03-05 05:01:18 +0000

    update qgis ver; point grass download to something stable; add opencpn
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3258 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/load_win_installers.sh
+++ bin/load_win_installers.sh
@@ -39,15 +39,16 @@ for URL in \
   http://downloads.sourceforge.net/project/geoserver/GeoServer/1.7.6/geoserver-1.7.6-ng.exe?use_mirror=transact \
   ftp://downloads.gvsig.org/gva/descargas/ficheros/11/gvSIG-update-1_1_2-windows-i586.exe \
   http://udig.refractions.net/files/downloads/udig-1.1.1.exe \
-  http://download.osgeo.org/qgis/win32/QGIS-1.2.0-0-No-GrassSetup.exe \
+  http://linfiniti.com/downloads/QGIS-1.4.0-1-No-GrassSetup.exe \
   http://home.gdal.org/tmp/vcredist_x86.exe \
-  http://mirror.aarnet.edu.au/pub/grass/grass64/binary/mswindows/native/WinGRASS-6.4.0SVN-r39271-1-Setup.exe \
+  http://download.osgeo.org/livedvd/data/grass/WinGRASS-6.4.SVN-r41287-1-Setup.exe \
   http://maptools.org/dl/ms4w/ms4w_3.0_beta7.zip \
   http://download.osgeo.org/mapguide/releases/2.0.2/MgServerSetup-2.0.2.3011.exe \
   http://maptiler.googlecode.com/files/maptiler-1.0-beta2-setup.exe \
   http://www.gpsbabel.org/plan9.php?dl=gpsbabel-1.3.6.zip \
   http://gpsvp.googlecode.com/files/gpsVPxp_0.4.18.zip \
   http://www.opengis.es/descargas/Kosmo_Desktop_2.0-RC1_windows_jre.zip \
+  http://downloads.sourceforge.net/project/opencpn/opencpn/1.3.6/OpenCPN_136_Setup.exe?use_mirror=transact
 ; do
   wget -c --progress=dot:mega "${URL}"
 done;
@@ -69,3 +70,5 @@ done;
   # GpsBabel + GUI (1.2mb)
   # gpsVP: (500kb)  http://code.google.com/p/gpsvp/
   # Kosmo
+  # OpenCPN (8mb)
+
