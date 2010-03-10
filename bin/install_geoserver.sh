5672001b05f92178a712c33686ae7eb67d41dc62
Brian M Hamlin <screenlight@mac.com>
2010-03-10 00:24:40 +0000

    re-write the delay for geoserver startup.. 40 sec total to be safe
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3342 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_geoserver.sh
+++ bin/install_geoserver.sh
@@ -105,8 +105,8 @@ sed -i s/8080/$GS_PORT/g $GS_HOME/etc/jetty.xml
 ## Add a script that will launch the browser after starting GS
 cat << EOF > $GS_HOME/bin/start_admin.sh
 $GS_HOME/bin/startup.sh &
-(echo "25" ; sleep 2; echo "50"; sleep 2; echo "75"; sleep 2; echo "100";  sleep 2) | zenity --progress --auto-close --text "GeoServer starting"
-sleep 40;firefox "http://localhost:$GS_PORT/geoserver/web/"
+(echo "25" ; sleep 10; echo "50"; sleep 10; echo "75"; sleep 10; echo "100";  sleep 10) | zenity --progress --auto-close --text "GeoServer starting"
+firefox "http://localhost:$GS_PORT/geoserver/web/"
 EOF
 
 ## Add a script that will stop GS and notify the user graphically
