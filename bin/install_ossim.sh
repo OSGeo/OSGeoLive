77e0bd0d70020dec25695561ecdc3a6672f27f80
Massimo Di Stefano <massimodisasha@yahoo.it>
2010-03-15 12:32:54 +0000

    added 3d-tiles to test ossim (elevation comes from srtm data, spain - around barcellona)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3396 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_ossim.sh
+++ bin/install_ossim.sh
@@ -75,6 +75,7 @@ wget -c --progress=dot:mega http://ossim.telascience.org/ossimdata/Documentation
         --output-document=/usr/local/share/ossim/OSSIM_Whitepaper.pdf
 
 mkdir /usr/local/share/ossim/ossim_data
+mkdir -p /usr/share/ossim/elevation/elev
 chmod -R 777 /usr/local/share/ossim/ossim_data
 
 wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/band1.tif  --output-document=/usr/local/share/ossim/ossim_data/band1.tif		
@@ -85,7 +86,12 @@ wget -c --progress=dot:mega http://www.geofemengineering.it/data/kml/Plaza_de_Ca
 wget -c --progress=dot:mega http://www.geofemengineering.it/data/kml/View_towards_Sagrada_Familia.kmz --output-document=/usr/local/share/ossim/ossim_data/View_towards_Sagrada_Familia.kmz
 wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/landsat.prj --output-document=/usr/local/share/ossim/ossim_data/landsat.prj
 wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/session.session --output-document=/usr/local/share/ossim/ossim_data/session.session
-
+wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N40E002.hgt --output-document=/usr/share/ossim/elevation/elev/N40E002.hgt
+wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N40E002.omd --output-document=/usr/share/ossim/elevation/elev/N40E002.omd
+wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N41E002.hgt --output-document=/usr/share/ossim/elevation/elev/N41E002.hgt
+wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N41E002.omd --output-document=/usr/share/ossim/elevation/elev/N41E002.omd 
+wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N42E002.hgt --output-document=/usr/share/ossim/elevation/elev/N42E002.hgt
+wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N42E002.omd --output-document=/usr/share/ossim/elevation/elev/N42E002.omd
 
 
 chmod 644 /usr/local/share/ossim/*.pdf
