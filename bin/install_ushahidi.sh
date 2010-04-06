24f439410727b0649acd68e93928950411b81f72
Henry Addo <henry@ushahidi.com>
2010-04-06 23:51:45 +0000

    Install script for Ushahidi
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3513 7da46fd8-bd2a-0410-a152-cf9810cf5b10

---
+++ bin/install_ushahidi.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+# Copyright (c) 2009 The Open Source Geospatial Foundation.
+# Licensed under the GNU LGPL.
+# 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+
+# About:
+# =====
+# This script will install ushahidi
+
+# Running:
+# =======
+# sudo ./install_ushahidi.sh
+
+# Requires: Apache2, PHP5
+#
+# Uninstall:
+# ============
+# sudo rm -rf /var/www/ushahidi/
+
+# live disc's username is "user"
+USER_NAME="eyedol"
+USER_HOME="/home/$USER_NAME"
+TMP_DIR="/tmp/build_ushahidi"
+mkdir -p "$TMP_DIR"
+
+# Install ushahidi dependencies.
+echo "Installing ushahidi"
+
+apt-get install --yes  php5 php5-mcrypt php5-curl 
+
+if [ ! -x "`which wget`" ] ; then
+    echo "ERROR: wget is required, please install it and try again"
+    exit 1
+fi
+
+if [ ! -e "ushahidi.tgz" ] ; then 
+   wget -c --progress=dot:mega "http://assets.ushahidi.com/downloads/ushahidi.tgz"
+else
+    echo "... Ushahidi already downloaded"
+fi
+
+# uncompress ushahidi
+tar xzf "ushahidi.tgz"
+cp -R ushahidi/ /var/www/
+chmod -R uga+r /var/www/ushahidi
+chown -R www-data:www-data /var/www/ushahidi
+
+#Add Launch icon to desktop
+if [ ! -e /usr/share/applications/ushahidi.desktop ] ; then
+   cat << EOF > /usr/share/applications/ushahidi.desktop
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=Ushahidi
+Comment=Ushahidi
+Categories=Application;Internet;
+Exec=firefox /var/www/ushahidi/readme.html
+Icon=gnome-globe
+Terminal=false
+StartupNotify=false
+Categories=Internet;
+EOF
+fi
+cp /usr/share/applications/ushahidi.desktop "$USER_HOME/Desktop/"
+
+echo -n "Done installing Ushahidi\n"
