8b9fed6c61a1e2dca22978afa6e0b3fd07a58da9
Hamish Bowman <hamish_b yahoo com>
2010-04-07 05:16:16 +0000

    +ushahidi
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3515 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/main.sh
+++ bin/main.sh
@@ -67,6 +67,7 @@ for SCRIPT in \
   ./install_udig.sh \
   ./install_openjump.sh \
   ./install_apache2.sh \
+  ./install_ushahidi.sh \
   ./install_postgis.sh \
   ./install_pgrouting.sh \
   ./install_osm.sh \
