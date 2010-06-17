62ddf79fd0afb6ffbd7ae94f2173cf4c8ba175ab
Hamish Bowman <hamish_b yahoo com>
2010-06-17 15:26:07 +0000

    no need to wget local files or use sudo, quote pathnames
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3751 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_spatialite.sh
+++ bin/install_spatialite.sh
@@ -35,18 +35,17 @@ if [ ! -x "`which wget`" ] ; then
    exit 1
 fi
 
+
 #Add repositories
-
-wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/sources.list.d/ubuntugis.list \
-     --output-document=/etc/apt/sources.list.d/ubuntugis.list
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160
-
+if [ ! -e /etc/apt/sources.list.d/ubuntugis.list ] ; then
+   cp ../sources.list.d/ubuntugis.list /etc/apt/sources.list.d/
+fi
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160
 apt-get update
 
 ### setup temp ###
-mkdir -p $TMP
-cd $TMP
+mkdir -p "$TMP"
+cd "$TMP"
 
 ### Download and unpack spatialite ###
 
@@ -57,30 +56,35 @@ apt-get install --assume-yes spatialite-bin
 
 
 BASEURL=http://www.gaia-gis.it/spatialite-2.4.0
-PACKAGES="librasterlite-linux-x86-1.0.tar.gz rasterlite-tools-linux-x86-1.0.tar.gz spatialite-gis-linux-x86-1.0.0.tar.gz spatialite-gui-linux-x86-1.3.0.tar.gz"
+PACKAGES="
+ librasterlite-linux-x86-1.0.tar.gz
+ rasterlite-tools-linux-x86-1.0.tar.gz
+ spatialite-gis-linux-x86-1.0.0.tar.gz
+ spatialite-gui-linux-x86-1.3.0.tar.gz"
 
 for i in $PACKAGES; do
   fn=$(basename $i)
   dir=${fn%.tar.gz}
-  wget -c --progress=dot:mega $BASEURL/$i
-  tar xzf $i
+  # dir=`basename .tar.gz`  # would do the same thing in 1 step
+  wget -c --progress=dot:mega "$BASEURL/$i"
+  tar xzf "$i"
   ## unpack it to /usr overwriting eventual existing copy
-  cp -r $dir/* $INSTALL_FOLDER
-  rm -rf $dir
+  cp -r "$dir"/* "$INSTALL_FOLDER"
+  rm -rf "$dir"
 done
 
 
-if [ ! -d $PKG_DATA ]
+if [ ! -d "$PKG_DATA" ]
 then
     echo "Creating $PKG_DATA directory"
-    mkdir $PKG_DATA
+    mkdir -p "$PKG_DATA"
 fi
 
 # download sample data
-wget -c --progress=dot:mega $BASEURL/samples.tar.gz
-(cd $PKG_DATA && tar xzf $TMP/samples.tar.gz)
+wget -N --progress=dot:mega "$BASEURL/samples.tar.gz"
+(cd "$PKG_DATA" && tar xzf "$TMP/samples.tar.gz")
 
-chown user:users $PKG_DATA/*
+chown "$USER_NAME":users "$PKG_DATA"/* -R
 
 ## start icon
 cat << EOF > /usr/share/applications/spatialite-gui.desktop
