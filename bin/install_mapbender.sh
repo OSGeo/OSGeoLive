dae959d5f8001c7779bf5169f8d0c15bd98c721a
Astrid Emde <astrid.emde@wheregroup.com>
2010-07-03 11:57:51 +0000

    test to checkin the script, sh not finished yet
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3815 7da46fd8-bd2a-0410-a152-cf9810cf5b10

---
+++ bin/install_mapbender.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Copyright (c) 2009 The Open Source Geospatial Foundation.
+# Licensed under the GNU LGPL.
+# 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public Lice*nse for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+
+# About:
+# =====
+# This script will install Mapbender 2.6.2
+
+# Running:
+# =======
+# sudo service apache2 start
+# Then open a web browser and go to http://localhost/mapbender/ Login with root/root
+#
+# Requires: Apache2, PHP5, mbstring, gettext, imagic
+#
