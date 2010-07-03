fa93a3d3908923afacad7d4c4d57d7571837b973
Hamish Bowman <hamish_b yahoo com>
2010-07-03 12:48:42 +0000

    +debug msg for bad chmod
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3823 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_deegree.sh
+++ bin/install_deegree.sh
@@ -111,6 +111,8 @@ getWithMd5 deegree-2.3_tomcat-6.0.26.tar.gz
 
 ## unpack as root, chmod everything to be group/world readable
 tar xzf deegree-2.3_tomcat-6.0.26.tar.gz -o -C $INSTALL_FOLDER
+#debug:
+ls $INSTALL_FOLDER/
 chmod -R go+r $INSTALL_FOLDER/deegree-2.3_tomcat-6.0.26
 
 ### Configure Application ###
