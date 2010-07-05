0a3f253234aa2d95edbbb245d306bcb202e0e945
Judit Mays <mays@lat-lon.de>
2010-07-05 09:28:06 +0000

    updated to kill the right application
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3866 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- app-conf/deegree/deegree_stop.sh
+++ app-conf/deegree/deegree_stop.sh
@@ -10,5 +10,5 @@
 
 ## stop tomcat (and deegree webapps):
 ## kill the deegree-tomcat process
-pid=$(ps fax|grep deegree|grep java|grep -v grep|cut -d\   -f 2)
+pid=$(ps fax|grep apache-tomcat-6.0.26|grep java|grep -v grep|cut -d\   -f 2)
 kill -9 $pid
