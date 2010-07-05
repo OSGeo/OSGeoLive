8ab7cb4478b17760f2741a02c8e0c4d28618bc3c
Brian M Hamlin <screenlight@mac.com>
2010-07-05 00:46:40 +0000

    psql -v ON_ERROR_STOP=1
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3858 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_postgis.sh
+++ bin/install_postgis.sh
@@ -83,7 +83,7 @@ pgis_file="/usr/share/postgresql/$PG_VERSION/contrib/postgis-1.5/postgis.sql"
 
 
 sudo -u $USER_NAME psql -d template_postgis -f "$pgis_file"
-sudo -u $USER_NAME psql --exit-on-error -d template_postgis \
+sudo -u $USER_NAME psql -v ON_ERROR_STOP=1 -d template_postgis \
    -f /usr/share/postgresql/$PG_VERSION/contrib/postgis-1.5/spatial_ref_sys.sql 
 
 # from install_gpsdrive - verify not necessary..
