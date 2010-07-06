2e1ec050e67ca535400ff634ffa029b27c84b810
Johan Van de Wauw <johan.vandewauw@gmail.com>
2010-07-06 08:49:03 +0000

    Add saga gis install script
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3881 7da46fd8-bd2a-0410-a152-cf9810cf5b10

---
+++ bin/install_saga.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# install_saga.sh
+# 
+#
+# Created by Johan Van de Wauw on 2010-07-02
+# Copyright (c) 2009 The Open Source Geospatial Foundation.
+# Licensed under the GNU LGPL.
+# lucid: saga 2.0.4 is provided in ubuntugis and ubuntugis/unstable
+# maverick: saga 2.0.4 is provided in universe
+# Add repositories
+cp ../sources.list.d/ubuntugis.list /etc/apt/sources.list.d/
+
+#Add signed key for repositorys LTS and non-LTS
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 68436DDF  
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160  
+
+apt-get update
+apt-get install saga libsaga
+
+# Additional documentation
+mkdir -p /usr/local/share/saga
+cd /usr/local/share/saga
+wget -c --progress=dot:mega http://sourceforge.net/projects/saga-gis/files/SAGA%20-%20Documentation/SAGA%202%20User%20Guide/SAGA2_UserGuide_Cimmery_20070401.pdf/download
+
+# Desktop icon
+cp /usr/share/applications/saga_gui.desktop "$USER_HOME/Desktop/"
+
