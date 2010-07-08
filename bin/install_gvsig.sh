6b40eb445b3e4b3e09023c8db771179f0b991f57
Hamish Bowman <hamish_b yahoo com>
2010-07-08 11:48:26 +0000

    revert r3926; install_desktop.sh sorts the icons from the desktop into their categories (creatingthe menu structure)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@3934 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_gvsig.sh
+++ bin/install_gvsig.sh
@@ -79,8 +79,8 @@ rm $TMP/$GVSIG_PACKAGE
 
 # place a gvSIG icon on desktop
 if [ -d $USER_DESKTOP ] ; then
-   echo "Copying icon to desktop at $USER_DESKTOP/Desktop GIS"
-		  cp /usr/share/applications/gvsig.desktop "$USER_DESKTOP/Desktop GIS"
+   echo "Copying icon to desktop at $USER_DESKTOP"
+   cp /usr/share/applications/gvsig.desktop "$USER_DESKTOP"
 fi
 
 # download documentation 
