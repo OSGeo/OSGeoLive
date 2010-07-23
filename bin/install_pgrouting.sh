c5564c0a49bc7dd9139ca24202935d7fb4973918
Daniel Kastl <daniel.kastl@georepublic.de>
2010-07-23 04:00:51 +0000

    no sudo with osm2pgrouting. it doesn't like the -U option then
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@4137 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_pgrouting.sh
+++ bin/install_pgrouting.sh
@@ -69,7 +69,7 @@ fi
 # Install osm2pgrouting package
 apt-get --assume-yes install osm2pgrouting
 
-# TODO: Install workshop material
+# Install workshop material
 apt-get --assume-yes install pgrouting-workshop
 
 # Create tmp folders
@@ -116,8 +116,8 @@ else
 	# NOTE: Conversion can take a a few minutes depending on the extent of the sample data.
 	# Assuming that the sample data won't be very big, it should be OK to run the conversion here, 
 	# otherwise it should be done in advance somehow (TODO).
-	echo "Run osm2pgrouting converter"
-	sudo -u $USER_NAME osm2pgrouting -file "$TMP/sampledata.osm" \
+	echo "Run osm2pgrouting converter (this may take a while)"
+	osm2pgrouting -file "$TMP/sampledata.osm" \
 					-conf "/usr/share/osm2pgrouting/mapconfig.xml" \
 					-dbname $OSM_DB \
 					-user $USER_NAME \
