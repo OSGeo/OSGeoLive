dabf8f96d31394e18450f14b14b37144a409351b
Gerald Fenoy <gerald.fenoy@geolabs.fr>
2010-07-29 02:21:47 +0000

    Add the ZOO Project files
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@4295 7da46fd8-bd2a-0410-a152-cf9810cf5b10

---
+++ bin/install_zoo-project.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+# Copyright (c) 2010 The Open Source Geospatial Foundation.
+# Licensed under the GNU LGPL.
+# 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+
+# About:
+# =====
+# This script will install ZOO Project
+
+# Running:
+# =======
+# sudo ./install_zoo-project.sh
+
+# Requires: Apache2, GeoServer (for the demo only)
+#
+# Uninstall:
+# ============
+# sudo rm /etc/apache2/conf.d/zoo-project
+# sudo rm -rf /var/www/zoo*
+# sudo rm -rf /usr/lib/cgi-bin/zoo_loader.cgi
+# sudo rm -rf /usr/lib/cgi-bin/proxy.cgi
+# sudo rm -rf /usr/lib/cgi-bin/*zcfg
+# sudo rm -rf /usr/lib/cgi-bin/ogr_service*
+# sudo rm -rf /usr/lib/cgi-bin/main.cfg
+# sudo rm -rf /usr/lib/cgi-bin/main.cfg
+# sudo rm -rf /usr/share/applications/zoo-project.desktop
+# sudo rm -rf /home/user/Desktop/Servers/zoo-project.desktop
+
+# live disc's username is "user"
+USER_NAME="user"
+USER_HOME="/home/$USER_NAME"
+ZOO_TMP=/tmp
+
+ZOO_APACHE_CONF="/etc/apache2/conf.d/zoo-project"
+
+# Download ZOO Project LiveDVD tarball.
+wget -c http://zoo-project.org/dl/zoo-livedvd.tar.bz2 -O $ZOO_TMP/zoo-livedvd.tar.bz2
+
+# Uncompress ZOO Project LiveDVD tarball.
+tar -xjpf $ZOO_TMP/zoo-livedvd.tar.bz2 -C /
+
+echo -n "Apache configuration update ..."
+# Add ZOO Project apache configuration
+cat << EOF > $ZOO_APACHE_CONF
+
+        <Directory /var/www/zoo/>
+                Options Indexes FollowSymLinks MultiViews
+                AllowOverride All
+                Order allow,deny
+                allow from all
+        </Directory>
+
+EOF
+
+echo -n "Done\n"
+
+a2enmod rewrite
+
+
+#Add Launch icon to desktop
+if [ ! -e /usr/share/applications/zoo-project.desktop ] ; then
+   cat << EOF > /usr/share/applications/zoo-project.desktop
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=ZOO Project
+Comment=ZOO Project
+Categories=Application;Education;Geography;
+Exec=firefox http://localhost/zoo-demo/spatialtools.html
+Icon=/var/www/zoo-demo/spatialtools_files/zoo-icon.png
+Terminal=false
+StartupNotify=false
+Categories=Education;Geography;
+EOF
+fi
+cp /usr/share/applications/zoo-project.desktop "$USER_HOME/Desktop/Servers"
+
+
+# Reload Apache
+/etc/init.d/apache2 force-reload
