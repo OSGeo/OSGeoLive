0f9ba47d103665b1a3b623f401bf232c45b2af75
Hamish Bowman <hamish_b yahoo com>
2010-07-31 09:22:18 +0000

    don't include deinstalled packages in the hog list
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@4357 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/main.sh
+++ bin/main.sh
@@ -135,7 +135,7 @@ dpkg --get-selections > "$DOC_DIR/package_manifest.txt"
 
 echo "==============================================================="
 echo "Show top 50 packages hogging the most space on the disc:"
-dpkg-query --show --showformat='${Package;-50}\t${Installed-Size}\n' \
+dpkg-query --show --showformat='${Package;-50}\t${Installed-Size}\t${Status}\n' \
   | sort -k 2 -n | grep -v deinstall | tac | head -n 50 | \
   awk '{printf "%.3f MB \t %s\n", $2/(1024), $1}'
 
