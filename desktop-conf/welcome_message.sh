2eed3058a24e662ca7db4e15cbec0551aa7adb4a
Hamish Bowman <hamish_b yahoo com>
2010-08-03 12:11:43 +0000

    demo of how to have a popup message on login
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@4457 7da46fd8-bd2a-0410-a152-cf9810cf5b10

---
+++ desktop-conf/welcome_message.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Welcome pop-up message when the user logs in.
+#
+# You need a *.desktop launcher (same as for the desktop icons)
+# and put it in /home/user/.config/autostart/.
+#  Exec=gxmessage -file /home/user/welcome.txt 
+#    or
+#  Exec=/path/to/this/script.sh
+#
+# and maybe more?
+
+echo"
+Welcome to the OSGeo Live GIS DVD - Barcelona 2010
+
+Have a nice time. There's beer in the fridge.
+" | gxmessage -file - -title "Welcome to the OSGeo Live GIS Disc"
+
