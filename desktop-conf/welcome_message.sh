8a5666664a865d491953e951a206cb27c1d126fe
Hamish Bowman <hamish_b yahoo com>
2010-08-04 05:03:22 +0000

    only display welcome message if machine is freshly turned on (n.b. this script is just experimental, not currently enabled)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@4476 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- desktop-conf/welcome_message.sh
+++ desktop-conf/welcome_message.sh
@@ -4,14 +4,52 @@
 #
 # You need a *.desktop launcher (same as for the desktop icons)
 # and put it in /home/user/.config/autostart/.
-#  Exec=gxmessage -file /home/user/welcome.txt 
-#    or
-#  Exec=/path/to/this/script.sh
-#
-# and maybe more?
 
-echo"
-Welcome to the OSGeo-Live GIS DVD.
+
+########################
+if [ 1 -eq 0 ] ; then
+# move to install_desktop.sh
+mkdir -p /usr/local/share/osgeo-desktop
+
+cat << EOF > "/usr/local/share/osgeo-desktop/welcome_message.desktop"
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=Welcome message
+Comment=Live Demo welcome message
+Exec=/usr/local/share/osgeo-desktop/welcome_message.sh
+Terminal=false
+StartupNotify=false
+Hidden=false
+EOF
+
+mkdir -p /home/user/.config/autostart
+cp /usr/local/share/osgeo-desktop/welcome_message.desktop \
+   /home/user/.config/autostart/
+   
+# better to put this file in SVN as desktop-conf/welcome_message.txt
+#cp ../desktop-conf/welcome_message.txt \
+#   /usr/local/share/osgeo-desktop/welcome_message.txt
+
+echo "
+Welcome to the OSGeo Live GIS DVD.
+
 Have a nice time. There's beer in the fridge.
-" | gxmessage -file - -title "Welcome to the OSGeo Live GIS Disc"
+" > /usr/local/share/osgeo-desktop/welcome_message.txt
+
+cp /usr/local/share/osgeo-desktop/welcome_message.txt /home/user/
+fi
+########################
+
+
+
+AWAKE=`uptime | sed -e 's/.* up //' -e 's/,.*//' | grep 'min' | cut -f1 -d' '`
+
+if [ -z "$AWAKE" ] || [ "$AWAKE" -gt 10 ] ; then
+   # only show when the machine is first switched on
+   exit
+fi
+
+gxmessage -file /usr/local/share/osgeo-desktop/welcome_message.txt \
+   -title "Welcome to the OSGeo Live GIS Disc"
 
