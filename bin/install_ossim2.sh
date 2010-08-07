ecf87ace2bb2451e8b471c8789f42c918baea70e
Hamish Bowman <hamish_b yahoo com>
2010-08-07 05:06:45 +0000

    standardize
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@4546 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_ossim2.sh
+++ bin/install_ossim2.sh
@@ -13,56 +13,55 @@ USER_HOME="/home/$USER_NAME"
 BUILD_DIR=`pwd`
 APP_DATA_DIR="$BUILD_DIR/../app-data/ossim"
 DATA_FOLDER="/usr/local/share/data"
+TMP_DIR=/tmp/build_ossim
+
 
 #Add repositories
-
-#wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/sources.list.d/ubuntugis.list \
-#     --output-document=/etc/apt/sources.list.d/ubuntugis.list
-
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160
+wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/sources.list.d/ubuntugis.list \
+   --output-document=/etc/apt/sources.list.d/ubuntugis.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160
+apt-get update
 
 
 # install main dependencies
-
-sudo apt-get install --assume-yes libtiff4 libgeotiff1.2 libgdal1-1.7.0 \
-libfreetype6 libcurl3 libopenscenegraph56 libqt4-opengl \
-libexpat1 libpng3 libgdal1-1.7.0-grass libfftw3-3 libqt3-mt
+apt-get install --assume-yes libtiff4 libgeotiff1.2 libgdal1-1.7.0 \
+  libfreetype6 libcurl3 libopenscenegraph56 libqt4-opengl \
+  libexpat1 libpng3 libgdal1-1.7.0-grass libfftw3-3 libqt3-mt
 
 
 # download ossim
+mkdir -p $TMP_DIR
+cd $TMP_DIR
 
-cd /tmp
-wget http://www.geofemengineering.it/data/ossim.tar.gz
-tar -zxvf ossim.tar.gz
-mv ossim /usr/local/
+wget -N --progress=dot:mega http://www.geofemengineering.it/data/ossim.tar.gz
+tar zxf ossim.tar.gz
+cp -r ossim /usr/local/
+
 echo "/usr/local/ossim/
 /usr/local/ossim/lib" >> ossim.conf
-mv ossim.conf /etc/ld.so.conf.d/
+cp -f ossim.conf /etc/ld.so.conf.d/
 ldconfig
 
 
-wget http://www.geofemengineering.it/data/ossim_settings.tar.gz
-tar -zxvf ossim_settings.tar.gz
-mv ossim_settings ossim
-mv ossim /usr/share/ossim
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_settings.tar.gz
+tar zxf ossim_settings.tar.gz
+cp -r ossim_settings /usr/share/ossim
 
-mv /usr/share/ossim/images/ossimPlanet.xpm /usr/share/pixmaps/ossimPlanet.xpm
-mv /usr/share/ossim/images/ossim.xpm /usr/share/pixmaps/ossim.xpm
-mv /usr/share/ossim/imagelinker.desktop /usr/share/applications/imagelinker.desktop
-mv /usr/share/ossim/ossimplanet.desktop /usr/share/applications/ossimplanet.desktop
+cp /usr/share/ossim/images/ossimPlanet.xpm /usr/share/pixmaps/ossimPlanet.xpm
+cp /usr/share/ossim/images/ossim.xpm /usr/sohare/pixmaps/ossim.xpm
+cp /usr/share/ossim/imagelinker.desktop /usr/share/applications/imagelinker.desktop
+cp /usr/share/ossim/ossimplanet.desktop /usr/share/applications/ossimplanet.desktop
 
-
+#maybe 'grep -c' to check if it's already done
 echo "PATH=$PATH:/usr/local/ossim:/usr/local/ossim/bin" >> /home/user/.bashrc
 echo "export PATH" >> /home/user/.bashrc
 # source /home/user/.bashrc
 
 
 # Additional dependence for Grass / Qgis plug-in :
-#
-apt-get install --assume-yes grass qgis python-pysqlite2 python-pygame python-scipy \
-   python-serial python-psycopg2
-
-
+apt-get install --assume-yes grass qgis python-pysqlite2 python-pygame \
+   python-scipy python-serial python-psycopg2
 
 
 cp /usr/share/applications/imagelinker.desktop "$USER_HOME/Desktop/"
@@ -96,15 +95,16 @@ EOF
 fi
 
 
-
 #Install the Manual and Intro guide locally and link them to the description.html
 mkdir /usr/local/share/ossim
-wget -c --progress=dot:mega http://download.osgeo.org/ossim/docs/pdfs/ossim_users_guide.pdf \
-	--output-document=/usr/local/share/ossim/ossim_users_guide.pdf
+wget -N --progress=dot:mega \
+   http://download.osgeo.org/ossim/docs/pdfs/ossim_users_guide.pdf \
+   --output-document=/usr/local/share/ossim/ossim_users_guide.pdf
 ln -s /usr/share/doc/ossim-doc/ossimPlanetUsers.pdf /usr/local/share/ossim/
 
-wget -c --progress=dot:mega http://ossim.telascience.org/ossimdata/Documentation/OSSIM_Whitepaper.pdf \
-        --output-document=/usr/local/share/ossim/OSSIM_Whitepaper.pdf
+wget -N --progress=dot:mega \
+   http://ossim.telascience.org/ossimdata/Documentation/OSSIM_Whitepaper.pdf \
+   --output-document=/usr/local/share/ossim/OSSIM_Whitepaper.pdf
 
 
 #Download data used to test the application
@@ -116,22 +116,53 @@ mkdir -p $KML_DATA
 mkdir -p $RASTER_DATA
 mkdir -p $ELEV_DATA
 
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/band1.tif  --output-document=$RASTER_DATA/band1.tif           
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/band2.tiff  --output-document=$RASTER_DATA/band2.tif
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/band3.tiff  --output-document=$RASTER_DATA/band3.tif
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/SRTM_u03_n041e002.tif  --output-document=$RASTER_DATA/SRTM_u03_n041e002.tif
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/kml/Plaza_de_Cataluna.kmz --output-document=$KML_DATA/Plaza_de_Cataluna.kmz
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/kml/View_towards_Sagrada_Familia.kmz --output-document=$KML_DATA/View_towards_Sagrada_Familia.kmz
-#wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/landsatrgb.prj --output-document=$PKG_DATA/landsatrgb.prj
-#wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/session.session --output-document=$PKG_DATA/session.session
-ossim-img2rr $RASTER_DATA/band1.tif $RASTER_DATA/band2.tif $RASTER_DATA/band3.tif
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/band1.tif \
+   --output-document=$RASTER_DATA/band1.tif           
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/band2.tiff \
+   --output-document=$RASTER_DATA/band2.tif
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/band3.tiff \
+    --output-document=$RASTER_DATA/band3.tif
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/SRTM_u03_n041e002.tif \
+    --output-document=$RASTER_DATA/SRTM_u03_n041e002.tif
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/kml/Plaza_de_Cataluna.kmz \
+   --output-document=$KML_DATA/Plaza_de_Cataluna.kmz
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/kml/View_towards_Sagrada_Familia.kmz \
+   --output-document=$KML_DATA/View_towards_Sagrada_Familia.kmz
+
+#wget -N --progress=dot:mega \
+#   http://www.geofemengineering.it/data/ossim_data/landsatrgb.prj \
+#   --output-document=$PKG_DATA/landsatrgb.prj
+#wget -N --progress=dot:mega \
+#   http://www.geofemengineering.it/data/ossim_data/session.session \
+#   --output-document=$PKG_DATA/session.session
+
+ossim-img2rr $RASTER_DATA/band1.tif $RASTER_DATA/band2.tif \
+   $RASTER_DATA/band3.tif
  
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N40E002.hgt --output-document=/usr/share/ossim/elevation/elev/N40E002.hgt 
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N40E002.omd --output-document=/usr/share/ossim/elevation/elev/N40E002.omd 
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N41E002.hgt --output-document=/usr/share/ossim/elevation/elev/N41E002.hgt 
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N41E002.omd --output-document=/usr/share/ossim/elevation/elev/N41E002.omd  
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N42E002.hgt --output-document=/usr/share/ossim/elevation/elev/N42E002.hgt 
-wget -c --progress=dot:mega http://www.geofemengineering.it/data/ossim_data/elev/N42E002.omd --output-document=/usr/share/ossim/elevation/elev/N42E002.omd 
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/elev/N40E002.hgt \
+   --output-document=/usr/share/ossim/elevation/elev/N40E002.hgt 
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/elev/N40E002.omd \
+   --output-document=/usr/share/ossim/elevation/elev/N40E002.omd 
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/elev/N41E002.hgt \
+   --output-document=/usr/share/ossim/elevation/elev/N41E002.hgt 
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/elev/N41E002.omd \
+   --output-document=/usr/share/ossim/elevation/elev/N41E002.omd  
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/elev/N42E002.hgt \
+   --output-document=/usr/share/ossim/elevation/elev/N42E002.hgt 
+wget -N --progress=dot:mega \
+   http://www.geofemengineering.it/data/ossim_data/elev/N42E002.omd \
+   --output-document=/usr/share/ossim/elevation/elev/N42E002.omd 
 
 cp -r $APP_DATA_DIR $QUICKSTART
 ln -s $QUICKSTART $USER_HOME/ossim
@@ -143,4 +174,4 @@ done
 
 chmod 644 /usr/local/share/ossim/*.pdf
 
-echo "Finished installing Ossim "
+echo "Finished installing Ossim"
