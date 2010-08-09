81621854723bd7634086ef9eefc9f88d84aec181
Gerald Fenoy <gerald.fenoy@geolabs.fr>
2010-08-09 10:05:19 +0000

    Update the install_zoo-project.sh to chown the desktop file as other scripts do, add the install_zoo-project.sh use from main.sh as requested.
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@4577 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_zoo-project.sh
+++ bin/install_zoo-project.sh
@@ -86,6 +86,7 @@ Categories=Education;Geography;
 EOF
 fi
 cp /usr/share/applications/zoo-project.desktop "$USER_HOME/Desktop/"
+chown -R $USER_NAME:$USER_NAME "$USER_HOME/Desktop/zoo-project.desktop"
 
 
 # Reload Apache
