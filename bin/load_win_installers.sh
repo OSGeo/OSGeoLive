c08f91c0614c800bae46b4b0e5ba3ef568c32136
Hamish Bowman <hamish_b yahoo com>
2010-08-12 05:34:47 +0000

    update to latest release rc
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@4615 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/load_win_installers.sh
+++ bin/load_win_installers.sh
@@ -40,7 +40,7 @@ for URL in \
   http://forge.osor.eu/frs/download.php/746/gvSIG-1_9-windows-i586-withjre.exe \
   http://linfiniti.com/downloads/QGIS-1.4.0-1-No-GrassSetup.exe \
   http://home.gdal.org/tmp/vcredist_x86.exe \
-  http://grass.osgeo.org/grass64/binary/mswindows/native/WinGRASS-6.4.0RC6-1-Setup.exe \
+  http://download.osgeo.org/livedvd/data/grass/WinGRASS-6.4.SVN-r43036-1-Setup.exe \
   http://maptools.org/dl/ms4w/ms4w_3.0_beta7.zip \
   http://download.osgeo.org/mapguide/releases/2.2.0/Beta/MapGuideOpenSource-2.2.0.4829-Beta1.exe \
   http://maptiler.googlecode.com/files/maptiler-1.0-beta2-setup.exe \
