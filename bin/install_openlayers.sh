4950e6f0ce119c27079d851d25809399f98527d2
crschmidt <crschmidt>
2011-02-11 11:01:03 +0000

    Update OpenLayers release to 2.10
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@5559 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_openlayers.sh
+++ bin/install_openlayers.sh
@@ -22,7 +22,7 @@
 # Then open a web browser and go to http://localhost/OpenLayers/
 
 TMP_DIR="/tmp/build_openlayers"
-OL_VERSION="2.9.1"
+OL_VERSION="2.10"
 
 USER_NAME="user"
 USER_HOME="/home/$USER_NAME"
