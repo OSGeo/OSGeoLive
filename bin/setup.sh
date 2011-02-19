5c575ce2770b130e483017db2abdc1e07689b84b
Hamish Bowman <hamish_b yahoo com>
2011-02-19 12:32:25 +0000

    put nedit back.
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@5677 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/setup.sh
+++ bin/setup.sh
@@ -55,7 +55,7 @@ apt-get install --yes wget less zip unzip bzip2 p7zip \
   cvs cvsutils subversion subversion-tools gnuit mercurial \
   openssh-client lftp sl usbutils wireless-tools \
   locate diff patch fuseiso menu \
-  vim medit nano screen \
+  vim medit nedit nano screen \
   evince ghostscript a2ps netpbm qiv \
   lynx mutt mc xchat rxvt units scrot
 
