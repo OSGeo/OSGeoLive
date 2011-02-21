04704d16ca0e96aad1d6a615a5e4bc5f7c7e9ba7
Bruno Binet <bruno.binet@camptocamp.com>
2011-02-21 08:36:19 +0000

    Revert "remove proj 900913 since already available in esri.extra"
    This reverts commit aa2954f4fb2351ae841a6b2eef2b1c8a68033b11.
    
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@5720 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_mapfish.sh
+++ bin/install_mapfish.sh
@@ -93,6 +93,13 @@ sudo -u postgres psql --quiet -f $MAPFISH_TMP_DIR/mapfish_www-data.sql
 sudo -u postgres dropdb v2.0_mapfishsample
 sudo -u postgres ./geodata/create_database.bash -p
 
+# add proj 900913 if not exists
+grep "<900913>" /usr/share/proj/epsg
+if [ $? -ne 0 ] ; then
+    echo "" >> /usr/share/proj/epsg
+    echo "<900913> +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs" >> /usr/share/proj/epsg
+fi
+
 # update tomcat server.xml conf to enable ajp
 wget -nv -O $TOMCAT_SERVER_CONF http://www.mapfish.org/downloads/foss4g_livedvd/server.xml
 /etc/init.d/tomcat6 restart
