67671c4d9c764bfb805f727957cb14439f7d9624
Alex Mandel <tech@wildintellect.com>
2011-03-16 06:53:55 +0000

    Rolled back to known working isolinux, TODO: revisit for ver 5
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@6022 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/build_iso.sh
+++ bin/build_iso.sh
@@ -62,14 +62,14 @@ wget -nv https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/app-data/remastersys.co
      --output-document="$LOGS"
 cp "$LOGS" /etc/remastersys.conf
 
-# grab copy of custom isolinux
-wget -nv http://osprey.ucdavis.edu/downloads/osgeo/gisvm/isolinux.tar.gz \
-	--output-document="$WORKDIR"/isolinux.tar.gz
+# grab copy of custom isolinux - disabled until fixed
+#wget -nv http://osprey.ucdavis.edu/downloads/osgeo/gisvm/isolinux.tar.gz \
+#	--output-document="$WORKDIR"/isolinux.tar.gz
 
-tar xzf "$WORKDIR"/isolinux.tar.gz  -C "$WORKDIR"
-cp -R  "$WORKDIR"/isolinux/* /etc/remastersys/customisolinux/
+#tar xzf "$WORKDIR"/isolinux.tar.gz  -C "$WORKDIR"
+#cp -R  "$WORKDIR"/isolinux/* /etc/remastersys/customisolinux/
 #Not sure if this is necessary but can't hurt to make them read only
-chmod -R uga-w /etc/remastersys/customisolinux/
+#chmod -R uga-w /etc/remastersys/customisolinux/
 
 # Add Windows and Mac installers by copying files into ISOTMP folder
 #./load_win_installers.sh
