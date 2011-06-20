fa215d88ccbd112d0cab4010b6206be45483bc44
Alex Mandel <tech@wildintellect.com>
2011-06-20 06:15:48 +0000

    Firebug isn't in repos anymore, install from web?
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@6229 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_mapfish.sh
+++ bin/install_mapfish.sh
@@ -43,7 +43,7 @@ fi
 apt-get --assume-yes install python2.6 python2.6-dev \
     cgi-mapserver postgis postgresql-8.4-postgis tomcat6 \
     libpq-dev libapache2-mod-fcgid libapache2-mod-wsgi \
-    firebug
+    #TODO: firebug no longer in repos
 
 if [ $? -ne 0 ] ; then
    echo 'ERROR: Package install failed! Aborting.'
