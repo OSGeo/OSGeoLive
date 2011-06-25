4b0af43335c38dda39c72916049c69eedfc1654f
camerons <cameron.shorter@gmail.com>
2011-06-25 11:55:46 +0000

    create symbolic links to data and doc directories from /var/www
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@6257 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_main_docs.sh
+++ bin/install_main_docs.sh
@@ -54,11 +54,18 @@ ln -s /usr/local/share/udig/udig-docs .
 ln -s /usr/local/mbsystem .
 ln -s /usr/local/share/qgis .
 ln -s /usr/share/doc/geopublishing .
+ln -s /usr/local/share/saga .
 # HTML
-ln -s /usr/share/doc/gmt/html gmt
+ln -s /usr/share/doc/gmt .
+ln -s /usr/share/doc/gmt-examples .
+ln -s /usr/share/doc/gmt-tutorial .
 ln -s /usr/local/share/mapnik/demo mapnik
 ln -s /usr/local/share/opencpn/doc opencpn
 ln -s /usr/local/share/ushahidi .
+ln -s /usr/local/share/otb .
+ln -s /usr/local/share/ossim .
+# Data
+ln -s /usr/local/share/data .
 
 # Create symbolic links to project specific data
 mkdir -p ${DATA_FOLDER}
