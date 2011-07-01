a2184bdb2779437cc73ee3abe30b7475cda887e7
Judit Mays <mays@lat-lon.de>
2011-07-01 12:59:32 +0000

    Update to deegree 3!
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@6312 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_deegree.sh
+++ bin/install_deegree.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #################################################################################
 #
-# Purpose: Installation of deegree_2.3-with-tomcat_6.0.26-all-in-one into Xubuntu
-# Author:  Judit Mays <mays@lat-lon.de>
+# Purpose: Installation of v3.1-pre8-with-tomcat_6.0.32-all-in-one into Xubuntu
+# Author:  Judit Mays <mays@lat-lon.de>, Johannes Kuepper <kuepper@lat-lon.de>
 # Credits: Stefan Hansen <shansen@lisasoft.com>
 #          H.Bowman <hamish_b  yahoo com>
 # Date:    $Date$
@@ -29,13 +29,9 @@
 # =====
 # This script will install deegree-tomcat-all-in-one into Xubuntu
 #
-# deegree version 2.3 runs with both java-sun-1.5 (preferred) and java-sun-1.6.
-# It works best with java-sun-1.5.
+# deegree version 3.1-pre8 runs with java-sun-1.6
 #
-# It can be installed into servlet containers:
-#    Tomcat 5.5.x (but not Tomcat 5.5.26)
-#    Tomcat 6.0.x (but not Tomcat 6.0.16)
-# The preferred servlet container is Tomcat (version as described above)
+# It should be installed into servlet container Tomcat 6.0.x
 #
 
 # Running:
@@ -46,7 +42,7 @@
 
 TMP="/tmp/build_deegree"
 INSTALL_FOLDER="/usr/lib"
-DEEGREE_FOLDER="$INSTALL_FOLDER/deegree-2.3_tomcat-6.0.26"
+DEEGREE_FOLDER="$INSTALL_FOLDER/deegree-3.1-pre8_tomcat-6.0.32"
 BIN="/usr/bin"
 USER_NAME="user"
 USER_HOME="/home/$USER_NAME"
@@ -61,7 +57,7 @@ if [ ! -x "`which wget`" ] ; then
    exit 1
 fi
 if [ ! -x "`which java`" ] ; then
-   echo "ERROR: java is required, please install a SUN version (preferably java-1.5-sun) and try again."
+   echo "ERROR: java is required, please install java-1.6-sun and try again."
    exit 1
 fi
 
@@ -72,7 +68,7 @@ cd "$TMP"
 getWithMd5()
 {
     rm -f $1.md5
-    wget -nv http://download.deegree.org/LiveDVD/FOSS4G2010/$1.md5
+    wget -nv http://download.deegree.org/LiveDVD/FOSS4G2011/$1.md5
 
     if (test -f $1) then
         if(md5sum -c $1.md5) then
@@ -81,10 +77,10 @@ getWithMd5()
         else
             echo "md5 hash is not correct. Downloading $1 again."
             rm -f $1
-            wget -c --progress=dot:mega http://download.deegree.org/LiveDVD/FOSS4G2010/$1
+            wget -c --progress=dot:mega http://download.deegree.org/LiveDVD/FOSS4G2011/$1
         fi
     else
-        wget -c --progress=dot:mega http://download.deegree.org/LiveDVD/FOSS4G2010/$1
+        wget -c --progress=dot:mega http://download.deegree.org/LiveDVD/FOSS4G2011/$1
     fi
 
     if (md5sum -c $1.md5) then
@@ -95,49 +91,34 @@ getWithMd5()
     fi
 }
 
-## TODO: improve. This function does not test on correct md5 sums. 
-getFromOsgeo()
-{
-    echo "FIXME: don't use wget for local files, just copy from local svn checkout."
-    rm -f $1
-    ## wget -c --progress=dot:mega http://download.deegree.org/LiveDVD/FOSS4G2010/$1
-    wget -nv http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/app-conf/deegree/$1
-}
-
 ### Install Application ###
 
 ## get deegree-tomcat-all-in-one
-getWithMd5 deegree-2.3_tomcat-6.0.26.tar.gz
+getWithMd5 deegree-3.1-pre8_tomcat-6.0.32.tar.gz
 
 ## unpack as root, chmod everything to be group/world readable
-tar xzf deegree-2.3_tomcat-6.0.26.tar.gz -o -C $INSTALL_FOLDER
-chmod -R go+r $INSTALL_FOLDER/apache-tomcat-6.0.26
+tar xzf deegree-3.1-pre8_tomcat-6.0.32.tar.gz -o -C $INSTALL_FOLDER
+chmod -R go+r $DEEGREE_FOLDER/*
 
 ### Configure Application ###
 
 ## Download startup script for deegree
-## according to issue 478, the deegree_start.sh script was moved to OSGeo-SVN
-#getWithMd5 deegree_start.sh
-getFromOsgeo deegree_start.sh
+getWithMd5 deegree_start.sh
 ## copy it into the /usr/bin folder
 cp deegree_start.sh $BIN
 
 ## Download shutdown script for deegree
-## according to issue 481, the deegree_stop.sh script was moved to OSGeo-SVN
-#getWithMd5 deegree_stop.sh
-getFromOsgeo deegree_stop.sh
+getWithMd5 deegree_stop.sh
 ## copy it into the /usr/bin folder
 cp deegree_stop.sh $BIN
 
-## make executable
+## make start and stop script executable
 chmod 755 $BIN/deegree_st*.sh
 
 
 ### install desktop icons ##
 if [ ! -e "/usr/share/icons/deegree_desktop_48x48.png" ] ; then
-   #wget -nv "http://download.deegree.org/LiveDVD/FOSS4G2010/deegree_desktop_48x48.png"
-   echo "FIXME: don't use wget for local files, just copy from local svn checkout."
-   wget -nv "http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/app-conf/deegree/deegree_desktop_48x48.png"
+   wget -nv "http://download.deegree.org/LiveDVD/FOSS4G2011/deegree_desktop_48x48.png"
    mv deegree_desktop_48x48.png /usr/share/icons/
 fi
 
@@ -153,7 +134,7 @@ if [ ! -e /usr/share/applications/deegree-start.desktop ] ; then
 Type=Application
 Encoding=UTF-8
 Name=Start deegree
-Comment=deegree v2.3
+Comment=deegree v3.1-pre8
 Categories=Application;Geography;Geoscience;Education;
 Exec=dash $USER_HOME/launchassist.sh $BIN/deegree_start.sh
 Icon=/usr/share/icons/deegree_desktop_48x48.png
@@ -172,7 +153,7 @@ if [ ! -e /usr/share/applications/deegree-stop.desktop ] ; then
 Type=Application
 Encoding=UTF-8
 Name=Stop deegree
-Comment=deegree v2.3
+Comment=deegree v3.1-pre8
 Categories=Application;Geography;Geoscience;Education;
 Exec=dash $USER_HOME/launchassist.sh  $BIN/deegree_stop.sh
 Icon=/usr/share/icons/deegree_desktop_48x48.png
@@ -182,4 +163,3 @@ fi
 
 cp -a /usr/share/applications/deegree-stop.desktop "$USER_HOME/Desktop/"
 chown -R $USER_NAME:$USER_NAME "$USER_HOME/Desktop/deegree-stop.desktop"
-
