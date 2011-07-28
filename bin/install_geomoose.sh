4f7a023250401446cb15d04a2a0fa3a742eddaf7
Jim Klassen <klassen.js@gmail.com>
2011-07-28 23:14:09 +0000

    Changes to work with MapServer 6
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@6632 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_geomoose.sh
+++ bin/install_geomoose.sh
@@ -21,14 +21,20 @@
 # =======
 # sudo ./install_geomoose.sh
 
+apt-get install php5-sqlite
+
+USER_NAME="user"
+USER_DIR="/home/user"
 
 mkdir -p /tmp/build-geomoose
 
 cd /tmp/build-geomoose
 
+# Download and extract GeoMOOSE 2.4
 wget -c "http://www.geomoose.org/downloads/geomoose-2.4.tar.gz"
+wget -c "http://www.geomoose.org/downloads/geomoose-2.4-mapserver-6.patch"
 
-tar -xzvf geomoose-2.4.tar.gz
+tar -xzf geomoose-2.4.tar.gz
 
 rm -rf /usr/local/geomoose
 
@@ -38,6 +44,32 @@ cd /usr/local/geomoose
 
 mv /tmp/build-geomoose/geomoose*/* .
 
-./configure --with-url-path=/geomoose --with-temp-directory=/tmp --with-mapfile-root=/usr/local/geomoose/maps
+# Configure GeoMOOSE 2.4 (Builds configuration files from templates)
+./configure --with-url-path=/geomoose --with-temp-directory=/tmp/ --with-mapfile-root=/usr/local/geomoose/maps/
 
+# Setup htdocs directory to be available to apache
 ln -s /usr/local/geomoose/htdocs /var/www/geomoose
+
+# Patch GeoMOOSE State Demo layer to work with MapServer 6.x
+# Patches are submitted upstream and will likely be included
+# (or their equivlent) in GeoMOOSE 2.6.
+patch -p1 < /tmp/build-geomoose/geomoose-2.4-mapserver-6.patch
+
+## Install menu and desktop shortcuts
+## Skip right now for lack of sutiable icon
+#cat << EOF > /usr/share/applications/GeoMOOSE.desktop
+#[Desktop Entry]
+#Version=1.0
+#Encoding=UTF-8
+#Type=Application
+#Name=GeoMOOSE
+#Comment=View GeoMOOSE sample application in browser
+#Categories=Application;Geography;Geoscience;Education;
+#Exec=sensible-browser http://localhost/geomoose/geomoose.html
+#Icon=/usr/share/icons/geomoose.png
+#Terminal=false
+#StartupNotify=false
+#EOF
+#cp /usr/share/applications/GeoMOOSE.desktop $USER_DIR/Desktop/
+#chown $USER_NAME:$USER_NAME $USER_DIR/Desktop/GeoMOOSE.desktop
+
