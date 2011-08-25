8cd38cc973f49d27a9bb60b9f99bc1d5c63ea5eb
Frank Gasdorf <fgdrf@users.sourceforge.net>
2011-08-25 13:52:04 +0000

    osssimPlanet to ossimPlanet with sed -i -e options
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@7095 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_ossim.sh
+++ bin/install_ossim.sh
@@ -55,8 +55,7 @@ tar -zxf ossim_settings.tar.gz
 mv ossim_settings/* /usr/share/ossim/
 
 #patch for ticket https://trac.osgeo.org/osgeo/ticket/647 
-sed 's/OsssimPlanet/OssimPlanet/g' /usr/share/ossim/ossimplanet.desktop > /usr/share/ossim/ossimplanet.desktop_2
-mv /usr/share/ossim/ossimplanet.desktop_2 /usr/share/ossim/ossimplanet.desktop
+sed -i -e 's/OsssimPlanet/OssimPlanet/g' /usr/share/ossim/ossimplanet.desktop
 
 mv /usr/share/ossim/images/ossimPlanet.xpm /usr/share/pixmaps/ossimPlanet.xpm
 mv /usr/share/ossim/images/ossim.xpm /usr/share/pixmaps/ossim.xpm
