fc5fa3916d9ebe173fff25b93e65dccfef509a11
Hamish Bowman <hamish_b yahoo com>
2011-11-20 06:39:48 +0000

    minor linewrap
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@7278 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_atlasstyler.sh
+++ bin/install_atlasstyler.sh
@@ -35,26 +35,34 @@ apt-get -q update
 # Install AtlasStyler and documentation
 apt-get -q install --yes --no-install-recommends atlasstyler geopublishing-doc
 
-# Now we download some predefined symbols for atlasstyler because the user is probably off-line
+# Now we download some predefined symbols for atlasstyler because the user
+#  is probably off-line
 USER_NAME="user"
 USER_HOME="/home/$USER_NAME"
 
-mkdir -p $USER_HOME/.AtlasStyler
+mkdir -p "$USER_HOME/.AtlasStyler"
 
 wget --cut-dirs=0 -nH -q -c -r \
    http://freemapsymbols.org/ -A "*.sld" \
    --exclude-directories="svg" \
-   -P $USER_HOME/.AtlasStyler/templates
+   -P "$USER_HOME/.AtlasStyler/templates"
 
-# Now we create a .properties file which predefines that AtlasStyler open-file-dialog will start in the data directory, and the export directory points to the desktop.
-echo "lastImportDirectory=$USER_HOME/data/natural_earth" > $USER_HOME/.AtlasStyler/atlasStyler.properties
-echo "lastExportDirectory=$USER_HOME/Desktop" >> $USER_HOME/.AtlasStyler/atlasStyler.properties
+# Now we create a .properties file which predefines that AtlasStyler
+#  open-file-dialog will start in the data directory, and the export
+#  directory points to the desktop.
+echo "lastImportDirectory=$USER_HOME/data/natural_earth" \
+   > $USER_HOME/.AtlasStyler/atlasStyler.properties
+echo "lastExportDirectory=$USER_HOME/Desktop" \
+   >> $USER_HOME/.AtlasStyler/atlasStyler.properties
+
 # Also predefine, that adding a PG datasource will open with the OSGeoLive defaults
-echo "dbList=postgis|postgresql\://localhost/user|localhost|5432|user|user|natural_earth|true|public@" >> $USER_HOME/.AtlasStyler/atlasStyler.properties
-echo "wfsList=http\://localhost\:8082/geoserver/ows 1.0.0|http\://localhost\:8082/geoserver/ows|v1_0_0|0|null|false|null|admin|geoserver@" >> $USER_HOME/.AtlasStyler/atlasStyler.properties
+echo "dbList=postgis|postgresql\://localhost/user|localhost|5432|user|user|natural_earth|true|public@" \
+   >> $USER_HOME/.AtlasStyler/atlasStyler.properties
+echo "wfsList=http\://localhost\:8082/geoserver/ows 1.0.0|http\://localhost\:8082/geoserver/ows|v1_0_0|0|null|false|null|admin|geoserver@" \
+   >> $USER_HOME/.AtlasStyler/atlasStyler.properties
 
 # Change the owner of the user's local AtlasStyler settings to user:user
-chown -R $USER_NAME:$USER_NAME $USER_HOME/.AtlasStyler
+chown -R $USER_NAME:$USER_NAME "$USER_HOME/.AtlasStyler"
 
 # Create a desktop icon
 cp /usr/share/applications/atlasstyler.desktop "$USER_HOME/Desktop/"
