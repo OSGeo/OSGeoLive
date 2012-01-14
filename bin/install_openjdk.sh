261d73a8c2dedba9e5f037032afcc20108c65113
Hamish Bowman <hamish_b yahoo com>
2012-01-14 02:34:45 +0000

    make it easy for developers to switch VMs (K.I.S.S.)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@7329 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_openjdk.sh
+++ bin/install_openjdk.sh
@@ -17,14 +17,28 @@
 # This script will install openjdk version 7
 
 TMP="/tmp/build_openjdk"
+USER_NAME="user"
+USER_HOME="/home/$USER_NAME"
 
 mkdir -p "$TMP"
 cd "$TMP"
 
 # Download and uncompress openjdk
-wget -c --progress=dot:mega http://download.oracle.com/otn-pub/java/jdk/7/jdk-7-linux-i586.tar.gz
+wget -c --progress=dot:mega \
+   "http://download.oracle.com/otn-pub/java/jdk/7/jdk-7-linux-i586.tar.gz"
 tar -zxvf jdk-7-linux-i586.tar.gz
 
-# Move JDK 7 directory to place where it should be. Right, to the /usr/lib/jvm/jdk1.7.0 directory. Use this command for that
+# Move JDK 7 directory to the /usr/lib/jvm/jdk1.7.0 directory.
+#FIXME: use /usr/local/ not /usr for custom installs if at all possible
 mkdir -p /usr/lib/jvm
 mv ./jdk1.7.0/ /usr/lib/jvm/jdk1.7.0
+
+
+# make it easy for developers to switch VMs
+if [ `grep -c JAVA_HOME "$USER_HOME/.profile"` -eq 0 ] ; then
+   echo >> "$USER_HOME/.profile"
+   echo "# Uncomment the following line to use the OpenJDK java VM" \
+      >> "$USER_HOME/.profile"
+   echo "#JAVA_HOME=/usr/lib/jvm/jdk1.7.0; export JAVA_HOME" \
+      >> "$USER_HOME/.profile"
+fi
