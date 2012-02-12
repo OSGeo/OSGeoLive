f1629db11fd27ec252d8ce1cb81c4aef91e15029
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-02-12 17:12:51 +0000

    pycsw installation fix to overpass new sourceforge certificates
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@7563 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_pycsw.sh
+++ bin/install_pycsw.sh
@@ -54,7 +54,7 @@ mkdir -p "$PYCSW_TMP"
 echo 'Downloading package ...'
 
 # Download pycsw LiveDVD tarball.
-wget -N --progress=dot:mega \
+wget -N --progress=dot:mega --no-check-certificate \
      "https://sourceforge.net/projects/pycsw/files/$VERSION/pycsw-$VERSION.tar.gz/download" \
      -O "$PYCSW_TMP/pycsw-$VERSION.tar.gz"
 
