acfcb5d478141d743e71d0fd815dbe58cfa6f7f2
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-04-29 01:48:08 +0000

    pycsw: switching from pure script to deb installation since package is available now through Launchpad
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@7905 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_pycsw.sh
+++ bin/install_pycsw.sh
@@ -23,11 +23,7 @@
 # sudo ./install_pycsw.sh
 #
 # Requires: Apache2, python-lxml, python-shapely and python-sqlalchemy
-#
-# Uninstall:
-# ============
-# sudo rm /etc/apache2/conf.d/pycsw
-# sudo rm -rf /var/www/pycsw*
+
 
 VERSION=1.2.0
 
@@ -36,75 +32,22 @@ echo "Installing pycsw $VERSION"
 echo 'Installing dependencies ...'
 
 # install dependencies
-apt-get install --yes apache2 python-lxml python-sqlalchemy python-shapely
+apt-get install --yes apache2 python-lxml python-sqlalchemy python-shapely python-proj
+
+echo 'Installing pycsw ...'
+
+add-apt-repository ppa:gcpp-kalxas/ppa-tzotsos
+apt-get update
+apt-get install --yes pycsw
 
 # live disc's username is "user"
 USER_NAME=user
 USER_HOME=/home/$USER_NAME
 
-WEB=/var/www
-
-# package specific settings
-PYCSW_HOME="$WEB/pycsw"
-PYCSW_TMP=/tmp/build_pycsw
-PYCSW_APACHE_CONF=/etc/apache2/conf.d/pycsw
-
-mkdir -p "$PYCSW_TMP"
-
-echo 'Downloading package ...'
-
-# Download pycsw LiveDVD tarball.
-wget -N --progress=dot:mega --no-check-certificate \
-     "https://sourceforge.net/projects/pycsw/files/$VERSION/pycsw-$VERSION.tar.gz/download" \
-     -O "$PYCSW_TMP/pycsw-$VERSION.tar.gz"
-
-echo 'Extracting package ...'
-
-# Uncompress pycsw LiveDVD tarball.
-tar zxf "$PYCSW_TMP/pycsw-$VERSION.tar.gz" -C "$PYCSW_TMP"
-mv "$PYCSW_TMP/pycsw-$VERSION" "$PYCSW_TMP/pycsw"
-mv "$PYCSW_TMP/pycsw" "$WEB"
-
-echo "Updating Apache configuration ..."
-# Add pycsw apache configuration
-cat << EOF > "$PYCSW_APACHE_CONF"
-
-        <Directory $PYCSW_HOME>
-	    Options FollowSymLinks +ExecCGI
-	    Allow from all
-	    AddHandler cgi-script .py
-        </Directory>
-
-EOF
-
-echo "Generating configuration files ..."
-# Add pycsw configuration files
-
-cp $PYCSW_HOME/default-sample.cfg $PYCSW_HOME/default.cfg
-
-echo "Done."
-
-#Add Launch icon to desktop
-if [ ! -e /usr/share/applications/pycsw.desktop ] ; then
-   cat << EOF > /usr/share/applications/pycsw.desktop
-[Desktop Entry]
-Type=Application
-Encoding=UTF-8
-Name=pycsw
-Comment=pycsw catalog server
-Categories=Application;Education;Geography;
-Exec=firefox http://localhost/pycsw/tester/index.html http://localhost/en/overview/pycsw_overview.html
-Icon=/var/www/pycsw/docs/_static/pycsw-logo.png
-Terminal=false
-StartupNotify=false
-Categories=Education;Geography;
-EOF
-fi
-
 cp /usr/share/applications/pycsw.desktop "$USER_HOME/Desktop/"
 chown "$USER_NAME:$USER_NAME" "$USER_HOME/Desktop/pycsw.desktop"
 
 # Reload Apache
-/etc/init.d/apache2 force-reload
+#/etc/init.d/apache2 force-reload
 
 
