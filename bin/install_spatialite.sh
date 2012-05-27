6c86d4af121bb22b4f412c50cc58339d82e60f7c
Micha Silver <micha@arava.co.il>
2012-05-27 17:06:50 +0000

    spatialite almost all from repos
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@7971 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_spatialite.sh
+++ bin/install_spatialite.sh
@@ -35,8 +35,8 @@ if [ ! -x "`which wget`" ] ; then
    exit 1
 fi
 
-
-#Add repositories
+########################
+### Add repositories ###
 if [ ! -e /etc/apt/sources.list.d/ubuntugis.list ] ; then
    cp ../sources.list.d/ubuntugis.list /etc/apt/sources.list.d/
 fi
@@ -47,28 +47,30 @@ apt-get update
 mkdir -p "$TMP"
 cd "$TMP"
 
-### Download and unpack spatialite ###
+###########################
+### Install from repo ###
+# Download and unpack spatialite ###
 
 ## get spatialite
-echo "Getting and unpacking spatialite"
+echo "Getting and installing spatialite"
+apt-get install --assume-yes spatialite-bin libspatialite3
+# The gui programs depend on libjpeg62
+apt-get install --assume-yes libjpeg62
+# Now the other stuff
+apt-get install --assume-yes librasterlite1 rasterlite-bin spatialite-gui
 
-apt-get install --assume-yes spatialite-bin
-
-
+###########################
+### Compile from source ###
+# Only spatialite-gis needs compiling from source now
 BASEURL=http://www.gaia-gis.it/spatialite-2.4.0
-# New trento.sqlite downloaded from download.osgeo.org 
-OSGEO_URL=http://download.osgeo.org/livedvd/data/spatialite
-SQLITE_DB=trento.sqlite.tar.gz
-PACKAGES="
- librasterlite-linux-x86-1.0.tar.gz
- rasterlite-tools-linux-x86-1.0.tar.gz
- spatialite-gis-linux-x86-1.0.0.tar.gz
- spatialite-gui-linux-x86-1.3.0.tar.gz"
-
+#PACKAGES="
+# librasterlite-linux-x86-1.0.tar.gz
+# rasterlite-tools-linux-x86-1.0.tar.gz
+# spatialite-gis-linux-x86-1.0.0.tar.gz
+# spatialite-gui-linux-x86-1.3.0.tar.gz"
+PACKAGES="spatialite-gis-linux-x86-1.0.0.tar.gz"
 for i in $PACKAGES; do
-  fn=$(basename $i)
-  dir=${fn%.tar.gz}
-  # dir=`basename .tar.gz`  # would do the same thing in 1 step
+  dir=`basename $i .tar.gz`
   wget -c --progress=dot:mega "$BASEURL/$i"
   tar xzf "$i"
   ## unpack it to /usr overwriting eventual existing copy
@@ -76,21 +78,25 @@ for i in $PACKAGES; do
   rm -rf "$dir"
 done
 
-
+##########################
+### Sample data ###
+# New trento.sqlite downloaded from download.osgeo.org 
+OSGEO_URL=http://download.osgeo.org/livedvd/data/spatialite
+SQLITE_DB=trento.sqlite.tar.gz
 if [ ! -d "$PKG_DATA" ]
 then
     echo "Creating $PKG_DATA directory"
     mkdir -p "$PKG_DATA"
 fi
 
-# download sample data
 wget -N --progress=dot:mega "${OSGEO_URL}/${SQLITE_DB}"
 (cd "$PKG_DATA" && tar xzf "${TMP}/${SQLITE_DB}")
 
 chgrp -R users $PKG_DATA
 chmod -R g+w $PKG_DATA
 
-## start icon
+#############################
+### GUI start icons ###
 cat << EOF > /usr/share/applications/spatialite-gui.desktop
 [Desktop Entry]
 Type=Application
