24e2fca2c38c2b6ed27431d4a5e7ea7b995cae6e
Hamish Bowman <hamish_b yahoo com>
2012-06-03 00:16:20 +0000

    more USER_HOME, linewrap, quote variables
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8001 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- app-conf/gvsig/gvSIG.sh
+++ app-conf/gvsig/gvSIG.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # gvSIG.sh
+
+if [ -z "$USER_NAME" ] ; then
+   USER_NAME="user"
+fi
+USER_HOME="/home/$USER_NAME"
+
 export GVSIG_LIBS="/opt/gvSIG_1.11/libs/"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$GVSIG_LIBS"
 export PROJ_LIB="/opt/gvSIG_1.11/bin/gvSIG/extensiones/org.gvsig.crs/data"
@@ -7,13 +13,15 @@ export GDAL_DATA="$GVSIG_LIBS/gdal_data"
 cd "/opt/gvSIG_1.11/bin"
 
 #copy default symbology
-if [ ! -d "$HOME/gvSIG/Styles" ]; then 
-	echo "Styles not found"
-	cp -r gvSIG/extensiones/org.gvsig.extended-symbology/default_symbology/Styles $HOME/gvSIG
+if [ ! -d "$USER_HOME/gvSIG/Styles" ]; then 
+    echo "Styles not found"
+    cp -r gvSIG/extensiones/org.gvsig.extended-symbology/default_symbology/Styles \
+          "$USER_HOME/gvSIG"
 fi 
-if [ ! -d "$HOME/gvSIG/Symbols" ]; then 
-	echo "Symbols not found"
-	cp -r gvSIG/extensiones/org.gvsig.extended-symbology/default_symbology/Symbols $HOME/gvSIG
+if [ ! -d "$USER_HOME/gvSIG/Symbols" ]; then 
+    echo "Symbols not found"
+    cp -r gvSIG/extensiones/org.gvsig.extended-symbology/default_symbology/Symbols \
+          "$USER_HOME/gvSIG"
 fi 
 
 
@@ -27,16 +35,20 @@ done
 #select JRE
 if [ -d "/usr/lib/jvm/java-6-openjdk" ]; then
         echo "OpenJDK found"
-        export PATH=/usr/lib/jvm/java-6-openjdk/bin:$PATH
+        export PATH="/usr/lib/jvm/java-6-openjdk/bin:$PATH"
 elif [ -d "/usr/lib/jvm/java-6-sun" ]; then
         echo "Sun JRE found"
-        export PATH=/usr/lib/jvm/java-6-sun/bin:$PATH
+        export PATH="/usr/lib/jvm/java-6-sun/bin:$PATH"
 else
         echo "JRE not found, using default"
 fi
 
-if [ -e "/home/user/gvSIG/sample-project.gvp" ]; then
-	java -Djava.library.path=/usr/lib:"/opt/gvSIG_1.11/libs" -cp andami.jar$LIBRARIES -Xmx500M com.iver.andami.Launcher gvSIG gvSIG/extensiones "/home/user/gvSIG/sample-project.gvp" 
+if [ -e "$USER_HOME/gvSIG/sample-project.gvp" ]; then
+	java -Djava.library.path=/usr/lib:"/opt/gvSIG_1.11/libs" \
+	     -cp andami.jar$LIBRARIES -Xmx500M com.iver.andami.Launcher \
+	     gvSIG gvSIG/extensiones "$USER_HOME/gvSIG/sample-project.gvp" 
 else
-	java -Djava.library.path=/usr/lib:"/opt/gvSIG_1.11/libs" -cp andami.jar$LIBRARIES -Xmx500M com.iver.andami.Launcher gvSIG gvSIG/extensiones "$@"
+	java -Djava.library.path=/usr/lib:"/opt/gvSIG_1.11/libs" \
+	     -cp andami.jar$LIBRARIES -Xmx500M com.iver.andami.Launcher \
+	     gvSIG gvSIG/extensiones "$@"
 fi
