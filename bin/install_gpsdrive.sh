694a0b7f2f8ef5523cc34826231bf7680945e040
Hamish Bowman <hamish_b yahoo com>
2012-06-04 09:52:21 +0000

    cleanup and rearrangements, further tweaks for Beijing
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8017 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_gpsdrive.sh
+++ bin/install_gpsdrive.sh
@@ -19,14 +19,7 @@
 #
 
 
-
-
-echo "The GpsDrive package is not yet ready for Ubuntu 12.04"
-exit 0
-
-
 CITY=Beijing
-
 
 # live disc's username is "user"
 if [ -z "$USER_NAME" ] ; then
@@ -40,63 +33,178 @@ BUILD_DIR=`pwd`
 
 #### install program ####
 
-## packaged version (2.10pre4) is long out of date, so we build 2.11svn manually.
-BUILD_LATEST=0
-
-# base packages
-if [ "$BUILD_LATEST" -eq 0 ] ; then
-   # install very old pre-packaged version
-   #PACKAGES="gpsd gpsd-clients python-gps gpsdrive"
-   PACKAGES="gpsd gpsd-clients python-gps"
-else
-   # important pre-req
-   PACKAGES="gpsd gpsd-clients python-gps"
-fi
-
-# add some useful Recommends
-PACKAGES="$PACKAGES espeak gdal-bin gpsbabel \
+PACKAGES="gpsd gpsd-clients python-gps \
+   espeak gdal-bin gpsbabel \
    graphicsmagick-imagemagick-compat \
-   postgresql-9.1-postgis python-mapnik2 \
-   speech-dispatcher"
+   postgresql-9.1-postgis \
+   python-mapnik2 \
+   speech-dispatcher \
+   openstreetmap-map-icons-square \
+   openstreetmap-map-icons-scalable \
+   openstreetmap-map-icons-classic \
+   ttf-dejavu \
+   wget netpbm optipng \
+   sqlite3 sqlitebrowser"
 
-apt-get --assume-yes install  $PACKAGES
+apt-get --assume-yes install $PACKAGES
 
 if [ $? -ne 0 ] ; then
    echo "An error occurred installing packages. Aborting install."
    exit 1
 fi
 
-# highly useful
-apt-get --assume-yes install sqlite3 sqlitebrowser
+
+
+#####################################################################
+echo "The GpsDrive package is not yet ready for Ubuntu 12.04"
+exit 0
+#####################################################################
+
 
 
 
 #######################
 ## use prebuilt debs
 
-if [ $BUILD_LATEST -eq 0 ] ; then
-  if [ ! -d "$TMP_DIR" ] ; then
-    mkdir "$TMP_DIR"
-  fi
+if [ ! -d "$TMP_DIR" ] ; then
+  mkdir "$TMP_DIR"
+fi
+cd "$TMP_DIR"
+
+URL="http://download.osgeo.org/livedvd/data/gpsdrive/precise"
+MAIN_FILE="gpsdrive_2.12+svn_i386.deb"
+EXTRA_FILES="
+  gpsdrive-friendsd_2.12+svn_i386.deb
+  gpsdrive-utils_2.12+svn_i386.deb"
+
+wget -c --progress=dot:mega "$URL/$MAIN_FILE"
+for FILE in $EXTRA_FILES ; do
+   wget -c -nv "$URL/$FILE"
+done
+
+gdebi --non-interactive --quiet gpsdrive-friendsd_2.12+svn_i386.deb
+gdebi --non-interactive --quiet gpsdrive-utils_2.12+svn_i386.deb
+gdebi --non-interactive --quiet gpsdrive_2.12+svn_i386.deb
+
+
+
+#### install data ####
+
+mkdir "$USER_HOME/.gpsdrive"
+
+
+# program defaults
+cat << EOF > "$USER_HOME/.gpsdrive/gpsdriverc"
+lastlong = 116.405
+lastlat = 39.98
+scalewanted = 50000
+dashboard_3 = 12
+autobestmap = 0
+mapnik = 1
+mapnik_caching = 0
+minsecmode = 2
+friendsname = LiveDVD
+showbutton_trackrestart = 0
+showbutton_trackclear = 0
+icon_theme = classic.small
+osmdbfile = /usr/share/data/osm/${CITY}_poi.db
+mapnik_postgis_dbname = osm_local_smerc
+EOF
+
+
+# add any waypoints you want to see displayed
+cat << EOF > "$USER_HOME/.gpsdrive/way.txt"
+Sydney_Convention_Centre        -33.8750   151.2005
+Barcelona_Convention_Centre      41.3724     2.1518
+FOSS4G_2011_(Sheraton_Denver)    39.74251 -104.9891
+OSM_State_of_the_Map_2011        39.7457  -105.0034
+Beijing_Conference_Center        40.0195   116.4218
+EOF
+
+#download latest OSM POIs for host city
+#wget -N --progress=dot:mega  http://poi.gpsdrive.de/$COUNTRY.db.bz2
+wget -N --progress=dot:mega \
+  "http://download.osgeo.org/livedvd/data/osm/${CITY}_poi.db.bz2"
+bzip2 -d "${CITY}_poi.db.bz2"
+mkdir -p /usr/share/data/osm/
+mv "${CITY}_poi.db" /usr/share/data/osm/
+
+
+# fool the hardcoded bastard
+mkdir -p /usr/share/mapnik/world_boundaries
+
+# bypass Mapnik wanting 300mb World Boundaries DB to be installed, use Natural Earth instead.
+sed -e 's+/usr/share/mapnik/world_boundaries/world_boundaries_m+/usr/local/share/data/natural_earth/10m_land+' \
+    -e 's/Layer name="world-1".*/Layer name="world-1" status="on" srs="+proj=longlat +datum=WGS84 +no_defs +over">/' \
+    \
+    -e 's+/usr/share/mapnik/world_boundaries/world_bnd_m+/usr/local/share/data/natural_earth/10m_land+' \
+    -e 's/Layer name="world".*/Layer name="world" status="on" srs="+proj=longlat +datum=WGS84 +no_defs +over">/' \
+    \
+    -e 's+/usr/share/mapnik/world_boundaries/processed_p+/usr/local/share/data/natural_earth/10m_land+' \
+    -e 's/Layer name="coast-poly".*/Layer name="coast-poly" status="on" srs="+proj=longlat +datum=WGS84 +no_defs +over">/' \
+    \
+    -e 's+/usr/share/mapnik/world_boundaries/builtup_area+/usr/local/share/data/natural_earth/10m_urban_areas+' \
+    -e 's/Layer name="buildup".*/Layer name="builtup" status="on" srs="+proj=longlat +datum=WGS84 +no_defs +over">/' \
+    \
+    -e 's+/usr/share/mapnik/world_boundaries/places+/usr/local/share/data/natural_earth/10m_populated_places_simple+' \
+    -e 's/Layer name="places".*/Layer name="builtup" status="on" srs="+proj=longlat +datum=WGS84 +no_defs">/' \
+    \
+    /usr/share/gpsdrive/osm-template.xml > "$USER_HOME/.gpsdrive/osm.xml"
+# "$TMP_DIR/gpsdrive-$VERSION/build/scripts/mapnik/osm-template.xml" \
+
+
+# change DB name from "gis" to "osm_local_smerc" as per install_osm.sh
+sed -i -e 's+<Parameter name="dbname">gis</Parameter>+<Parameter name="dbname">osm_local_smerc</Parameter>+' \
+  "$USER_HOME/.gpsdrive/osm.xml"
+
+
+chown -R $USER_NAME:$USER_NAME "$USER_HOME/.gpsdrive"
+
+cp /usr/share/applications/gpsdrive.desktop "$USER_HOME/Desktop/"
+chown $USER_NAME:$USER_NAME "$USER_HOME/Desktop/gpsdrive.desktop"
+
+
+echo "Finished installing GpsDrive."
+
+exit
+
+
+
+
+
+############################################################################
+############################################################################
+############################################################################
+
+############################################################################
+
+
+############################################################################
+
+
+
+
+############################################################################
+
+
+if [ 1 -eq 0 ] ; then
+  ## needed for newer builds if icons were *not* installed via .debs above
+  # minimal icon set
+  wget -c -nv "http://downloads.sourceforge.net/project/gpsdrive/additional%20data/minimal%20icon%20set/openstreetmap-map-icons-minimal.tar.gz?use_mirror=internode"
+  cd /
+  tar xzf "$TMP_DIR"/openstreetmap-map-icons-minimal.tar.gz
   cd "$TMP_DIR"
 
-  URL="http://download.osgeo.org/livedvd/data/gpsdrive/precise"
-  MAIN_FILE="gpsdrive_2.12+svn_i386.deb"
-  EXTRA_FILES="
-    gpsdrive-friendsd_2.12+svn_i386.deb
-    gpsdrive-utils_2.12+svn_i386.deb"
-
-  wget -c --progress=dot:mega "$URL/$MAIN_FILE"
-  for FILE in $EXTRA_FILES ; do
-     wget -c -nv "$URL/$FILE"
-  done
-
-  gdebi --non-interactive --quiet gpsdrive-friendsd_2.12+svn_i386.deb
-  gdebi --non-interactive --quiet gpsdrive-utils_2.12+svn_i386.deb
-  gdebi --non-interactive --quiet gpsdrive_2.12+svn_i386.deb
-
+  #debug dummy copy of geoinfo.db
+  #tar xzf openstreetmap-map-icons-minimal.tar.gz usr/share/icons/map-icons/geoinfo.db
+  #cp usr/share/icons/map-icons/geoinfo.db "$USER_HOME/.gpsdrive/"
+  #  .gpsdrive/gpsdriverc: geoinfofile = $USER_HOME/.gpsdrive/geoinfo.db
 fi
 
+
+
+## packaged version (2.10pre4) is long out of date, so we build 2.11svn manually.
+BUILD_LATEST=0
 
 
 #######################
@@ -111,7 +219,7 @@ if [ $BUILD_LATEST -eq 1 ] ; then
 
   ## FIXME (use better home once known/officially released)
   ##  wget -c --progress=dot:mega "http://www.gpsdrive.de/packages/gpsdrive-$VERSION.tar.gz"
-  FILE=gpsdrive_2.12svn2638.tar.gz
+  FILE=gpsdrive_2.12svn2645.tar.gz
   wget --progress=dot:mega -O "$FILE" \
      "http://sites.google.com/site/hamishbowman/${FILE}?attredirects=0"
 
@@ -131,43 +239,13 @@ if [ $BUILD_LATEST -eq 1 ] ; then
   ## --- apply any patches here ---
 
   # fix package dependencies
-  PATCHES="gpsdrive_blue_mapnik"
-
-  for PATCH in $PATCHES ; do
-     patch -p0 < "$BUILD_DIR/../app-conf/gpsdrive/$PATCH.patch"
-  done
+  PATCH="gpsdrive_blue_mapnik"
+  patch -p0 < "$BUILD_DIR/../app-conf/gpsdrive/$PATCH.patch"
 
   if [ $? -ne 0 ] ; then
      echo "An error occurred patching package. Aborting install."
      exit 1
   fi
-
-  # local database name is "osm_local_smerc"
-  sed -i -e 's/"gis"/"osm_local_smerc"/' src/database.c
-
-  # installed mapnik version is 2.0
-  MAPNIK_VER=2.0
-  sed -i -e "s+/usr/lib/mapnik/0.5+/usr/lib/mapnik/$MAPNIK_VER+" src/gpsdrive_config.c
-  sed -i -e 's+^      mapnik$+      mapnik2+' cmake/Modules/FindMapnik.cmake
-
-
-# no longer needed? better to use `sed -i` for this anyway..
-if [ 0 -eq 1 ] ; then
-  cat << EOF > "gpsdrive_fix_icon.patch"
---- data/gpsdrive.desktop.ORIG  2009-08-31 01:42:39.000000000 +1200
-+++ data/gpsdrive.desktop       2009-08-31 01:43:19.000000000 +1200
-@@ -3,7 +3,7 @@
- Comment=GPS Navigation. You need to setup Gpsd manually
- Comment[de]=GPS Navigationsprogramm
- Exec=gpsdrive
--Icon=gpsicon
-+Icon=/usr/share/gpsdrive/pixmaps/gpsicon.png
- Terminal=false
- Type=Application
- Categories=Graphics;Network;Geography;
-EOF
-   patch -p0 < "gpsdrive_fix_icon.patch"
-fi
 
 
   ### install any missing build-dep packages
@@ -213,11 +291,11 @@ fi
   sed -i -e 's+/usr/local/lib64+/usr/lib/i386-linux-gnu+' \
        /usr/share/cmake-2.8/Modules/FindGTK2.cmake
 
-  #fix unneeded package (?scripts/osm/perl_lib/Geo/Gpsdrive/getstreet.pm 
-  #   http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=580588 )
-  sed -i -e 's+^\t libtext-query-perl+#\t libtext-query-perl+' debian/control
+#  #fix unneeded package (?scripts/osm/perl_lib/Geo/Gpsdrive/getstreet.pm 
+#  #   http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=580588 )
+#  sed -i -e 's+^\t libtext-query-perl+#\t libtext-query-perl+' debian/control
 
-  
+
   # build package
   # - debuild and co. should already be installed by setup.sh
   #debuild binary
@@ -233,10 +311,6 @@ fi
  
   # get+install at least one OSM icon set package
   #   see http://www.gpsdrive.de/development/map-icons/overview.en.shtml
-
-  # see what happens if we install the offical icon packages
-  apt-get  --assume-yes install openstreetmap-map-icons-square \
-     openstreetmap-map-icons-scalable openstreetmap-map-icons-classic
 
   #echo "Downloading support packages ... (please wait)"
   #DL_URL="http://www.gpsdrive.de/ubuntu/pool/precise"
@@ -316,101 +390,3 @@ fi
 ##
 ## end self-build
 #######################
-
-
-
-
-#### install data ####
-mkdir "$USER_HOME/.gpsdrive"
-
-
-if [ 1 -eq 0 ] ; then
-  ## needed for newer builds if icons were *not* installed via .debs above
-  # minimal icon set
-  wget -c -nv "http://downloads.sourceforge.net/project/gpsdrive/additional%20data/minimal%20icon%20set/openstreetmap-map-icons-minimal.tar.gz?use_mirror=internode"
-  cd /
-  tar xzf "$TMP_DIR"/openstreetmap-map-icons-minimal.tar.gz
-  cd "$TMP_DIR"
-
-  #debug dummy copy of geoinfo.db
-  #tar xzf openstreetmap-map-icons-minimal.tar.gz usr/share/icons/map-icons/geoinfo.db
-  #cp usr/share/icons/map-icons/geoinfo.db "$USER_HOME/.gpsdrive/"
-  #  .gpsdrive/gpsdriverc: geoinfofile = $USER_HOME/.gpsdrive/geoinfo.db
-fi
-
-
-# program defaults
-cat << EOF > "$USER_HOME/.gpsdrive/gpsdriverc"
-lastlong = -104.9895
-lastlat = 39.7424
-scalewanted = 3000
-dashboard_3 = 12
-autobestmap = 0
-mapnik = 1
-mapnik_caching = 0
-minsecmode = 2
-friendsname = LiveDVD
-showbutton_trackrestart = 0
-showbutton_trackclear = 0
-icon_theme = classic.small
-osmdbfile = /usr/share/gpsdrive/${CITY}_poi.db
-EOF
-
-
-# add any waypoints you want to see displayed
-cat << EOF > "$USER_HOME/.gpsdrive/way.txt"
-Sydney_Convention_Centre        -33.8750   151.2005
-Barcelona_Convention_Centre      41.3724     2.1518
-FOSS4G_2011_(Sheraton_Denver)    39.74251 -104.9891
-OSM_State_of_the_Map_2011        39.7457  -105.0034
-Beijing_Conference_Center        40.0195   116.4218
-EOF
-
-#download latest OSM POIs for host city
-#wget -N --progress=dot:mega  http://poi.gpsdrive.de/$COUNTRY.db.bz2
-wget -N --progress=dot:mega \
-  "http://download.osgeo.org/livedvd/data/osm/${CITY}_poi.db.bz2"
-bzip2 -d "${CITY}_poi.db.bz2"
-mv "${CITY}_poi.db" /usr/share/gpsdrive/
-
-
-# fool the hardcoded bastard
-mkdir -p /usr/share/mapnik/world_boundaries
-
-# bypass Mapnik wanting 300mb World Boundaries DB to be installed, use Natural Earth instead.
-sed -e 's+/usr/share/mapnik/world_boundaries/world_boundaries_m+/usr/local/share/data/natural_earth/10m_land+' \
-    -e 's/Layer name="world-1".*/Layer name="world-1" status="on" srs="+proj=longlat +datum=WGS84 +no_defs +over">/' \
-    \
-    -e 's+/usr/share/mapnik/world_boundaries/world_bnd_m+/usr/local/share/data/natural_earth/10m_land+' \
-    -e 's/Layer name="world".*/Layer name="world" status="on" srs="+proj=longlat +datum=WGS84 +no_defs +over">/' \
-    \
-    -e 's+/usr/share/mapnik/world_boundaries/processed_p+/usr/local/share/data/natural_earth/10m_land+' \
-    -e 's/Layer name="coast-poly".*/Layer name="coast-poly" status="on" srs="+proj=longlat +datum=WGS84 +no_defs +over">/' \
-    \
-    -e 's+/usr/share/mapnik/world_boundaries/builtup_area+/usr/local/share/data/natural_earth/10m_urban_areas+' \
-    -e 's/Layer name="buildup".*/Layer name="builtup" status="on" srs="+proj=longlat +datum=WGS84 +no_defs +over">/' \
-    \
-    -e 's+/usr/share/mapnik/world_boundaries/places+/usr/local/share/data/natural_earth/10m_populated_places_simple+' \
-    -e 's/Layer name="places".*/Layer name="builtup" status="on" srs="+proj=longlat +datum=WGS84 +no_defs">/' \
-    \
-    /usr/share/gpsdrive/osm-template.xml > "$USER_HOME/.gpsdrive/osm.xml"
-# "$TMP_DIR/gpsdrive-$VERSION/build/scripts/mapnik/osm-template.xml" \
-
-
-# change DB name from "gis" to "osm_local_smerc" as per install_osm.sh
-sed -i -e 's+<Parameter name="dbname">gis</Parameter>+<Parameter name="dbname">osm_local_smerc</Parameter>+' \
-  "$USER_HOME/.gpsdrive/osm.xml"
-sed -i -e 's/mapnik_postgis_dbname = gis/mapnik_postgis_dbname = osm_local_smerc/' \
-  "$USER_HOME/.gpsdrive/gpsdriverc"
-
-# ensure fonts are loaded for Mapnik
-apt-get --assume-yes install ttf-dejavu-extra
-
-
-chown -R $USER_NAME:$USER_NAME "$USER_HOME/.gpsdrive"
-
-cp /usr/share/applications/gpsdrive.desktop "$USER_HOME/Desktop/"
-chown $USER_NAME:$USER_NAME "$USER_HOME/Desktop/gpsdrive.desktop"
-
-
-echo "Finished installing GpsDrive."
