59d462e67627e31664e78a911002a5c7be67a827
Stephan Meissl <stephan@meissl.name>
2012-06-28 19:32:27 +0000

    Correcting and extending EOxServer install script and overview.
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8070 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_eoxserver.sh
+++ bin/install_eoxserver.sh
@@ -21,20 +21,31 @@
 # =======
 # sudo ./install_eoxserver.sh
 
+
+echo "Starting EOxServer installation"
+
 if [ -z "$USER_NAME" ] ; then
    USER_NAME="user"
 fi
 USER_HOME="/home/$USER_NAME"
 DATA_DIR="$USER_HOME/gisvm/app-data/eoxserver"
+DOC_DIR="$USER_HOME/gisvm/app-data/eoxserver/doc"
 APACHE_CONF="/etc/apache2/conf.d/eoxserver"
 TMP_DIR=/tmp/build_eoxserver
+
+## check required tools are installed
+if [ ! -x "`which wget`" ] ; then
+   echo "ERROR: wget is required, please install it and try again" 
+   exit 1
+fi
+
 
 #Install packages
 apt-get update
 apt-get --assume-yes install gcc libgdal1-1.7.0 libgdal1-dev python-gdal \
     libxml2 python-libxml2 sqlite libsqlite3-dev python-lxml python-pip \
     cgi-mapserver python-mapscript python2.7 python2.7-dev \
-    libapache2-mod-wsgi libproj0 libproj-dev
+    libapache2-mod-wsgi libproj0 libproj-dev libgeos-dev libgeos++-dev
 
 if [ $? -ne 0 ] ; then
    echo 'ERROR: Package install failed! Aborting.'
@@ -42,14 +53,14 @@ if [ $? -ne 0 ] ; then
 fi
 
 
-# Install EOxServer
-pip install --upgrade eoxserver
-
-
 if [ ! -d "$TMP_DIR" ] ; then
   mkdir "$TMP_DIR"
 fi
 cd "$TMP_DIR"
+
+
+# Install EOxServer
+pip install --upgrade eoxserver==0.2.0
 
 
 # Adjust pysqlite installation (without define=SQLITE_OMIT_LOAD_EXTENSION)
@@ -66,7 +77,6 @@ EOF
 
 python setup.py install --force
 cd ..
-#rm pysqlite-2.6.3.tar.gz
 rm -r pysqlite-2.6.3
 
 # Install further dependencies
@@ -77,13 +87,17 @@ pip install --upgrade pyspatialite
 [ -d "$DATA_DIR" ] || mkdir -p "$DATA_DIR"
 cd "$DATA_DIR"
 if [ ! -d eoxserver_demonstration ] ; then
+    echo "Creating EOxServer demonstration instance"
     eoxserver-admin.py create_instance eoxserver_demonstration \
         --init_spatialite
     cd eoxserver_demonstration
+    # Configure logging
+    sed -e 's/#logging_level=/logging_level=INFO/' -i conf/eoxserver.conf
+    sed -e 's/DEBUG = True/DEBUG = False/' -i settings.py
     python manage.py syncdb --noinput
     # Download and register demonstration data
     wget -c --progress=dot:mega \
-       "http://eoxserver.org/export/1718/downloads/EOxServer_autotest-0.2.0.tar.gz"
+       "http://eoxserver.org/export/head/downloads/EOxServer_autotest-0.2.0.tar.gz"
     echo "Extracting demonstration data in `pwd`."
     tar -xzf EOxServer_autotest-0.2.0.tar.gz
     mv EOxServer_autotest-0.2.0/data/fixtures/* data/fixtures/
@@ -102,11 +116,11 @@ if [ ! -d eoxserver_demonstration ] ; then
     touch logs/eoxserver.log
     chown www-data logs/eoxserver.log data/ data/config.sqlite
     sed -e 's/http_service_url=http:\/\/localhost:8000\/ows/http_service_url=http:\/\/localhost\/eoxserver\/ows/' -i conf/eoxserver.conf
-    
 fi
 
 
-# Deploy instance in Apache2
+# Deploy demonstration instance in Apache
+echo "Deploying EOxServer demonstration instance"
 if [ ! -e "$DATA_DIR"/eoxserver_demonstration/wsgi.py ] ; then
     cat << EOF > "$DATA_DIR"/eoxserver_demonstration/wsgi.py
 import os
@@ -120,20 +134,40 @@ application = WSGIHandler()
 EOF
 fi
 
+# Add Apache configuration
 cat << EOF > "$APACHE_CONF"
 Alias /media /usr/local/lib/python2.7/dist-packages/django/contrib/admin/media
 Alias /static /usr/local/lib/python2.7/dist-packages/eoxserver/webclient/static
 Alias /eoxserver "$DATA_DIR/eoxserver_demonstration/wsgi.py"
 
+################################################################################
+#Restrict wsgi threads in order to run non thread safe code:
+WSGIDaemonProcess ows processes=10 threads=1
+WSGIProcessGroup ows
+################################################################################
+
 <Directory "$DATA_DIR/eoxserver_demonstration">
     AllowOverride None
     Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch
     AddHandler wsgi-script .py
+    ############################################################################
+    #Restrict wsgi threads in order to run non thread safe code:
+    WSGIRestrictProcess ows
+    SetEnv PROCESS_GROUP ows
+    ############################################################################
     Order allow,deny
     allow from all
 </Directory>
 EOF
 echo "Done"
+
+
+# Install desktop icon
+echo "Installing EOxServer icon"
+if [ ! -e "/usr/share/icons/eoxserver_60x60.logo.png" ] ; then
+   cp "$USER_HOME/gisvm/doc/images/project_logos/logo-eoxserver-3.png" \
+       /usr/share/icons/eoxserver_60x60.logo.png
+fi
 
 
 # Add Launch icon to desktop
@@ -144,15 +178,43 @@ Type=Application
 Encoding=UTF-8
 Name=EOxServer
 Comment=EOxServer
-Categories=Application;Education;Geography;
+Categories=Geospatial;Geoservers;
 Exec=firefox http://localhost/eoxserver/
-Icon=gnome-globe
+Icon=/usr/share/icons/eoxserver_60x60.logo.png
 Terminal=false
 StartupNotify=false
-Categories=Education;Geography;
 EOF
 fi
 cp /usr/share/applications/eoxserver.desktop "$USER_HOME/Desktop/"
+chown -R $USER_NAME.$USER_NAME "$USER_HOME/Desktop/eoxserver.desktop"
+
+
+# EOxServer Documentation
+echo "Getting EOxServer documentation"
+[ -d "$DOC_DIR" ] || mkdir "$DOC_DIR"
+cd "$DOC_DIR"
+wget -c "http://eoxserver.org/export/head/downloads/EOxServer_documentation-0.2.0.pdf" \
+  -O EOxServer_documentation-0.2.0.pdf
+ln -s EOxServer_documentation-0.2.0.pdf EOxServer_documentation.pdf
+ln -s "$DOC_DIR" /var/www/eoxserver-docs
+
+# Add Documentation Launch icon to desktop
+if [ ! -e /usr/share/applications/eoxserver-docs.desktop ] ; then
+   cat << EOF > /usr/share/applications/eoxserver-docs.desktop
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=EOxServer Documentation
+Comment=EOxServer Documentation
+Categories=Geospatial;Geoservers;
+Exec=evince "$DOC_DIR/EOxServer_documentation.pdf"
+Icon=/usr/share/icons/eoxserver_60x60.logo.png
+Terminal=false
+StartupNotify=false
+EOF
+fi
+cp -a /usr/share/applications/eoxserver-docs.desktop "$USER_HOME/Desktop/"
+chown -R $USER_NAME:$USER_NAME "$USER_HOME/Desktop/eoxserver-docs.desktop"
 
 
 # Reload Apache
@@ -160,5 +222,10 @@ cp /usr/share/applications/eoxserver.desktop "$USER_HOME/Desktop/"
 
 
 # Uninstall dev packages
-echo "TODO: *** UNINSTALL DEV PACKAGES ***"
+apt-get --assume-yes remove libgdal1-dev libsqlite3-dev python2.7-dev \
+    libproj-dev libgeos-dev libgeos++-dev
+apt-get --assume-yes autoremove
+rm "$TMP_DIR"/pysqlite-2.6.3.tar.gz
 
+
+echo "Finished EOxServer installation"
