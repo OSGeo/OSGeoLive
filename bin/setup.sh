ba8ac1f5609580301a72a5116b89500a33a1f278
Hamish Bowman <hamish_b yahoo com>
2012-06-28 01:16:18 +0000

    update linux-headers pkg versions
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8064 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/setup.sh
+++ bin/setup.sh
@@ -116,8 +116,9 @@ apt-get --assume-yes remove linux-image-2.6.38-8-generic
 # this will clear out 96mb (uncompressed), but users who want the
 # nvidia proprietary driver will need to reinstall it. ah well.
 #Version must be kept up to date (removes 3 packages)
-apt-get --assume-yes remove linux-headers-2.6.38-13
-apt-get --assume-yes remove linux-headers-2.6.38-11
+apt-get --assume-yes remove linux-headers-3.2.0-25
+apt-get --assume-yes remove linux-headers-3.2.0-24
+apt-get --assume-yes remove linux-headers-3.2.0-23
 
 
 # Remove unused home directories
