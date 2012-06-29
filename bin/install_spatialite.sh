7037fc3452611d3a1f95d677da2415554d29d84e
Micha Silver <micha@arava.co.il>
2012-06-29 21:56:26 +0000

    Updated install_spatialite.sh to compile spatialite_gui from source
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8072 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_spatialite.sh
+++ bin/install_spatialite.sh
@@ -20,7 +20,7 @@
 # =======
 # sudo ./install_spatialite.sh
 
-TMP="/tmp/build_spatialite"
+BUILD_TMP="/tmp/build_spatialite"
 INSTALL_FOLDER="/usr/local"
 DATA_FOLDER="/usr/local/share/data"
 PKG_DATA=$DATA_FOLDER/spatialite
@@ -46,32 +46,24 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 314DF160
 apt-get update
 
 ### setup temp ###
-mkdir -p "$TMP"
-cd "$TMP"
+mkdir -p "$BUILD_TMP"
+cd "$BUILD_TMP"
 
 ###########################
 ### Install from repo ###
-# Download and unpack spatialite ###
-
-## get spatialite
+## get spatialite cli and libs
 echo "Getting and installing spatialite"
 apt-get install --assume-yes spatialite-bin libspatialite3
 # The gui programs depend on libjpeg62
 apt-get install --assume-yes libjpeg62
 # Now the other stuff
-apt-get install --assume-yes librasterlite1 rasterlite-bin spatialite-gui
+# (except for spatialite-gui)
+apt-get install --assume-yes librasterlite1 rasterlite-bin # spatialite-gui
 
 ###########################
-### Compile from source ###
-# Only spatialite-gis needs compiling from source now
+### Download spatialite-gis from gaia-gis.it ###
 BASEURL=http://www.gaia-gis.it/spatialite-2.4.0
-#PACKAGES="
-# librasterlite-linux-x86-1.0.tar.gz
-# rasterlite-tools-linux-x86-1.0.tar.gz
-# spatialite-gis-linux-x86-1.0.0.tar.gz
-# spatialite-gui-linux-x86-1.3.0.tar.gz"
-PACKAGES="spatialite-gis-linux-x86-1.0.0.tar.gz
-	spatialite-gui-linux-x86-1.3.0.tar.gz"
+PACKAGES="spatialite-gis-linux-x86-1.0.0.tar.gz"
 for i in $PACKAGES; do
   dir=`basename $i .tar.gz`
   wget -c --progress=dot:mega "$BASEURL/$i"
@@ -80,6 +72,27 @@ for i in $PACKAGES; do
   cp -r "$dir"/* "$INSTALL_FOLDER"
   rm -rf "$dir"
 done
+
+
+##########################
+### Download and compile spatialite-gui 1.5.0 from source
+# First get dependencies
+DEV_PKGS="libwxgtk2.8-dev libgeos-dev libgeos++-dev libgeotiff-dev libcairo2-dev libfreexl-dev libspatialite-dev"
+apt-get --yes install $DEV_PKGS
+
+GAIA_URL="http://www.gaia-gis.it/gaia-sins"
+PACKAGES="libgaiagraphics-0.4b spatialite_gui-1.5.0-stable"
+for i in $PACKAGES; do
+	wget $GAIA_URL/$i.tar.gz
+	tar xzf $i.tar.gz
+	cd $i
+	./configure
+	make
+	make install-strip
+	ldconfig
+	cd ..
+done
+
 
 ##########################
 ### Sample data ###
@@ -93,27 +106,15 @@ then
 fi
 
 wget -N --progress=dot:mega "${OSGEO_URL}/${SQLITE_DB}"
-(cd "$PKG_DATA" && tar xzf "${TMP}/${SQLITE_DB}")
+(cd "$PKG_DATA" && tar xzf "${BUILD_TMP}/${SQLITE_DB}")
 
 chgrp -R users $PKG_DATA
 chmod -R g+w $PKG_DATA
 
 #############################
 ### GUI start icons ###
-cat << EOF > /usr/share/applications/spatialite-gui.desktop
-[Desktop Entry]
-Type=Application
-Encoding=UTF-8
-Name=SpatiaLite GUI
-Comment=SpatiaLite GUI
-Categories=Application;Geography;Geoscience;Education;
-Exec=spatialite-gui
-Icon=gnome-globe
-Terminal=false
-EOF
-
-cp -a /usr/share/applications/spatialite-gui.desktop "$USER_HOME/Desktop/"
-chown -R $USER_NAME:$USER_NAME "$USER_HOME/Desktop/spatialite-gui.desktop"
+cp $BUILD_TMP/spatialite_gui-1.5.0-stable/gnome_resource/spatialite-gui.desktop /usr/share/applications/
+cp $BUILD_TMP/spatialite_gui-1.5.0-stable/gnome_resource/spatialite-gui.png /usr/share/pixmaps
 
 cat << EOF > /usr/share/applications/spatialite-gis.desktop
 [Desktop Entry]
@@ -127,5 +128,9 @@ Icon=gnome-globe
 Terminal=false
 EOF
 
-cp -a /usr/share/applications/spatialite-gis.desktop "$USER_HOME/Desktop/"
-chown -R $USER_NAME:$USER_NAME "$USER_HOME/Desktop/spatialite-gis.desktop"
+#############################
+### Clean up
+cd ~
+rm -rf $BUILD_TMP
+apt-get --yes purge $DEV_PKGS
+
