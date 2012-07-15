da39c0f7842f42652dfa6f2e1bbe918157f233b0
Jorge Sanz <jsanz@gvsig.com>
2012-07-15 21:34:07 +0000

    added temporal dropbox url for the deb package
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8111 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_gvsig.sh
+++ bin/install_gvsig.sh
@@ -54,7 +54,7 @@ USER_HOME="/home/$USER_NAME"
 USER_DESKTOP="$USER_HOME/Desktop" 
 
 GVSIG_PACKAGE="gvsig_1.11-1305_i386_OSGeoLive.deb"
-GVSIG_PATH="http://FIXME"
+GVSIG_PACK="https://dl.dropbox.com/s/8h0lm1gewzvuage"
 # Notice to upload the gvSIG Package somwhere, when it's fixed
 # please, remove this two lines and let the script continue
 echo "FIX the path to download gvSIG deb package"
