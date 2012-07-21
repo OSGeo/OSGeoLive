fe2c85f180b30c66ca1711934b305aaa4c115552
Hamish Bowman <hamish_b yahoo com>
2012-07-21 06:02:25 +0000

    revive the wombat icon
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8174 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_main_docs.sh
+++ bin/install_main_docs.sh
@@ -110,9 +110,9 @@ if [ `grep -c 'localhost' /etc/firefox/pref/syspref.js` -eq 0 ] ; then
 fi
 
 #Alternative, just put an icon on the desktop that launched firefox and points to index.html
-\cp -f ../desktop-conf/arramagong-wombat-small.png  /usr/local/share/icons/
-#wget -nv  -O /usr/local/share/icons/arramagong-wombat-small.png \
-#  "http://svn.osgeo.org/osgeo/livedvd/artwork/backgrounds/arramagong/arramagong-wombat-small.png"
+mkdir -p /usr/local/share/icons
+cp -f ../desktop-conf/arramagong-wombat-small.png  /usr/local/share/icons/
+
 
 #What logo to use for launching the help?
 # HB: IMO wombat roadsign is good- it says "look here" and is friendly
