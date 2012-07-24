acfc6933ae931f3e6de553db63fb124a9ffb113f
Jinsongdi Yu <j.yu@jacobs-university.de>
2012-07-24 14:30:56 +0000

    use start_rasdaman.sh for rasmgr.conf -host setting instead of rc.local
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8312 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_rasdaman.sh
+++ bin/install_rasdaman.sh
@@ -23,7 +23,7 @@
 #
 
 # rasdaman src to be used
-VERSION=8.3.0
+VERSION=8.3.1
 RASDAMAN_LOCATION="http://kahlua.eecs.jacobs-university.de/~earthlook/osgeo"
 RASDAMAN_TARBALL="rasdaman-$VERSION.tar.gz"
 
@@ -323,14 +323,4 @@ cp /usr/share/applications/stop_rasdaman_server.desktop "$USER_HOME/Desktop/"
 cp /usr/share/applications/start_rasdaman_server.desktop "$USER_HOME/Desktop/"
 cp /usr/share/applications/rasdaman-earthlook-demo.desktop "$USER_HOME/Desktop/"
 
-
-### rasmgr.conf wants the hostname to be defined at build time, but the hostname on our
-###   ISO and VM are different ('user' vs 'osgeo-live'). so we have to re-set the value
-###   at boot time.
-if [ `grep -c 'rasdaman' /etc/rc.local` -eq 0 ] ; then
-    sed -i -e 's|exit 0||' /etc/rc.local
-    echo 'sed -i -e "s/ -host [^ ]*/ -host `hostname`/" /usr/local/rasdaman/etc/rasmgr.conf' >> /etc/rc.local
-    echo >> /etc/rc.local
-    echo "exit 0" >> /etc/rc.local
-fi
 
