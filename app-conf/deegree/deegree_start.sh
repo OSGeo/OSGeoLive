4a7fd37bcd01f294b4227c0bda2ca3c8ab2f5571
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-07-26 17:32:05 +0000

    adding deegree start script
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8374 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- app-conf/deegree/deegree_start.sh
+++ app-conf/deegree/deegree_start.sh
@@ -8,19 +8,16 @@
 ##
 #########################
 
+export DEEGREE_WORKSPACE_ROOT="/usr/local/share/deegree"
 DEE_DIR="/usr/local/lib/deegree-webservices-3.2-pre9"
 
-## stop system's tomcat (port 8080 conflict)
-#echo "user" | sudo -S /etc/init.d/tomcat6 stop
-#sleep 2
-
 ## start tomcat (and deegree webapps)
-sudo bash -c "$DEE_DIR/bin/catalina.sh run" &
+cd $DEE_DIR
+mkdir -p logs
+./bin/catalina.sh start
 
 ## sleep for 5 sec, due to the tomcat hasn't started yet
 sleep 5
 
 ## open firefox with deegree 3 console
-## open as user "user" to avoid problems with already running instances of firefox
-sudo -u user firefox -new-tab http://localhost:8033/deegree-webservices
-
+firefox -new-tab http://localhost:8033
