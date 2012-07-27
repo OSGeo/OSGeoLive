0e2429b80b4fc8f0103d085f6908c56b6751b661
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-07-27 11:26:33 +0000

    fixing permissions for rasdaman rasmgr.conf that has to be writable
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8397 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_rasdaman.sh
+++ bin/install_rasdaman.sh
@@ -191,9 +191,9 @@ sed -i -e 's/OPTIONS="-w"/OPTIONS="-w -i"/' /etc/init.d/rpcbind
 /etc/init.d/rpcbind restart
 # needed to set the host name if it's empty
 # sed -i -e "s/ -host [^ ]*/ -host `hostname`/" $RASDAMAN_HOME/etc/rasmgr.conf
-chgrp "GROUP_NAME" "$RASDAMAN_HOME"/etc/
-chmod g+w "$RASDAMAN_HOME"/etc/
-chmod 774 "$RASDAMAN_HOME"/etc/rasmgr.conf
+#chgrp "GROUP_NAME" "$RASDAMAN_HOME"/etc/
+chmod 777 "$RASDAMAN_HOME"/etc/
+chmod 777 "$RASDAMAN_HOME"/etc/rasmgr.conf
 su - "$USER_NAME" "$RASDAMAN_HOME"/bin/stop_rasdaman.sh
 su - "$USER_NAME" "$RASDAMAN_HOME"/bin/start_rasdaman.sh
 
