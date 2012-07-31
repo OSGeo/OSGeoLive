3d774d294c56a4264a87923c8655256323cf3409
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-07-31 23:32:55 +0000

    Added more comments in chroot method
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8593 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/build_chroot.sh
+++ bin/build_chroot.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#################################################
+#############################################################################
 # 
 # Purpose: Creating OSGeoLive as an Ubuntu customization
 # 	   https://help.ubuntu.com/community/LiveCDCustomization
 # Authors: Alex Mandel <tech_dev@wildintellect.com>
 #	   Angelos Tzotsos <tzotsos@gmail.com>
 #
-#################################################
+#############################################################################
 # Copyright (c) 2010 Open Source Geospatial Foundation (OSGeo)
 #
 # Licensed under the GNU LGPL.
@@ -20,7 +20,17 @@
 # See the GNU Lesser General Public License for more details, either
 # in the "LICENSE.LGPL.txt" file distributed with this software or at
 # web page "http://www.fsf.org/licenses/lgpl.html".
-##################################################
+#############################################################################
+
+# System Requirements
+#
+#     1. At least 3-5 GB of free space
+#     2. At least 512 MB RAM and 1 GB swap (recommended)
+#     3. squashfs-tools
+#     4. genisoimage, which provides mkisofs
+#     5. An Ubuntu kernel with squashfs support (present in Ubuntu 6.06 and later)
+#     6. QEMU/KVM, VirtualBox or VMware for testing (optional) 
+#
 
 #DIR=`dirname ${0}`
 #VERSION=`cat "$DIR"/../VERSION.txt`
@@ -33,49 +43,61 @@ sudo apt-get install squashfs-tools genisoimage
 #TODO add wget to grab a fresh image, optional
 
 #Stuff to be done the 1st time, should already be in place for additional builds
+#Download into an empty directory 
 mkdir ~/livecdtmp
 cd ~/livecdtmp
 #mv ubuntu-9.04-desktop-i386.iso ~/livecdtmp
 wget http://se.archive.ubuntu.com/mirror/cdimage.ubuntu.com/xubuntu/releases/12.04/release/xubuntu-12.04-desktop-i386.iso
-#mv xubuntu-10.04-desktop-i386.iso ~/livecdtmps
-
-mkdir mnt
 
 #Start with a fresh copy
+#Mount the Desktop .iso
+mkdir mnt
 sudo mount -o loop xubuntu-12.04-desktop-i386.iso mnt
 
+#Extract .iso contents into dir 'extract-cd' 
 mkdir extract-cd
 rsync --exclude=/casper/filesystem.squashfs -a mnt/ extract-cd
 
+#Extract the SquashFS filesystem 
 sudo unsquashfs mnt/casper/filesystem.squashfs
 #Does the above need to be done every time or can it be done once, and then just make a fresh copy of the chroot for each builds
 sudo mv squashfs-root edit
 
+#If you need the network connection within chroot 
 sudo cp /etc/resolv.conf edit/etc/
 sudo cp /etc/hosts edit/etc/
 
+#These mount important directories of your host system - if you later decide to delete the edit/ directory,
+#then make sure to unmount before doing so, otherwise your host system will become unusable at least 
+#temporarily until reboot
 sudo mount --bind /dev/ edit/dev
+#NOW IN CHROOT
 sudo chroot edit
 mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t devpts none /dev/pts
 
+#To avoid locale issues and in order to import GPG keys 
 export HOME=/roots
 export LC_ALL=C
 
-
+#In 9.10, before installing or upgrading packages you need to run
+#TODO: Check/ask if this needs to be done in 12.04
 dbus-uuidgen > /var/lib/dbus/machines-id
 dpkg-divert --local --rename --add /sbin/initctl
 ln -s /bin/true /sbin/initctl
 
-#Execute the build
+#To view installed packages by size
+#dpkg-query -W --showformat='${Installed-Size}\t${Package}\n' | sort -nr | less
+#When you want to remove packages remember to use purge 
+#aptitude purge package-name
+
+#Execute the osgeolive build
 cd /tmp/
 wget https://svn.osgeo.org/osgeo/livedvd/gisvm/trunk/bin/bootstrap.sh
 chmod a+x bootstrap.sh
 ./bootstrap.sh
-cd /usr/local/share/gisvm
-svn update -r 8413
-cd bin
+cd /usr/local/share/gisvm/bin
 ./main.sh 2>&1 | tee /var/log/osgeolive/main_install.log
 
 #After the build
@@ -83,27 +105,37 @@ cd bin
 awk -F: '$3 > 999' /etc/passwd
 
 #Cleanup
+#Be sure to remove any temporary files which are no longer needed, as space on a CD is limited
 apt-get clean
+#Or delete temporary files
 rm -rf /tmp/* ~/.bash_history
+#Or delete hosts file 
 rm /etc/hosts
+#Or nameserver settings 
 rm /etc/resolv.conf
+#If you installed software, be sure to run 
 rm /var/lib/dbus/machine-id
 rm /sbin/initctl
 dpkg-divert --rename --remove /sbin/initctl
+#now umount (unmount) special filesystems and exit chroot 
 umount /proc || umount -lf /proc
 umount /sys
 umount /dev/pts
 exit
+#OUT OF CHROOT
 sudo umount edit/dev
+
+#compress osgeolive build logs
 tar czf osgeo-live-${VERSION}-log.tar.gz -C edit/var/log osgeolive
 
 #remaster the dvd
+#Regenerate manifest 
 chmod +w extract-cd/casper/filesystem.manifest
 sudo chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > extract-cd/casper/filesystem.manifest
 sudo cp extract-cd/casper/filesystem.manifest extract-cd/casper/filesystem.manifest-desktop
 sudo sed -i '/ubiquity/d' extract-cd/casper/filesystem.manifest-desktop
 sudo sed -i '/casper/d' extract-cd/casper/filesystem.manifest-desktop
-
+#Compress filesystem
 sudo rm extract-cd/casper/filesystem.squashfs
 sudo mksquashfs edit extract-cd/casper/filesystem.squashfs
 
@@ -112,12 +144,14 @@ sudo mksquashfs edit extract-cd/casper/filesystem.squashfs
 chmod +w extract-cd/casper/filesystem.size
 printf $(sudo du -sx --block-size=1 edit | cut -f1) > extract-cd/casper/filesystem.size
 chmod -w extract-cd/casper/filesystem.size
+
+#this is now compressed in squashfs so we delete to save VM disk space
 cd ~/livecdtmp
 sudo rm -rf edit
 
 #Set an image name in extract-cd/README.diskdefines
 #KVM VNC doesn't pass ctrl, can't use vim or nano
-#Can probably use sed magic
+#Can probably use sed magic or copy a predefined file from gisvm/app-data
 sudo nano extract-cd/README.diskdefines
 
 #Remove old md5sum.txt and calculate new md5 sums
