7e4cc27426295d488920731e1f7e35cfd8a4eda6
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-08-08 15:36:49 +0000

    testing live session password fix
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8855 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/inchroot_debug.sh
+++ bin/inchroot_debug.sh
@@ -81,58 +81,58 @@ export USER_NAME
 ./install_apache2.sh
 ./install_tomcat6.sh
 
-# ./install_geoserver.sh
-# ./install_geomajas.sh
-# ./install_geonetwork.sh
-# ./install_deegree.sh
-# ./install_52nWPS.sh
-# ./install_kosmo.sh
-# ./install_udig.sh
-# ./install_openjump.sh
-# ./install_postgis.sh
-# ./install_osm.sh
-# ./load_postgis.sh
-# ./install_pgrouting.sh
-# ./install_sahana.sh
-# ./install_ushahidi.sh
-# ./install_mapserver.sh
-# ./install_mapbender.sh
-# ./install_geokettle.sh
-# ./install_gmt.sh
-# ./install_grass.sh
-# ./install_qgis.sh
-# ./install_qgis_mapserver.sh
-# ./install_saga.sh
-# ./install_mapnik.sh
-# ./install_mapproxy.sh
-# ./install_maptiler.sh
-# ./install_marble.sh
-# ./install_opencpn.sh
-# ./install_prune.sh
-# ./install_viking.sh
-# ./install_zygrib.sh
-# ./install_geopublisher.sh
-# ./install_atlasstyler.sh
-# ./install_liblas.sh
-# ./install_gpsdrive.sh
-# ./install_mapfish.sh
-# ./install_openlayers.sh
-# ./install_R.sh
-# ./install_ossim.sh
-# ./install_osgearth.sh
-# ./install_spatialite.sh
-# ./install_zoo-project.sh
-# ./install_52nSOS.sh
-# ./install_otb.sh
-# # ./install_rasdaman.sh
-# ./install_tinyows.sh
-# ./install_52nWSS.sh
-# ./install_pycsw.sh
-# ./install_geomoose.sh
-# ./install_mb-system.sh
-# ./install_gvsig.sh
+./install_geoserver.sh
+./install_geomajas.sh
+./install_geonetwork.sh
+./install_deegree.sh
+./install_52nWPS.sh
+./install_kosmo.sh
+./install_udig.sh
+./install_openjump.sh
+./install_postgis.sh
+./install_osm.sh
+./load_postgis.sh
+./install_pgrouting.sh
+./install_sahana.sh
+./install_ushahidi.sh
+./install_mapserver.sh
+./install_mapbender.sh
+./install_geokettle.sh
+./install_gmt.sh
+./install_grass.sh
+./install_qgis.sh
+./install_qgis_mapserver.sh
+./install_saga.sh
+./install_mapnik.sh
+./install_mapproxy.sh
+./install_maptiler.sh
+./install_marble.sh
+./install_opencpn.sh
+./install_prune.sh
+./install_viking.sh
+./install_zygrib.sh
+./install_geopublisher.sh
+./install_atlasstyler.sh
+./install_liblas.sh
+./install_gpsdrive.sh
+./install_mapfish.sh
+./install_openlayers.sh
+./install_R.sh
+./install_ossim.sh
+./install_osgearth.sh
+./install_spatialite.sh
+./install_zoo-project.sh
+./install_52nSOS.sh
+./install_otb.sh
+# ./install_rasdaman.sh
+./install_tinyows.sh
+./install_52nWSS.sh
+./install_pycsw.sh
+./install_geomoose.sh
+./install_mb-system.sh
+./install_gvsig.sh
 
-# ./load_gisdata.sh
+./load_gisdata.sh
 ./install_main_docs.sh
 ./install_edutools.sh
 
