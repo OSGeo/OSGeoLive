f5d3f50ba619806e4f3c13f9dd82934da5335bad
Hamish Bowman <hamish_b yahoo com>
2012-08-11 04:54:27 +0000

    update to modern versions, comment out remastersys dir
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8945 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/load_win_installers.sh
+++ bin/load_win_installers.sh
@@ -26,8 +26,8 @@
 # Requires: nothing
 
 #Add the files to the directory where remastersys wants them
-#TMP="/tmp/build_win_installers"
-TMP="/tmp/remastersys/ISOTMP/WindowsInstallers"
+TMP="/tmp/build_win_installers"
+#TMP="/tmp/remastersys/ISOTMP/WindowsInstallers"
 mkdir -p $TMP
 cd $TMP
 
@@ -54,7 +54,7 @@ applications. Visit http://trac.osgeo.org/osgeo4w/ for more information.
 vcredist_x86.exe is the  Runtime from Microsoft, it is required for many
 of the other applications to work.
 
-GeoServer:  geoserver-2.0.2-bin.zip in the Windows Installers cache also
+GeoServer:  geoserver-2.1.4-bin.zip in the Windows Installers cache also
 works on a Mac.
 
 Happy Mapping!
@@ -64,14 +64,20 @@ EOF
 for URL in \
   http://download.osgeo.org/osgeo4w/osgeo4w-setup.exe \
   http://forge.osor.eu/frs/download.php/1570/gvSIG-1_11-1305-final-win-i586-j1_5.exe \
-  http://www.qgis.org/downloads/QGIS-OSGeo4W-1.7.3-00624b3-Setup.exe \
+  http://qgis.org/downloads/QGIS-OSGeo4W-1.8.0-1-Setup.exe \
   http://home.gdal.org/tmp/vcredist_x86.exe \
-  http://grass.osgeo.org/grass64/binary/mswindows/native/WinGRASS-6.4.1-1-Setup.exe \
-  http://download.osgeo.org/livedvd/data/gpsbabel/GPSBabel-1.4.2-Setup.exe \
+  http://grass.osgeo.org/grass64/binary/mswindows/native/WinGRASS-6.4.2-2-Setup.exe \
+  http://download.osgeo.org/livedvd/data/gpsbabel/GPSBabel-1.4.3-Setup.exe \
   http://gpsvp.googlecode.com/files/gpsVPxp_0.4.24.zip \
-  http://downloads.sourceforge.net/project/opencpn/opencpn/2.5.0/opencpn_2.5.0_setup.exe?use_mirror=cdnetworks-us-2 \
+  http://downloads.sourceforge.net/project/opencpn/opencpn/3.0.2/opencpn_3.0.2_setup.exe?use_mirror=cdnetworks-us-2 \
   http://download.osgeo.org/ossim/installers/windows/ossimplanet-installer-1.8.4.exe \
   http://sourceforge.net/projects/saga-gis/files/SAGA%20-%202.0/SAGA%202.0.8/saga_2.0.8_win32_setup.exe?use_mirror=cdnetworks-us-2 \
+  http://www.opensig.es/public/kosmo/v_2.0.1/binaries/kosmo_desktop_2.0.1_windows_jre_setup.exe \
+  http://udig.refractions.net/files/downloads/udig-1.3.1.win32.win32.x86.exe \
+  http://download.osgeo.org/livedvd/data/zygrib/zyGrib_win-6.0.0.zip \
+  http://github.com/downloads/mapbox/tilemill/TileMill-v0.9.1-Setup.exe \
+  http://download.osgeo.org/livedvd/data/mapwindow/MapWindowx86Full-v48Final-installer.exe \
+  http://downloads.sourceforge.net/geoserver/geoserver-2.1.4-bin.zip \
 ; do
   # sourceforge filename sanitation:
   OUTFILE=`basename "$URL" | cut -f1 -d'?'`
@@ -79,19 +85,13 @@ for URL in \
   wget -c --progress=dot:mega "$URL" -O "$OUTFILE"
 done;
 
-wget -c --progress=dot:mega "http://download.codeplex.com/Download?ProjectName=mapwindow4&DownloadId=300267&FileTime=129649216070600000&Build=18416" -O "MapWindow.exe"
-
 #Disabled because they are very outdated
 # http://maptiler.googlecode.com/files/maptiler-1.0-beta2-setup.exe
 # http://home.gdal.org/fwtools/FWTools247.exe \
 
 #FIXME:
 #\mv "plan9.php?dl=gpsbabel-1.3.6.zip" gpsbabel-1.3.6.zip
-
-#( sorry udig and kosmo, no space :-( )
-#  http://www.kosmoland.es/public/kosmo/v_2.0/binaries/Kosmo_Desktop_2.0_windows_jre.zip \
-#  http://udig.refractions.net/files/downloads/udig-1.1.1.exe \
-
+# MapWindow also obfuscates the download URL.
 
   # FWTools
   # geonetwork
