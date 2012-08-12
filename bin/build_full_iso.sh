2a6a26548780a0e0996ff3cb6fd393715a54b38b
Alex Mandel <tech@wildintellect.com>
2012-08-12 06:07:14 +0000

    Draft script to build a full iso based on a mini
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@8989 7da46fd8-bd2a-0410-a152-cf9810cf5b10

---
+++ bin/build_full_iso.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+#############################################################################
+# 
+# Purpose: Creating OSGeoLive as an Ubuntu customization
+# 	   https://help.ubuntu.com/community/LiveCDCustomization
+# Authors: Alex Mandel <tech_dev@wildintellect.com>
+#
+#
+#############################################################################
+# Copyright (c) 2010 Open Source Geospatial Foundation (OSGeo)
+#
+# Licensed under the GNU LGPL.
+# 
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+#############################################################################
+
+# System Requirements
+#
+#     1. At least 10 GB of free space
+#     2. At least 512 MB RAM and 1 GB swap (recommended)
+#     4. genisoimage, which provides mkisofs
+#     6. QEMU/KVM, VirtualBox or VMware for testing (optional) 
+#
+DIR="/home/user/gisvm/bin"
+SVN_DIR="/home/user/gisvm"
+VERSION=`cat "$DIR"/../VERSION.txt`
+PACKAGE_NAME="osgeo-live"
+cd "$SVN_DIR"
+#REVISION=`svn info | grep "Revision" | sed 's/Revision: //'`
+
+#Is it a public or an internal build?
+MINI_ISO_NAME="$PACKAGE_NAME-mini-$VERSION"
+ISO_NAME="$PACKAGE_NAME-$VERSION"
+
+echo
+echo "Installing genisoimage"
+echo "==================================="
+
+sudo apt-get install genisoimage
+
+
+echo
+echo "Extract mini iso"
+echo "==================================="
+
+mkdir -p ~/livecdtmp
+cd ~/livecdtmp
+
+#Start with a fresh copy
+#Mount the Desktop .iso to mnt2 to not interfere with other builds
+mkdir mnt2
+sudo mount -o loop "$MINI_ISO_NAME.iso" mnt2
+
+#Extract .iso contents into dir 'extract-cd' 
+mkdir "extract-cd"
+rsync -a mnt2/ "extract-cd"
+
+echo
+echo "Download Windows and Mac Installers"
+echo "==================================="
+
+cd extract-cd
+./load_win_installers.sh
+./load_mac_installers.sh
+cd ..
+
+echo
+echo "Regenerating manifest..."
+echo "======================================"
+#Regenerate manifest 
+chmod +w extract-cd/casper/filesystem.manifest
+sudo chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > \
+   extract-cd/casper/filesystem.manifest
+sudo cp extract-cd/casper/filesystem.manifest \
+   extract-cd/casper/filesystem.manifest-desktop
+sudo sed -i '/ubiquity/d' \
+   extract-cd/casper/filesystem.manifest-desktop
+sudo sed -i '/casper/d' \
+   extract-cd/casper/filesystem.manifest-desktop
+
+echo
+echo "Calculating new filesystem size..."
+echo "======================================"
+#Update the filesystem.size file, which is needed by the installer:
+# TODO: get it to run as sudo no sudo su
+chmod +w extract-cd/casper/filesystem.size
+printf $(sudo du -sx --block-size=1 edit | cut -f1) > \
+   extract-cd/casper/filesystem.size
+chmod -w extract-cd/casper/filesystem.size
+
+
+echo
+echo "Calculating new md5 sums..."
+echo "======================================"
+#Remove old md5sum.txt and calculate new md5 sums
+cd extract-cd
+sudo rm md5sum.txt
+find -type f -print0 | sudo xargs -0 md5sum | \
+   grep -v isolinux/boot.cat | sudo tee md5sum.txt
+
+echo
+echo "Creating iso..."
+echo "======================================"
+#Create the ISO image
+sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -b \
+   isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
+   -boot-load-size 4 -boot-info-table -o ../"$ISO_NAME.iso" .
+
