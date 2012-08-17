20998977f96f2e8a74032ad1f8299c8686e08814
Alex Mandel <tech@wildintellect.com>
2012-08-17 23:14:40 +0000

    Fixing space after _
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@9075 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/build_full_iso.sh
+++ bin/build_full_iso.sh
@@ -31,7 +31,7 @@
 #
 # Checkout a copy of the svn, change to the bin folder
 # Run as 
-# sudo ./build_full_iso.sh /path/to/iso 2>&1 | tee ~/build_full_iso.log
+# sudo ./build_full_iso.sh /path/to/file.iso 2>&1 | tee ~/build_full_iso.log
 # Expects 1 argument, the path to the mini iso
 
 #DIR="/home/user/gisvm/bin"
