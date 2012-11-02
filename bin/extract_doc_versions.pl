25f0c7e265a0d06fd2b7f1a109df92f3d0a243df
camerons <cameron.shorter@gmail.com>
2012-11-02 21:05:02 +0000

    Now have a summary table being generated
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@9240 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/extract_doc_versions.pl
+++ bin/extract_doc_versions.pl
@@ -30,108 +30,120 @@ use strict;
 use warnings;
 use File::Basename;
 
+# initialise variables
+my $osgeolive_docs_url="http://adhoc.osgeo.osuosl.org/livedvd/docs/";
+my %svninfo;
+my $line;
+
 # Store the script root directory for later
 my $scriptDir = dirname($0);
 
 # cd to the svn document directory
 #chdir("$scriptDir/../doc");
 
-#my $pwd=cwd();
-#print("pwd=",$pwd);
-#cd "$scriptDir";
-#print "Fred:",`pwd`;
-
-#my @lines = split /\n/, $svnlist;
-#print @lines;
-
-
-# Store the script root directory for later
-#cd `dirname ${0}`
-#scriptDir=`pwd`
-
-# cd to the svn document directory
-#cd ${scriptDir}/../doc
-
-#echo "dir/file,docname,version,directory,language,username,date,project,last updated:" `date`
-
 #my @svnlist = split(/\n/, `svn list -v -R`);
-#my @svnlist = split(/\n/, `cat list.txt`);
-my @svnlist = split(/\n/, `cat short.txt`);
-my $line;
+my @svnlist = split(/\n/, `cat list.txt`);
+
 foreach (@svnlist) {
+  my $line2=$_;
   if (
-    m/.rst$/ # Only look at rst source docs
+    m#/# # Ignore files in the root directory
+    && m/.rst$/ # Only look at rst source docs
     && ! m/template_/ # The template files have been removed from subversion, but are still pickedup by svn list.
-    && ! m/ index.rst$/ #Ignore doc/index.rst which is just an autoforward to doc/en/index.rst
   ) {
     $line = $_;
-    $line =~ s/en/enn/;
-    #&& s#\( [^/]*\)/\([^/]*$\)#\1/./\2# #Change en/contact.rst to en/./contact.rst (so all files have 2 dirs)
-#Insert space delimiters between dirs
-# | sed -e's#/# #g' \
-#Reorder and insert commas
-# | awk '{print $7"/"$8"/"$9","$9","$1","$8","$7","$2","$9,",",$4," ",$5," ",$6}' \
-# change . to "." so that it is read as a string instead of number
-# | sed -e's/,\.,/,".",/' \
-#Extract project name from filename
-# | sed -e's/_overview.rst$//' \
-# | sed -e's/_quickstart.rst$//' \
-# | sed -e's/.rst$//' \
-    print "$line\n";
+
+    #Change en/contact.rst to en/./contact.rst (so all files have 2 dirs)
+    $line =~ s#( [^/]*)/([^/]*$)#$1/./$2#;
+
+    #Insert space delimiters between dirs
+    $line =~ s#/# #g;
+
+    my @args = split /\s+/, $line;
+    my $lang=$args[7];
+    my $dir_file="$args[8]/$args[9]";
+
+    # Extract info into a hash array
+    $svninfo{$lang}{$dir_file}{"version"}=$args[1];
+    $svninfo{$lang}{$dir_file}{"author"}=$args[2];
+    $svninfo{$lang}{$dir_file}{"date"}="$args[4] $args[5] $args[6] $args[7]";
+    $svninfo{$lang}{$dir_file}{"dir"}=$args[8];
+    $svninfo{$lang}{$dir_file}{"file"}=$args[9];
+
+    #my $index=0;
+    #foreach (@args) {
+    #  print $index++,"->",$_," ";
+    #}
+    #print "\n";
   }
 }
-exit
 
-##cat list.txt \
-#List doc svn version numbers
-#  svn list -v -R \
-#Only look at rst source docs
-# | grep ".rst$" \
-# The template files have been removed from subversion, but are still picked
-# up by svn list.
-# | grep -v "/template_" \
-#Ignore doc/index.rst which is just an autoforward to doc/en/index.rst
-# | grep -v " index.rst$" \
-#Ignore directories
-# | grep -v "/$"  \
-#Change en/contact.rst to en/./contact.rst (so all files have 2 dirs)
-# | sed -e's#\( [^/]*\)/\([^/]*$\)#\1/./\2#' \
-#Insert space delimiters between dirs
-# | sed -e's#/# #g' \
-#Reorder and insert commas
-# | awk '{print $7"/"$8"/"$9","$9","$1","$8","$7","$2","$9,",",$4," ",$5," ",$6}' \
-# change . to "." so that it is read as a string instead of number
-# | sed -e's/,\.,/,".",/' \
-#Extract project name from filename
-# | sed -e's/_overview.rst$//' \
-# | sed -e's/_quickstart.rst$//' \
-# | sed -e's/.rst$//' \
+# print 
+#foreach my $lang (keys %svninfo) {
+#  foreach my $dir_file (keys $svninfo{$lang}) {
+#    print "$lang ";
+#    print "$dir_file ";
+#    print "$svninfo{$lang}{$dir_file}{'version'} ";
+#    print "$svninfo{$lang}{$dir_file}{'author'} ";
+#    print "$svninfo{$lang}{$dir_file}{'date'} ";
+#    #print "$svninfo{$lang}{$dir_file}{'dir'} ";
+#    #print "$svninfo{$lang}{$dir_file}{'file'} ";
+#    print "\n";
+#  }
+#}
+#
+#print "--------------\n";
 
+# print: dir/file en_version languages ... 
+# print: total translated
+# print: total partial
+# print: total complete
+#http://trac.osgeo.org/osgeo/changeset?new=9055%40livedvd%2Fgisvm%2Ftrunk%2Fdoc%2Fde%2Foverview%2F52nSOS_overview.rst&old=9054%40livedvd%2Fgisvm%2Ftrunk%2Fdoc%2Fde%2Foverview%2F52nSOS_overview.rst
 
-#svn list -v -R \
-#  | grep ".rst$" \
-#  | grep -v "/template_" \
-#  | grep -v " index.rst$" \
-#  | grep -v "/$"  \
-#  | sed -e's#\( [^/]*\)/\([^/]*$\)#\1/./\2#' \
-#  | sed -e's#/# #g' \
-#  | awk '{print $8"/"$9"/"$10","$10","$1","$9","$8","$2",",$4,$5,$6,$7","$10}' \
-#  | sed -e's/,\.,/,".",/' \
-#  | sed -e's/_overview.rst$//' \
-#  | sed -e's/_quickstart.rst$//' \
-#  | sed -e's/.rst$//' \
+&printhtml;
 
-# cd to the svn bin directory
-#cd ${scriptDir}
+sub printhtml() {
 
-#echo "INSTALL SCRIPTS:"
-#echo "dir/file,docname,version,directory,language,username,date,project,last updated:" `date`
+  print "<table border='1'>\n";
+  print "<tr><th>dir/file</th><th>en</th>";
+  foreach my $lang (sort keys %svninfo) {
+    $lang =~ /en/ && next;
+    print "<th>$lang</th>";
+  }
+  print "</tr>\n";
 
-#cat list2.txt \
-#ols line was | awk '{print "bin/"$7","$7","$1",bin,,"$2","$4" "$5" "$6","$7}' \
-#svn list -v -R \
-#  | grep -v "/$"  \
-#  | awk '{print "bin/"$8","$8","$1",bin,,"$2","$4" "$5" "$6,$7","$8}' \
-#  | sed -e's#,install_\([^,]*\).sh$#,\1#' \
-#  | sed -e's/,\.,/,".",/' \
-#  #| sed -e's#\(install_\)\([^,]*[.sh$\)#\2#' \
+  # loop through filenames
+  foreach my $dir_file (sort keys $svninfo{"en"}) {
+
+    # print file/dir and url
+    my $html_file=$svninfo{'en'}{$dir_file}{'file'};
+    $html_file=~s#.rst$#.html#;
+    print "<tr><td>";
+    print "<a href='$osgeolive_docs_url/en/";
+    print "$svninfo{'en'}{$dir_file}{'dir'}/$html_file'>";
+    print "$dir_file</a></td>";
+
+    # print english version
+    print "<td>$svninfo{'en'}{$dir_file}{'version'}</td>";
+
+    # loop through languages
+    foreach my $lang (sort keys %svninfo) {
+      $lang =~ /en/ && next;
+
+      # print language's version
+      print "<td>";
+      if (exists $svninfo{$lang}{$dir_file} ) {
+        if ($svninfo{$lang}{$dir_file}{'version'} >= $svninfo{"en"}{$dir_file}{'version'}) {
+          print '<font color="green">';
+        }else{
+          print '<font color="orangered">';
+        }
+        print "$svninfo{$lang}{$dir_file}{'version'}";
+        print "</font>";
+      }
+      print "</td>";
+    }
+    print "</tr>\n";
+  }
+  print "\n</table>\n";
+}
