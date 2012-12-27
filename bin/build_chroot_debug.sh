03d864f4725eed8f6d218b2cc08b52e7bd1dde19
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2012-12-27 14:14:07 +0000

    build version has to be shown in plymouth for debug builds
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@9493 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/build_chroot_debug.sh
+++ bin/build_chroot_debug.sh
@@ -158,14 +158,14 @@ cp ../../gisvm/app-conf/build_chroot/casper.conf etc/casper.conf
 sed -i -e 's/U6aMy0wojraho/eLyJdzDtonrIc/g' scripts/casper-bottom/25adduser
 
 #Change the text on the loader
-sed -i -e "s/title=Xubuntu $UBU_RELEASE/title=OSGeo Live $VERSION/g" \
+sed -i -e "s/title=Xubuntu $UBU_RELEASE/title=OSGeo Live build$REVISION/g" \
    lib/plymouth/themes/xubuntu-text/xubuntu-text.plymouth
 #might be in this file
-sed -i -e "s/title=Xubuntu $UBU_RELEASE/title=OSGeo Live $VERSION/g" \
+sed -i -e "s/title=Xubuntu $UBU_RELEASE/title=OSGeo Live build$REVISION/g" \
    lib/plymouth/themes/text.plymouth
 
 #Optional change it in the .disk/info too
-sed -i -e "s/title=Xubuntu $UBU_RELEASE/title=OSGeo Live $VERSION/g" extract-cd/.disk/info
+sed -i -e "s/title=Xubuntu $UBU_RELEASE/title=OSGeo Live build$REVISION/g" extract-cd/.disk/info
 #copy in a different background
 cp ../../gisvm/desktop-conf/osgeo-desktop.png \
    lib/plymouth/themes/xubuntu-logo/xubuntu-greybird.png
