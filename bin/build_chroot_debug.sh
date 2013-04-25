3efab8c0346da731061788989b818bc319ddd23c
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2013-04-25 16:06:28 +0000

    mkiso bug fix: -b is followed by a binary file so -quiet had to move
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@10113 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/build_chroot_debug.sh
+++ bin/build_chroot_debug.sh
@@ -251,7 +251,7 @@ echo
 echo "Creating iso..."
 echo "======================================"
 #Create the ISO image
-sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -b -quiet \
+sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -quiet -b \
    isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
    -boot-load-size 4 -boot-info-table -o ../"$ISO_NAME.iso" .
 
