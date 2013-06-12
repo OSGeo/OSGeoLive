c321bcbb0a893376e648677de300b5d617bf8614
Edgar Soldin <edgar.soldin@web.de>
2013-06-12 15:54:16 +0000

    should work for livedvd70
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@10161 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_openjump.sh
+++ bin/install_openjump.sh
@@ -9,10 +9,10 @@
 # Changes:
 #  25 Jan 2011  Update script to openJUMP 1.4
 #   8 Jan 2012  changes for OJ 1.5
-#  12 Jun 2013  changes for OJ 1.6
+#  12 Jun 2013  changes for OJ 1.6 live-dvd 7.0
 #
 #################################################
-# Copyright (c) 2011-2012 Edgar Soldin, openjump team
+# Copyright (c) 2011-2013 Edgar Soldin, openjump team
 # Copyright (c) 2010 Open Source Geospatial Foundation (OSGeo)
 # Copyright (c) 2009 LISAsoft
 #
@@ -41,25 +41,34 @@ echo "==============================================================="
 echo "install_openjump.sh"
 echo "==============================================================="
 
+# dns trouble? test if we can see it
+host sourceforge.net || exit 1
+
 TMP="/tmp/build_openjump"
 if [ -z "$USER_NAME" ] ; then
    USER_NAME="user"
 fi
 USER_HOME="/home/$USER_NAME"
 
+## default defs, may be overwritten by online conf file below 
 PKG_NAME=OpenJUMP
 PKG_VERSION=1.5
 
-## get defs from online conf below ( file vars, pkg_version )
+## get defs from online conf file ( file vars, pkg_version ) for update convenience
 URL_LIST=http://downloads.sourceforge.net/project/jump-pilot/OpenJUMP/osgeo/osgeo.conf
+# download and set vars
+eval "$(wget -nv -O- "$URL_LIST")"
 
 PKG_FOLDER=$PKG_NAME-$PKG_VERSION
 PKG_HOME=/usr/lib/$PKG_FOLDER
 PKG_DATA=/usr/local/share/$PKG_NAME
+PKG_DATA_SAMPLES="$PKG_DATA/sample_data"
+PKG_DATA_SAMPLES_LINK="/usr/local/share/data/vector/$PKG_NAME"
 PKG_LINK=/usr/local/bin/openjump
 PKG_DESKTOP=$USER_HOME/Desktop/openjump.desktop
 PKG_SUCCESS=$PKG_HOME/.installed
 
+## these defs are defined by conf file above
 #URL_PKG=http://downloads.sourceforge.net/project/jump-pilot/OpenJUMP_snapshots/OpenJUMP-20120108-r2597-CORE.zip
 #URL_ICON=http://jump-pilot.svn.sourceforge.net/viewvc/jump-pilot/core/trunk/icon/openjump_icon3.svg
 #URL_DATA=http://sourceforge.net/projects/jump-pilot/files/Documentation/OpenJUMP%201.4%20Tutorials/ojtutorial_general_2011_data.zip
@@ -71,13 +80,15 @@ PKG_SUCCESS=$PKG_HOME/.installed
 if [ -f $PKG_SUCCESS ] && [ -z "$1" ]; then
   echo "Use --force to reinstall."
   exit 1
-elif [ ! -x "`which wget`" ] ; then
-  echo "ERROR: wget is required, please install it and try again" 
+fi
+
+if [ ! -x "`which wget`" ] ; then
+  echo "ERROR: wget is required, please install it and try again"
   exit 1
 fi
 
 # always cleanup first
-(rm -rfv "$TMP" "$PKG_HOME" "$PKG_DESKTOP" "$PKG_LINK" "$PKG_DATA" "$USER_HOME/.openjump") 2>/dev/null
+(rm -rfv "$TMP" /usr/lib/$PKG_NAME-* "$PKG_DESKTOP" "$PKG_LINK" "$PKG_DATA" "$PKG_DATA_SAMPLES_LINK" "$USER_HOME/.openjump") 2>/dev/null
 [ "$1" = "--clean" ] && exit
 
 # create tmp folders
@@ -86,11 +97,8 @@ mkdir -p "$TMP"
 cd "$TMP"
 
 ## get file list ##
-wget -nv "$URL_LIST" && \
-  eval $(cat $(basename "$URL_LIST")) &&\
-
-# dns trouble? test if we can see it
-host sourceforge.net
+# wget -nv "$URL_LIST" && \
+#  eval $(cat $(basename "$URL_LIST")) &&\
 
 ## Install Application ##
 wget -c --progress=dot:mega "$URL_PKG" && \
@@ -140,15 +148,14 @@ unzip -o -j -q $(basename $URL_DATA) -x '*/.*' -d "$PKG_DATA/sample_data" 2>&1 |
 wget -N --progress=dot:mega "$URL_DOC" &&\
 cp $(basename "$URL_DOC") "$PKG_DATA/" &&\
 
-## set proper permissions ##
+## set proper permissions for data ##
 chmod 644 -R "$PKG_DATA" &&\
 chmod a+X -R "$PKG_DATA" &&\
 touch $PKG_SUCCESS
 
-
 # share data with the rest of the disc
-mkdir -p /usr/local/share/data/vector
-ln -s /usr/local/share/OpenJUMP/sample_data \
-      /usr/local/share/data/vector/openjump
+mkdir -p $(dirname "$PKG_DATA_SAMPLES_LINK") &&\
+ln -s "$PKG_DATA_SAMPLES" \
+      "$PKG_DATA_SAMPLES_LINK"
 
 
