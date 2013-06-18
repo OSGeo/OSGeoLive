b648a646de1b723c11032102ba796e92bd855693
Daniel Kastl <daniel.kastl@georepublic.de>
2013-06-18 12:55:42 +0000

    updated pgRouting install script to install v2.0.0-alpha from PPA and using CREATE EXTENSION pgrouting;
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@10182 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_pgrouting.sh
+++ bin/install_pgrouting.sh
@@ -48,26 +48,13 @@ TMP="/tmp/build_pgrouting"
 OSM_FILE="/usr/local/share/data/osm/feature_city_CBD.osm.bz2"
 OSM_DB="pgrouting"
 
-POSTGRES_VERSION="9.1"
-PGROUTING_FOLDER="/usr/share/postlbs"
-
-# PostGIS 1.4
-#POSTGIS_FOLDER="/usr/share/postgresql/$POSTGRES_VERSION/contrib/"
-
-# PostGIS 1.5
-#POSTGIS_FOLDER="/usr/share/postgresql/$POSTGRES_VERSION/contrib/postgis-1.5/"
-
-# postGIS 2.0 - create via EXTENSION
-
 # Add pgRouting launchpad repository
-add-apt-repository --yes ppa:georepublic/pgrouting
-apt-get -q update
+# TODO: switch from "unstable" to "stable" when repository is updated
+add-apt-repository -y ppa:georepublic/pgrouting-unstable
+apt-get update -qq
 
 # Install pgRouting packages
-apt-get --assume-yes install gaul-devel \
-	postgresql-9.1-pgrouting \
-	postgresql-9.1-pgrouting-dd \
-	postgresql-9.1-pgrouting-tsp
+apt-get install -y -qq postgresql-9.1-pgrouting
 	
 if [ $? -ne 0 ] ; then
    echo 'ERROR: pgRouting Package install failed! Aborting.'
@@ -75,42 +62,20 @@ if [ $? -ne 0 ] ; then
 fi
 
 # Install osm2pgrouting package
-apt-get --assume-yes install osm2pgrouting
+apt-get install -y -qq osm2pgrouting
 
 # Install workshop material
-apt-get --assume-yes install pgrouting-workshop
+# TODO: not packaged yet
+#apt-get install -y -qq pgrouting-workshop
 
 # Create tmp folders
-mkdir -p "$TMP"
-cd "$TMP"
+mkdir -p "$TMP" && cd "$TMP"
 
 # create $OSM_DB database
-echo "create $OSM_DB database"
+echo "create $OSM_DB database with PostGIS and pgRouting"
 sudo -u $USER_NAME createdb -E UTF8 $OSM_DB
-#sudo -u $USER_NAME createlang plpgsql $OSM_DB
-
-# add PostGIS functions
-#echo "add PostGIS functions"
-#sudo -u $USER_NAME psql --quiet -f $POSTGIS_FOLDER/postgis.sql $OSM_DB
-#sudo -u $USER_NAME psql --quiet -f $POSTGIS_FOLDER/spatial_ref_sys.sql $OSM_DB
-sudo -u $USER_NAME psql $OSM_DB -c 'create extension postgis'
-sudo -u $USER_NAME psql $OSM_DB -f /usr/share/postgresql/9.1/contrib/postgis-2.0/legacy.sql
-
-# add pgRouting core functions
-echo "add pgRouting core functions"
-sudo -u $USER_NAME psql --quiet -f $PGROUTING_FOLDER/routing_core.sql $OSM_DB
-sudo -u $USER_NAME psql --quiet -f $PGROUTING_FOLDER/routing_core_wrappers.sql $OSM_DB
-sudo -u $USER_NAME psql --quiet -f $PGROUTING_FOLDER/routing_topology.sql $OSM_DB
-
-# add pgRouting TSP functions
-echo "add pgRouting TSP functions"
-sudo -u $USER_NAME psql --quiet -f $PGROUTING_FOLDER/routing_tsp.sql $OSM_DB
-sudo -u $USER_NAME psql --quiet -f $PGROUTING_FOLDER/routing_tsp_wrappers.sql $OSM_DB
-
-# add pgRouting Driving Distance functions
-echo "add pgRouting Driving Distance functions"
-sudo -u $USER_NAME psql --quiet -f $PGROUTING_FOLDER/routing_dd.sql $OSM_DB
-sudo -u $USER_NAME psql --quiet -f $PGROUTING_FOLDER/routing_dd_wrappers.sql $OSM_DB
+sudo -u $USER_NAME psql $OSM_DB -c 'CREATE EXTENSION postgis;'
+sudo -u $USER_NAME psql $OSM_DB -c 'CREATE EXTENSION pgrouting;'
 
 # Process sample data that comes with "install_osm.sh"
 if [ ! -e "$OSM_FILE" ]
@@ -135,43 +100,34 @@ else
 	    -clean \
 	  > pgrouting_import.log
 
-	# FIX for Multigeometry bug in osm2pgrouting
-	echo "Fix missing source and target attributes"
-	sudo -u "$USER_NAME" psql "$OSM_DB" -c \
-           "ALTER TABLE ways ALTER COLUMN the_geom TYPE geometry(Linestring,4326) USING ST_GeometryN(the_geom, 1)"
-	sudo -u "$USER_NAME" psql "$OSM_DB" -c \
-           "SELECT assign_vertex_id('ways', 0.00001, 'the_geom', 'gid')"
-
-	sudo -u "$USER_NAME" psql "$OSM_DB" -c "vacuum analyze"
+	sudo -u "$USER_NAME" psql "$OSM_DB" -c "VACUUM ANALYZE;"
 fi
 
-
-
+# NOTE: the following is going to change with the updated workshop
 #### recenter the workshop demo on the OSM_local database
-LONG_LAT="-1.147 52.954"   # Nottingham CBD
+#LONG_LAT="-1.147 52.954"   # Nottingham CBD
 
-GOOG_SMERC="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 \
-   +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"
+#GOOG_SMERC="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 \
+#   +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs"
 
 # reproject lat/long to Google's funny smerc (epsg:900913)
-EN=`echo "$LONG_LAT" | cs2cs +proj=longlat +datum=WGS84 +to $GOOG_SMERC | awk '{printf("%.0f, %.0f", $1, $2)}'`
+#EN=`echo "$LONG_LAT" | cs2cs +proj=longlat +datum=WGS84 +to $GOOG_SMERC | awk '{printf("%.0f, %.0f", $1, $2)}'`
 
 # set as 'center: [x, y],' in OpenLayers demo
-sed -i -e "s|center: \[.*\]|center: \[$EN\]|" \
-       -e 's|zoom: 12,|zoom: 14,|' \
-  /usr/share/pgrouting/workshop/web/routing-*.html
+#sed -i -e "s|center: \[.*\]|center: \[$EN\]|" \
+#       -e 's|zoom: 12,|zoom: 14,|' \
+#  /usr/share/pgrouting/workshop/web/routing-*.html
 
 # adjust DB and user name in workshop example
-sed -i -e 's|"routing"|"pgrouting"|' \
-       -e 's|"postgres"|"user"|' \
-  /usr/share/pgrouting/workshop/web/php/pgrouting.php
+#sed -i -e 's|"routing"|"pgrouting"|' \
+#       -e 's|"postgres"|"user"|' \
+#  /usr/share/pgrouting/workshop/web/php/pgrouting.php
 
 # symlink it into a served dir so the php will run
-ln -s /usr/share/pgrouting/workshop/web /var/www/pgrouting
+#ln -s /usr/share/pgrouting/workshop/web /var/www/pgrouting
 
 # to get the routing-final.html demo working you'll still need to set
 # the IPv4 host pgsql permissions to 'trust' in pg_hpa.conf. but we
 # don't want to do that by default.
 
-
-echo "Finished installing pgRouting and pgRouting tools."
+echo "Finished installing pgRouting and osm2pgrouting."
