c717ff44357e63c4cbf57d08845ce3131d113e52
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2013-06-19 07:58:23 +0000

    (#756) rename Geonetwork menu items and launch firefox when GeoNetwork starts
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@10188 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- app-conf/geonetwork/start-geonetwork.sh
+++ app-conf/geonetwork/start-geonetwork.sh
@@ -15,3 +15,5 @@ export JETTY_HOME=.
 ${JAVA_HOME}/bin/java -Xms48m -Xmx512m -Xss2M -XX:MaxPermSize=128m -DSTOP.PORT=8879 -Djeeves.filecharsetdetectandconvert=enabled -Dmime-mappings=../web/geonetwork/WEB-INF/mime-types.properties -Djava.awt.headless=true -DSTOP.KEY=geonetwork -jar start.jar > logs/output.log 2>&1 &
 
 (sleep 10; echo "25"; sleep 10; echo "50"; sleep 10; echo "75"; sleep 10; echo "100") | zenity --progress --auto-close --text "GeoNetwork starting"
+
+firefox http://localhost:8880/geonetwork/
