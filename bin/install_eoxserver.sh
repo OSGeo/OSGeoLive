3201ec2c93f3e65ba6a60daa393742de24d809f7
Brian M Hamlin <screenlight@mac.com>
2013-07-01 17:11:06 +0000

    remove one space
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@10275 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_eoxserver.sh
+++ bin/install_eoxserver.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2012 Open Source Geospatial Foundation (OSGeo)
 #
 # Licensed under the GNU LGPL.
-# 
+#
 # This library is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation, either version 2.1 of the License,
