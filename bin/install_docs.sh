f5da92e3783b6454d9b50bd84a95fc7460722821
Hamish Bowman <hamish_b yahoo com>
2013-07-07 09:08:19 +0000

    fix broken symlink (temporary)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@10325 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_docs.sh
+++ bin/install_docs.sh
@@ -165,7 +165,8 @@ ln -s /usr/share/doc/gmt-tutorial .
 ln -s /usr/share/doc/grass-doc/html grass
 ln -s /usr/local/share/mapnik/demo mapnik
 ln -s /usr/local/share/mapserver/doc mapserver
-ln -s /usr/share/doc/opencpn-doc/doc opencpn
+#ln -s /usr/share/doc/opencpn-doc/doc opencpn  # <-- correct & future home
+ln -s /usr/share/opencpn/doc opencpn
 ln -s /usr/local/share/otb .
 ln -s /usr/local/share/ossim .
 # Data
