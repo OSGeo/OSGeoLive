62a1ff2569af40ad4b90d1d008c6095d0eb00a8d
Hamish Bowman <hamish_b yahoo com>
2013-08-03 07:46:58 +0000

    take /tmp usage into account
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@10599 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/tools/pkg_disk_use_plot.m
+++ bin/tools/pkg_disk_use_plot.m
@@ -45,8 +45,12 @@
 % 
 % cut -f2 disk_usage.prn | sed -e 's/^\(Used\)/%\1/' > disk_usage.dat
 % cut -f3 disk_usage.prn | sed -e 's/^\(date\)/%\1/' > disk_usage.times
-%
-% alias xwdtopng='xwd | xwdtopnm | pnmtopng > '
+% 
+% sed -e 's/^Temp Usage: //' tmp_usage.log | cut -f1 | tr ',' '\t' \
+%   > tmp_usage.prn
+% cat tmp_usage.prn | cut -f2 | sed -e 's/^\(^tmp\)/%\1/' > tmp_usage.dat
+% 
+% % alias xwdtopng='xwd | xwdtopnm | pnmtopng > '
 % ###### end of prep work ######
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -54,13 +58,15 @@
 %%  probably works with minor tweaks in GNU Octave, and with moderate
 %%  tweaks in Python's matplotlib.
 
-build_ver = 'r10560';
+%addpath ./bin/tools
+
+build_ver = 'r10592';
 
 load disk_usage.dat
-u0 = disk_usage(1);
-u_after =  disk_usage - u0;
-u_before = [NaN; u_after(1:end-1)];
-u_pkg = u_after - u_before;
+raw_u0 = disk_usage(1);
+raw_u_after =  disk_usage - raw_u0;
+raw_u_before = [NaN; raw_u_after(1:end-1)];
+raw_u_pkg = raw_u_after - raw_u_before;
 
 
 %%% load in package names
@@ -92,16 +98,29 @@ while (~feof(fd1))
 end
 fclose(fd1);
 
-t0 = pkg_times(1);
-t_after =  pkg_times - t0;
-t_before = [NaN t_after(1:end-1)];
-t_pkg = t_after - t_before;
+time0 = pkg_times(1);
+time_after =  pkg_times - time0;
+time_before = [NaN time_after(1:end-1)];
+time_pkg = time_after - time_before;
 
 
-% clear out bootstrap
-u_pkg(1) = [];
+%%% load in /tmp usage
+load tmp_usage.dat
+tmp_u0 = tmp_usage(1);
+tmp_u_after =  tmp_usage - tmp_u0;
+tmp_u_before = [NaN; tmp_u_after(1:end-1)];
+tmp_u_pkg = tmp_u_after - tmp_u_before;
+
+
+%%% clear out bootstrap
+raw_u_pkg(1) = [];
 pkg_names(1) = [];
-t_pkg(1) = [];
+time_pkg(1) = [];
+tmp_u_pkg(1) = [];
+
+
+%%% remove /tmp space from drive use space to get installed usage
+u_pkg = raw_u_pkg - tmp_u_pkg;
 
 
 %%% plot timeseries %%%
@@ -111,6 +130,7 @@ hB = bar(u_pkg);
 colormap([.7 .7 .7]) % grey
 grid on
 xlim([0 length(u_pkg)+1])
+ylim([-200 max(ylim)])
 ylabel('megabytes used')
 title(['OSGeo Live 7.0.' build_ver ' disk usage'], 'fontsize', 12)
 set(gca, 'xtick', 1:length(u_pkg))
@@ -121,6 +141,7 @@ xticklabel_rotate;
 % left top(above bot) width height
 set(gcf,'paperpos', [0 6 50 10], 'PaperType', 'A2', ...
         'PaperOrientation', 'landscape')
+set(gcf, 'Position', [50 500 1750 400])
 
 print -deps2 'disk_usage_timeseries.eps'
 !gv disk_usage_timeseries.eps
@@ -130,18 +151,19 @@ print -deps2 'disk_usage_timeseries.eps'
 %%% timeseries of times
 figure, clf
 set(gcf, 'color', 'w')
-hB = bar(t_pkg * 24*60);  % convert to minutes
+hB = bar(time_pkg * 24*60);  % convert to minutes
 colormap([.7 .7 .7]) % grey
 grid on
-xlim([0 length(t_pkg)+1])
+xlim([0 length(time_pkg)+1])
 ylabel('time to install (minutes)')
 title(['OSGeo Live 7.0.' build_ver ' install times'], 'fontsize', 12)
-set(gca, 'xtick', 1:length(t_pkg))
+set(gca, 'xtick', 1:length(time_pkg))
 set(gca, 'xticklabel', pkg_names)
 xticklabel_rotate;
-
 set(gcf,'paperpos', [0 6 50 10], 'PaperType', 'A2', ...
         'PaperOrientation', 'landscape')
+set(gcf, 'Position', [50 500 1750 400])
+
 print -deps2 'time_to_install_timeseries.eps'
 !gv time_to_install_timeseries.eps
 %!xwdtopng time_to_install_timeseries.png
@@ -158,6 +180,7 @@ hB = bar(u_sort(:,1));
 colormap([.7 .7 .7]) % grey
 grid on
 xlim([0 length(u_pkg)+1])
+ylim([-200 max(ylim)])
 ylabel('megabytes used')
 title(['OSGeo Live 7.0.' build_ver ' disk usage'], 'fontsize', 12)
 set(gca, 'xtick', 1:length(u_sort))
@@ -167,6 +190,8 @@ xticklabel_rotate;
 %plot(1, 40, 'k*')  % setup.sh unknown
 set(gcf,'paperpos',  [0 6 50 10], 'PaperType', 'A2', ...
         'PaperOrientation', 'landscape')
+set(gcf, 'Position', [50 500 1750 400])
+
 print -deps2 'disk_usage_sorted.eps'
 %!gv disk_usage_sorted.eps
 %!ps2pdf disk_usage_sorted.eps
@@ -174,7 +199,7 @@ print -deps2 'disk_usage_sorted.eps'
 
 
 %%% sorted timeseries of times
-t_sort = flipud(sortrows([t_pkg' (1:length(t_pkg))']));
+t_sort = flipud(sortrows([time_pkg' (1:length(time_pkg))']));
 pkg_names_sorted = pkg_names(t_sort(:,2))';
 
 figure, clf
@@ -188,9 +213,10 @@ title(['OSGeo Live 7.0.' build_ver ' install times'], 'fontsize', 12)
 set(gca, 'xtick', 1:length(t_sort))
 set(gca, 'xticklabel', pkg_names_sorted)
 xticklabel_rotate;
-
 set(gcf,'paperpos', [0 6 50 10], 'PaperType', 'A2', ...
         'PaperOrientation', 'landscape')
+set(gcf, 'Position', [50 500 1750 400])
+
 print -deps2 'time_to_install_sorted.eps'
 !gv time_to_install_sorted.eps
 %!xwdtopng time_to_install_sorted.png
@@ -205,8 +231,8 @@ disp(['Max package size:' tab num2str(nanmax(u_pkg)) ' mb'])
 disp(['Min package size:' tab num2str(nanmin(u_pkg)) ' mb'])
 build_time = (max(pkg_times) - min(pkg_times));
 disp(['Package install time:' tab datestr(build_time, 15)])
-disp(['Median install time:' tab datestr(median(t_pkg), 13)]) 
-disp(['Average install time:' tab datestr(mean(t_pkg), 13)]) 
+disp(['Median install time:' tab datestr(median(time_pkg), 13)]) 
+disp(['Average install time:' tab datestr(mean(time_pkg), 13)]) 
 
 
 
@@ -214,14 +240,15 @@ disp(['Average install time:' tab datestr(mean(t_pkg), 13)])
 % http://www.mathworks.com/help/toolbox/stats/boxplot.html
 figure, clf
 set(gcf, 'color', 'w')
-boxplot(u_pkg, 'notch', 'on', 'labels', ' ')
+boxplot(u_pkg(1:end-1), 'notch', 'on', 'labels', ' ')
 grid on
 ylabel('megabytes used')
-
+%ylim([-200 max(ylim)])
 x = (0.10 * (max(xlim) - min(xlim))) + min(xlim);  % 10% from the left
 y = min(u_pkg);
 text(x, y, ['n = ' num2str(length(u_pkg))])
-set(gcf,'paperpos',  [6.5816  7.2245  7.8207 15.2284])
+set(gcf, 'paperpos', [6.5816  7.2245  7.8207 15.2284], ...
+         'Position', [200 500 300 500])
 
 print -depsc2 'disk_usage_whisker.eps'
 
@@ -231,14 +258,15 @@ print -depsc2 'disk_usage_whisker.eps'
 % http://www.mathworks.com/help/toolbox/stats/boxplot.html
 figure, clf
 set(gcf, 'color', 'w')
-boxplot(t_pkg * 24*60, 'notch', 'on', 'labels', ' ')
+boxplot(time_pkg * 24*60, 'notch', 'on', 'labels', ' ')
 grid on
 ylabel('time to install (minutes)')
 
 x = (0.10 * (max(xlim) - min(xlim))) + min(xlim);  % 10% from the left
-y = min(t_pkg) * 24*60 + 0.5;
+y = min(time_pkg) * 24*60 + 0.5;
 text(x, y, ['n = ' num2str(length(u_pkg))])
-set(gcf,'paperpos',  [6.5816  7.2245  7.8207 15.2284])
+set(gcf, 'paperpos', [6.5816  7.2245  7.8207 15.2284], ...
+         'Position', [400 500 300 500])
 
 print -depsc2 'install_time_whisker.eps'
 %!xwdtopng install_time_whisker.png
