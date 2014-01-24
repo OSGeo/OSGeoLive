d4cfc08676b0b4373fbca166b90fa6d62c88dc14
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2014-01-24 13:54:02 +0000

    another update to geoserver version
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11000 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_cartaro.sh
+++ bin/install_cartaro.sh
@@ -40,7 +40,7 @@ DB_PASSWORD="cartaro"
 
 GEO_ADMIN="admin"
 GEO_PASS="geoserver"
-GEO_VERSION="2.4.3"
+GEO_VERSION="2.4.4"
 GEO_PATH="/usr/local/lib/geoserver-$GEO_VERSION"
 
 TMP_DIR="/tmp/build_cartaro"
