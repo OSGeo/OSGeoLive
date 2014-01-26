779b7d23d5c3d438aa5d963781659bd5f5488bf1
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2014-01-26 15:00:43 +0000

    bringing in the latest matplotlib
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11016 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_iris.sh
+++ bin/install_iris.sh
@@ -34,7 +34,8 @@ pip install netCDF4 pyshp
 
 # Specify specific matplotlib update for OSGeo Live 7.0
 echo "FIXME: verify no conflicts with the system pacakged version of matplotlib"
-pip install matplotlib==1.2.0
+#pip install matplotlib==1.2.0
+pip install --upgrade matplotlib
 
 # Build and install grib_api (optional):
 mkdir /tmp/build_iris
