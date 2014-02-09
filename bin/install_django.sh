474f3ce79e36fd4d80a1404f087e37dd8ec7df64
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2014-02-09 10:21:07 +0000

    forgot to change ppa for django to geonode-release
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11082 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_django.sh
+++ bin/install_django.sh
@@ -36,8 +36,8 @@
 # pip install --upgrade Django=="$DJVER"
 
 #Install packages
-#add-apt-repository -y ppa:geonode/release
-add-apt-repository -y ppa:geonode/unstable
+add-apt-repository -y ppa:geonode/release
+#add-apt-repository -y ppa:geonode/unstable
 #add-apt-repository -y ppa:geonode/testing
 apt-get -q update
 apt-get --assume-yes install python-django
