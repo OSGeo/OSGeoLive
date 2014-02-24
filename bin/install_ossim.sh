32697d9c6bd9a13a2c81d64d38b60524dd8ed1e3
Massimo Di Stefano <massimodisasha@yahoo.it>
2014-02-24 01:12:32 +0000

    updated ossim_install (testing needed)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11143 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_ossim.sh
+++ bin/install_ossim.sh
@@ -20,8 +20,8 @@ USER_HOME="/home/$USER_NAME"
 TMP_DIR=/tmp/build_ossim
 APP_DATA_DIR="$BUILD_DIR/../app-data/ossim"
 DATA_FOLDER="/usr/local/share/data"
-#OSSIM_VERSION=1.8.12
-#BUILD_DATE=20110704
+OSSIM_VERSION=1.8.16
+BUILD_DATE=20140202
 
 #CAUTION: UbuntuGIS should be enabled only through setup.sh
 #Add repositories
@@ -38,14 +38,14 @@ apt-get install --assume-yes ossim-core libgdal1-1.10.0-grass
 apt-get install --assume-yes libtiff4 libgeotiff2 \
   libfreetype6 libcurl3 libopenscenegraph80 libqt4-opengl \
   libexpat1 libpng3 libfftw3-3 libqt3-mt \
-  libopenmpi1.3 libqt4-qt3support python-pip python-pandas python-netcdf \
+  libopenmpi1.3 libqt4-qt3support python-pip \
   spyder
-
+# python-pandas python-netcdf
 apt-get install --assume-yes krb5-multidev libexpat-ocaml libfindlib-ocaml \
   libgnutls-openssl27 libopenjpeg2 libpodofo0.9.0 libpthread-stubs0 \
   libqt4-sql-sqlite libtiffxx0c2 ocaml-base-nox \
   ocaml-findlib ocaml-interp ocaml-nox pkg-config \
-  qt4-dev-tools qt4-linguist-tools qt4-qmake xorg-sgml-doctools
+  qt4-dev-tools qt4-linguist-tools qt4-qmake xorg-sgml-doctools libqt4-core
 
 # fragile @ ubuntugis
 #apt-get install --assume-yes libgdal1h  libgdal1-1.10.0-grass
@@ -80,25 +80,6 @@ ldconfig
 # if that works, keep going ...
 
 
-exit 0
-#########################################
-
-#wget -N --progress=dot:mega "http://geofemengineering.it/osgeolive/ossim.tar.gz" 
- 
-wget -N --progress=dot:mega \
-   "http://download.osgeo.org/livedvd/data/ossim/ossim.tar.gz" 
-
-tar -zxf ossim.tar.gz
-
-# running tar as root expands as the UID on the host machine that made it, so
-chown -R root.root ossim/
-mv ossim /usr/local/
-# is /usr/local/ossim/ actually needed in the lib search path?
-echo "/usr/local/ossim/
-/usr/local/ossim/lib/" >> ossim.conf
-mv ossim.conf /etc/ld.so.conf.d/
-ldconfig
-
 mkdir /usr/share/ossim/
 wget -N --progress=dot:mega \
    "http://download.osgeo.org/livedvd/data/ossim/ossim_settings.tar.gz"
@@ -108,72 +89,28 @@ tar -zxf ossim_settings.tar.gz
 chown -R root.root ossim_settings/
 mv ossim_settings/* /usr/share/ossim/
 
+
 #patch for ticket https://trac.osgeo.org/osgeo/ticket/647 
 sed -i -e 's/OsssimPlanet/OssimPlanet/g' /usr/share/ossim/ossimplanet.desktop
 
 mv /usr/share/ossim/images/ossimPlanet.xpm /usr/share/pixmaps/ossimPlanet.xpm
 mv /usr/share/ossim/images/ossim.xpm /usr/share/pixmaps/ossim.xpm
+
 mv /usr/share/ossim/imagelinker.desktop /usr/share/applications/imagelinker.desktop
 mv /usr/share/ossim/ossimplanet.desktop /usr/share/applications/ossimplanet.desktop
 
-if [ `grep -c '/usr/local/ossim/bin' "$USER_HOME/.bashrc"` -eq 0 ] ; then
-   cat << EOF >> "$USER_HOME/.bashrc"
-
-PATH="\$PATH:/usr/local/ossim/bin"
-export PATH
 OSSIM_PREFS_FILE="/usr/share/ossim/ossim_preference"
 export OSSIM_PREFS_FILE
-EOF
-   #source "$USER_HOME/.bashrc"
-fi
 
 BRCFILE="/etc/skel/.bashrc"
-if [ `grep -c '/usr/local/ossim/bin' "$BRCFILE"` -eq 0 ] ; then
-   # we know that it is safe to use bashisms in .BASHrc
-   echo 'export PATH="$PATH:/usr/local/ossim/bin"' >> "$BRCFILE"
-   echo 'export OSSIM_PREFS_FILE="/usr/share/ossim/ossim_preference"' >> "$BRCFILE"
-fi
-
-
-# Additional dependencies for Grass / Qgis plug-in :
-#
+echo 'export OSSIM_PREFS_FILE="/usr/share/ossim/ossim_preference"' >> "$BRCFILE"
 
 apt-get install --assume-yes grass-core qgis python-pysqlite2 \
    python-scipy python-serial python-psycopg2 proj-bin python-lxml \
    libqt4-core python-distutils-extra python-setuptools \
    python-qscintilla2
-   # python-pygame
-   # spyder
-
-
-#### PlanetSasha commented for now
-#mkdir $USER_HOME/Desktop/PlanetSasha
-#FIXME: Please do not use "chmod 777". Add to the "users" group and chmod g+w instead.
-#chmod -R 777 $USER_HOME/Desktop/PlanetSasha
-#
-#FIXME: do not checkout directly to $USER_HOME. Use "svn export" to /usr/local/share/data/
-#  instead, or svn co to /tmp/build_ossim/ then copy dir to /usr/local/share/data/ and
-#  symlink into $HOME.
-#svn co http://svn.osgeo.org/ossim/trunk/gsoc/PlanetSasha $USER_HOME/Desktop/PlanetSasha
-#FIXME: Do not use chmod 777. see above.
-#chmod -R 777 $USER_HOME/Desktop/PlanetSasha
-#
-#cp $USER_HOME/Desktop/PlanetSasha/grass_script/r.planet.py /usr/lib/grass64/scripts/
-#cp $USER_HOME/Desktop/PlanetSasha/grass_script/v.planet.py /usr/lib/grass64/scripts/
-#cp $USER_HOME/Desktop/PlanetSasha/grass_script/ogrTovrt.py /usr/lib/grass64/scripts/
-#cp $USER_HOME/Desktop/PlanetSasha/grass_script/d.png.legend /usr/lib/grass64/scripts/
-#
-#FIXME: python-sphinx package needs to be installed first?
-#
-#hg clone https://spyderlib.googlecode.com/hg/ spyderlib
-#cd spyderlib
-#python setup.py install
-#cd ..
-#rm -rf spyderlib
-####
-
-
-
+   
+   
 cp /usr/share/applications/imagelinker.desktop "$USER_HOME/Desktop/"
 chown "$USER_NAME.$USER_NAME" "$USER_HOME/Desktop/imagelinker.desktop"
 sed -i -e 's/^Name=imagelinker/Name=Imagelinker/' \
@@ -211,59 +148,58 @@ fi
 #Install the Manual and Intro guide locally and link them to the description.html
 mkdir /usr/local/share/ossim
 
-wget --read-timeout=20 --tries=5 --progress=dot:mega \
-     "http://download.osgeo.org/ossim/docs/pdfs/ossim_users_guide.pdf" \
-     --output-document=/usr/local/share/ossim/ossim_users_guide.pdf
+##wget --read-timeout=20 --tries=5 --progress=dot:mega \
+##     "http://download.osgeo.org/ossim/docs/pdfs/ossim_users_guide.pdf" \
+##     --output-document=/usr/local/share/ossim/ossim_users_guide.pdf
 
 #echo "FIXME: doesn't exist ==>
 # 'ln -s /usr/share/doc/ossim-doc/ossimPlanetUsers.pdf /usr/local/share/ossim/'"
 
 # pdf temporary stored on my ftp, waiting to add it on ossim download page.   
-wget --read-timeout=20 --tries=5 --progress=dot:mega \
-     "http://download.osgeo.org/livedvd/data/ossim/OSSIM_Whitepaper.pdf" \
-     --output-document=/usr/local/share/ossim/OSSIM_Whitepaper.pdf
+##wget --read-timeout=20 --tries=5 --progress=dot:mega \
+##     "http://download.osgeo.org/livedvd/data/ossim/OSSIM_Whitepaper.pdf" \
+##     --output-document=/usr/local/share/ossim/OSSIM_Whitepaper.pdf
 
 
 #Download data used to test the application
-#KML_DATA="$DATA_FOLDER/kml"
+KML_DATA="$DATA_FOLDER/kml"
 RASTER_DATA="$DATA_FOLDER/raster"
 ELEV_DATA=/usr/share/ossim/elevation/elev
-#SAT_DATA="$RASTER_DATA/cape_cod"
+SAT_DATA="$RASTER_DATA/cape_cod"
 #echo "FIXME: does VRT data actually ship anymore?"
 VRT_DATA="$DATA_FOLDER/vrt"
 QUICKSTART=/usr/local/share/ossim/quickstart
 
-#mkdir -p "$KML_DATA"
+mkdir -p "$KML_DATA"
 mkdir -p "$RASTER_DATA"
-#mkdir -p "$SAT_DATA"
+mkdir -p "$SAT_DATA"
 mkdir -p "$ELEV_DATA"   # ?? unused ??
-#mkdir -p "$VRT_DATA"
+mkdir -p "$VRT_DATA"
 
 
 
 # disabled: $VRT_DATA $KML_DATA $SAT_DATA
-for ITEM in $RASTER_DATA $ELEV_DATA ;  do
+for ITEM in $RASTER_DATA $ELEV_DATA $VRT_DATA $KML_DATA $SAT_DATA;  do
    chmod -R 775 "$ITEM"
    chgrp -R users "$ITEM"
 done
 
-if [ 0 -eq 1 ] ; then
+
 # Cape Cod SRTM and LANDSAT
 DATA_URL="http://download.osgeo.org/livedvd/data/ossim/"
 BASENAME="p011r031_7t19990918_z19_nn"
-for BAND in 10 20 30 ; do
-    # LANDSAT
-    wget --progress=dot:mega "$DATA_URL/ossim_data/${BASENAME}$BAND.tif" \
-         --output-document="$SAT_DATA/${BASENAME}$BAND.tif"
-    ls -l "$SAT_DATA/${BASENAME}$BAND.tif"
-    chmod a-x "$SAT_DATA/${BASENAME}$BAND.tif"
-done
+##for BAND in 10 20 30 ; do
+##    # LANDSAT
+##    wget --progress=dot:mega "$DATA_URL/ossim_data/${BASENAME}$BAND.tif" \
+##         --output-document="$SAT_DATA/${BASENAME}$BAND.tif"
+##    ls -l "$SAT_DATA/${BASENAME}$BAND.tif"
+##    chmod a-x "$SAT_DATA/${BASENAME}$BAND.tif"
+##done
 
 # SRTM
-wget --progress=dot:mega "$DATA_URL/ossim_data/SRTM_fB03_p011r031.tif"  \
-     --output-document="$SAT_DATA/SRTM_fB03_p011r031.tif"
-chmod a-x "$SAT_DATA/SRTM_fB03_p011r031.tif"
-fi
+##wget --progress=dot:mega "$DATA_URL/ossim_data/SRTM_fB03_p011r031.tif"  \
+##     --output-document="$SAT_DATA/SRTM_fB03_p011r031.tif"
+##chmod a-x "$SAT_DATA/SRTM_fB03_p011r031.tif"
 
 #wget --progress=dot:mega $DATA_URL/ossim_data/bluemarble.tif  \
 #--output-document=/usr/share/ossim/images/reference/bluemarble.tif
@@ -286,23 +222,20 @@ apt-get --assume-yes install libjpeg62
 OSSIM_PREFS_FILE=/usr/share/ossim/ossim_preference
 export OSSIM_PREFS_FILE
 
-if [ 0 -eq 1 ] ; then
-/usr/local/ossim/bin/ossim-img2rr \
+
+/usr/bin/ossim-img2rr \
     "$SAT_DATA/p011r031_7t19990918_z19_nn10.tif" \
     "$SAT_DATA/p011r031_7t19990918_z19_nn20.tif" \
     "$SAT_DATA/p011r031_7t19990918_z19_nn30.tif"
 
-/usr/local/ossim/bin/ossim-create-histo \
+/usr/bin/ossim-create-histo \
     "$SAT_DATA/p011r031_7t19990918_z19_nn10.tif" \
     "$SAT_DATA/p011r031_7t19990918_z19_nn20.tif" \
     "$SAT_DATA/p011r031_7t19990918_z19_nn30.tif"
-fi
+
 
 #/usr/bin/gdal_translate -of VRT "$RASTER_DATA"/BlueMarble_small.tif \
 #    /usr/share/ossim/images/reference/bluemarble.tif
-
-
-
 
 unset OSSIM_PREFS_FILE
 
@@ -316,49 +249,58 @@ chgrp users /usr/share/ossim/elevation
 
 
 #### spearfish subset to VRT
-SPEARFISH_RASTER=/usr/local/share/grass/spearfish60/PERMANENT/cellhd/
 
-#for i in $SPEARFISH_RASTER; do
-#    gdal_translate -of VRT $SPEARFISH_RASTER/$i /home/user/data/workspace/$i.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/aspect /home/user/data/raster/vrt/aspect.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/erosion1 /home/user/data/raster/vrt/erosion1.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/quads /home/user/data/raster/vrt/quads.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/soils /home/user/data/raster/vrt/soils.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/strm.dist /home/user/data/raster/vrt/strm.dist.vrt 
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/bugsites /home/user/data/raster/vrt/bugsites.vrt 
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/fields /home/user/data/raster/vrt/fields.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/railroads /home/user/data/raster/vrt/railroads.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/soils.Kfactor /home/user/data/raster/vrt/soils.Kfactor.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/texture /home/user/data/raster/vrt/texture.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/density /home/user/data/raster/vrt/density.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/geology /home/user/data/raster/vrt/geology.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/roads /home/user/data/raster/vrt/roads.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/soils.ph /home/user/data/raster/vrt/soils.ph.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/tractids /home/user/data/raster/vrt/tractids.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/elevation.10m /home/user/data/raster/vrt/elevation.10m.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/landcover.30m /home/user/data/raster/vrt/landcover.30m.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/rstrct.areas /home/user/data/raster/vrt/rstrct.areas.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/soils.range /home/user/data/raster/vrt/soils.range.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/transport.misc /home/user/data/raster/vrt/transport.misc.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/elevation.dem /home/user/data/raster/vrt/elevation.dem.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/landcover.orig /home/user/data/raster/vrt/landcover.orig.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/rushmore /home/user/data/raster/vrt/rushmore.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/soils.Tfactor /home/user/data/raster/vrt/soils.Tfactor.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/trn.sites /home/user/data/raster/vrt/trn.sites.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/elevation.dted /home/user/data/raster/vrt/elevation.dted.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/landuse /home/user/data/raster/vrt/landuse.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/slope /home/user/data/raster/vrt/slope.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/spot.image /home/user/data/raster/vrt/spot.image.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/uparea /home/user/data/raster/vrt/uparea.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/erode.index /home/user/data/raster/vrt/erode.index.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/owner /home/user/data/raster/vrt/owner.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/soil.br.depth /home/user/data/raster/vrt/soil.br.depth.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/streams /home/user/data/raster/vrt/streams.vrt
+gdal_translate -of VRT /usr/local/share/grass/spearfish60/PERMANENT/cellhd/vegcover /home/user/data/raster/vrt/vegcover.vrt
+
+
 
 # gdal-grass broken .. commentimng the conversion to vrt
-#/usr/bin/gdal_translate -of VRT \
-#    "$SPEARFISH_RASTER"/elevation.10m \
-#    "$QUICKSTART"/workspace/elevation10m.vrt
+/usr/bin/gdal_translate -of VRT \
+    "$SPEARFISH_RASTER"/elevation.10m \
+    "$QUICKSTART"/workspace/elevation10m.vrt
 
-#/usr/bin/gdal_translate -of GTIFF -ot Float64 \
-#    "$QUICKSTART"/workspace/elevation10m.vrt \
-#    "$QUICKSTART"/workspace/elevation10m.tif
+/usr/bin/gdal_translate -of GTIFF -ot Float64 \
+    "$QUICKSTART"/workspace/elevation10m.vrt \
+    "$QUICKSTART"/workspace/elevation10m.tif
 
-
-
-# Broken (see trac #1159)
-# broken (gdal grass plugin required to generate src file)
-#OSSIM_PREFS_FILE=/usr/share/ossim/ossim_preference \
-#  /usr/local/ossim/bin/ossim-orthoigen -w general_raster_bip \
-#    "$QUICKSTART"/workspace/elevation10m.tif \
-#    /usr/share/ossim/elevation/spearfish/elevation10m.ras
-
-#rm -f "$QUICKSTART"/workspace/elevation10m.tif
-
-#/usr/bin/gdal_translate -of VRT "$SPEARFISH_RASTER"/geology \
-#    "$QUICKSTART"/workspace/geology.vrt
-
-
-#COORDS="N40E002 N41E002 N42E002"
-# ? N40 is corrupt and N42 is all zeros ?
-#COORDS=" N41E002 "
-#for COORD in $COORDS ; do
-#   wget --progress=dot:mega "$DATA_URL/ossim_data/elev/$COORD.hgt" \
-#        --output-document="/usr/share/ossim/elevation/elev/$COORD.hgt"
-#   wget --progress=dot:mega "$DATA_URL/ossim_data/elev/$COORD.omd" \
-#        --output-document="/usr/share/ossim/elevation/elev/$COORD.omd"
-#done
-
-# get rid of unused reference map
-#rm -rf /usr/share/ossim/images/reference/earth.jpg
-#rm -rf /usr/share/ossim/images/reference/earth.ovr
-#rm -rf /usr/share/ossim/images/reference/earth.geom
+OSSIM_PREFS_FILE=/usr/share/ossim/ossim_preference \
+  /usr/bin/ossim-orthoigen -w general_raster_bip \
+    "$QUICKSTART"/workspace/elevation10m.tif \
+    /usr/share/ossim/elevation/spearfish/elevation10m.ras
 
 
 cp -r "$APP_DATA_DIR"/* "$QUICKSTART"/
@@ -386,4 +328,4 @@ rm -rf "$QUICKSTART"/.svn
 
 
 ####
-"$BUILD_DIR"/diskspace_probe.sh "`basename $0`" end
+#"$BUILD_DIR"/diskspace_probe.sh "`basename $0`" end
