96bcfb9945d922c1e2bd98964b2a1889bf0dbd2f
Hamish Bowman <hamish_b yahoo com>
2014-02-26 23:19:13 +0000

    GdalTools.py patch no longer needed, applied upstream
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11215 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_qgis.sh
+++ bin/install_qgis.sh
@@ -244,13 +244,5 @@ cat "$TMPFILE" "$TMPFILE".b > "$CONFFILE"
 rm -f "$TMPFILE" "$TMPFILE".b
 
 
-#Apply patch for trac ticket #1208
-cp "$BUILD_DIR"/../app-conf/qgis/fix_gdaltools_version.patch /usr/share/qgis/
-cd /usr/share/qgis/
-patch -p1 < fix_gdaltools_version.patch
-rm -f /usr/share/qgis/fix_gdaltools_version.patch
-cd "$BUILD_DIR"
-
-
 ####
 "$BUILD_DIR"/diskspace_probe.sh "`basename $0`" end
