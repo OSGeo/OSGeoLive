773943977c318a7690f93d60a87db49caa2d81d5
Hamish Bowman <hamish_b yahoo com>
2014-02-26 07:39:26 +0000

    replace bashisms, linewrap, quote paths
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11197 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- app-conf/kosmo/Kosmo.sh
+++ app-conf/kosmo/Kosmo.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 # Kosmo.sh
-cd /usr/lib/Kosmo-3.0/bin
-KOSMO_LIBS_PATH="../libs"
-export PROJ_LIB="./crs/data"
 
-if [ -n "$LD_LIBRARY_PATH" ]; then
-	export LD_LIBRARY_PATH=$KOSMO_LIBS_PATH:$LD_LIBRARY_PATH
+cd /usr/lib/Kosmo-3.0/bin
+
+KOSMO_LIBS_PATH="../libs"
+PROJ_LIB="./crs/data"
+export PROJ_LIB
+
+if [ -n "$LD_LIBRARY_PATH" ] ; then
+    LD_LIBRARY_PATH="$KOSMO_LIBS_PATH:$LD_LIBRARY_PATH"
 else
-	export LD_LIBRARY_PATH=$KOSMO_LIBS_PATH
+    LD_LIBRARY_PATH="$KOSMO_LIBS_PATH"
 fi
-java -Djava.library.path=/usr/lib:"../libs" -Dsun.java2d.d3d=false -cp .:./kosmo_desktop.jar:./libs/jai_codec-1.1.3.jar:./libs/jai_core-1.1.3.jar:./jai_imageio-1.1.3.jar -Xmx800M com.vividsolutions.jump.workbench.JUMPWorkbench -plug-in-directory ./ext
+export LD_LIBRARY_PATH
+
+java -Djava.library.path=/usr/lib:"../libs" \
+     -Dsun.java2d.d3d=false \
+     -cp ".:./kosmo_desktop.jar:./libs/jai_codec-1.1.3.jar:./libs/jai_core-1.1.3.jar:./jai_imageio-1.1.3.jar" \
+     -Xmx800M "com.vividsolutions.jump.workbench.JUMPWorkbench" \
+     -plug-in-directory "./ext"
 
