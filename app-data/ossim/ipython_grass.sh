81ee0644f4b7345c39c79b9b04ebc22ca8c185f1
Hamish Bowman <hamish_b yahoo com>
2014-03-02 04:06:42 +0000

    bashims and other enhancements
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11258 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- app-data/ossim/ipython_grass.sh
+++ app-data/ossim/ipython_grass.sh
@@ -1,13 +1,38 @@
-export LD_LIBRARY_PATH=/usr/lib/grass64/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/usr/lib/grass64/etc/python:$PYTHONPATH
-export GISBASE=/usr/lib/grass64/
-export PATH=/usr/lib/grass64/bin/:$GISBASE/bin:$GISBASE/scripts:$PATH
-export GIS_LOCK=$$
-export GISRC=/home/$USER/.grassrc6
-export GISDBASE=/home/$USER/grassdata
-export GRASS_TRANSPARENT=TRUE
-export GRASS_TRUECOLOR=TRUE
-export GRASS_PNG_COMPRESSION=9
-export GRASS_PNG_AUTO_WRITE=TRUE
+#!/bin/sh
+#
+# Script to start minimal GRASS session for ipython
+#
 
-ipython notebook --port=12345 --no-browser --notebook-dir=/home/user/ossim/workspace/geo-notebook --ip='*'
+# Conditionally add $LD_LIBRARY_PATH and $PYTHONPATH only if they already exist.
+# An empty ":" at the end can add `pwd` to the path, which is generally frowned upon.
+if [ -n "$LD_LIBRARY_PATH" ] ; then
+   LD_LIBRARY_PATH="/usr/lib/grass64/lib:$LD_LIBRARY_PATH"
+else
+   LD_LIBRARY_PATH="/usr/lib/grass64/lib"
+fi
+if [ -n "$PYTHONPATH" ] ; then
+   PYTHONPATH="/usr/lib/grass64/etc/python:$PYTHONPATH"
+else
+   PYTHONPATH="/usr/lib/grass64/etc/python"
+fi
+GISBASE="/usr/lib/grass64"
+PATH="$GISBASE/bin:$GISBASE/scripts:$PATH"
+GIS_LOCK="$$"
+GISRC="/home/$USER/.grassrc6"
+export LD_LIBRARY_PATH PYTHONPATH GISBASE PATH GIS_LOCK GISRC
+
+# note: $GISDBASE is generally a g.gisenv variable stored in .grassrc6, not a shell variable anymore
+GISDBASE="/home/$USER/grassdata"
+export GISDBASE
+
+GRASS_TRANSPARENT=TRUE
+GRASS_TRUECOLOR=TRUE
+GRASS_PNG_COMPRESSION=9
+GRASS_PNG_AUTO_WRITE=TRUE
+export GRASS_TRANSPARENT GRASS_TRUECOLOR GRASS_PNG_COMPRESSION GRASS_PNG_AUTO_WRITE
+
+
+ipython notebook --port=12345 --no-browser \
+   --notebook-dir=/home/user/ossim/workspace/geo-notebook \
+   --ip='*'
+
