cde6672d9bc1ca4f5a6d6caced85467720f63570
Hamish Bowman <hamish_b yahoo com>
2014-03-10 20:02:21 +0000

    still over-budget
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11362 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/load_win_installers.sh
+++ bin/load_win_installers.sh
@@ -65,13 +65,11 @@ for URL in \
   http://qgis.org/downloads/QGIS-OSGeo4W-2.0.1-3-Setup-x86.exe \
   http://grass.osgeo.org/grass64/binary/mswindows/native/WinGRASS-6.4.3-1-Setup.exe \
   http://download.osgeo.org/livedvd/data/gpsbabel/GPSBabel-1.4.4-Setup.exe \
-  http://github.com/downloads/mapbox/tilemill/TileMill-v0.10.1-Setup.exe \
   http://gpsvp.googlecode.com/files/gpsVPxp_0.4.24.zip \
   http://downloads.sourceforge.net/project/opencpn/opencpn/3.2.2/opencpn_3.2.2_setup.exe?use_mirror=cdnetworks-us-2 \
   http://sourceforge.net/projects/saga-gis/files/SAGA%20-%202.1/SAGA%202.1.1/saga_2.1.1_win32.zip?use_mirror=cdnetworks-us-2 \
   http://zygrib.org/getfile.php?file=zyGrib_win-6.2.3.zip \
   http://download.osgeo.org/fdo/3.8.0/release/binaries/win32/fdo-win32-3.8.0_3601.tar.gz \
-  http://sourceforge.net/projects/geoserver/files/GeoServer/2.4.4/geoserver-2.4.4-bin.zip?use_mirror=cdnetworks-us-2 \
   http://download.osgeo.org/livedvd/data/mapwindow/MapWindowx86Full-v488SR-installer.exe \
 ; do
 
@@ -91,14 +89,15 @@ done;
 #  http://udig.refractions.net/files/downloads/udig-1.4.0.win32.win32.x86.exe \
 #  http://www.opensig.es/public/kosmo/v_2.0.1/binaries/kosmo_desktop_2.0.1_windows_jre_setup.exe \
 #  http://download.osgeo.org/mapguide/releases/2.5.2/MapGuideOpenSource-2.5.2.7949-Final-x86.exe \
+#  http://github.com/downloads/mapbox/tilemill/TileMill-v0.10.1-Setup.exe \
 
 
 #Disabled because they are very outdated
 # http://maptiler.googlecode.com/files/maptiler-1.0-beta2-setup.exe   :: but it still works fine and is only 13mb.
 # http://home.gdal.org/fwtools/FWTools247.exe \
 
-# Disabled, we only do desktop apps  (er, readded above)
-# http://downloads.sourceforge.net/geoserver/geoserver-2.1.4-bin.zip \
+# Disabled, we only do desktop apps
+#  http://sourceforge.net/projects/geoserver/files/GeoServer/2.4.4/geoserver-2.4.4-bin.zip?use_mirror=cdnetworks-us-2 \
 
 #  from the README:
 #GeoServer:  geoserver-2.1.4-bin.zip in the Windows Installers cache also
