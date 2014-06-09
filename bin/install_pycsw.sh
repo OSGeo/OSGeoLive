709ce3a4af90709822594e435f5f77b313f44234
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2014-06-09 14:15:26 +0000

    Temp logo fix
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11489 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_pycsw.sh
+++ bin/install_pycsw.sh
@@ -43,5 +43,9 @@ apt-get install --yes python-owslib python-pycsw pycsw-cgi
 cp /usr/share/applications/pycsw.desktop "$USER_HOME/Desktop/"
 chown "$USER_NAME:$USER_NAME" "$USER_HOME/Desktop/pycsw.desktop"
 
+# Temp fix: to be added in deb
+wget https://github.com/geopython/pycsw/raw/master/docs/_static/pycsw-logo.png
+mv pycsw-logo.png /var/www/html/pycsw/docs/_static/
+
 ####
 ./diskspace_probe.sh "`basename $0`" end
