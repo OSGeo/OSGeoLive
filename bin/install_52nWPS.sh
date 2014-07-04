df574465483c9039b1ed0fb6049b761bda7c6712
Benjamin Pross <b.pross@52north.org>
2014-07-04 11:44:40 +0000

    Adjusted version of 52nWPS tarball.
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11549 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_52nWPS.sh
+++ bin/install_52nWPS.sh
@@ -12,7 +12,7 @@
 # in the "LICENSE.LGPL.txt" file distributed with this software or at
 # web page "http://www.fsf.org/licenses/lgpl.html".
 #
-# Version: 2013-02-08
+# Version: 2014-07-04
 # Author: e.h.juerrens@52north.org, b.pross@52north.org (modified for WPS)
 # TODO
 # - maybe delete war file after set-up in tomcat (?) -> save space on disc
@@ -40,7 +40,7 @@ USER_HOME="/home/$USER_NAME"
 TOMCAT_USER_NAME="tomcat6"
 WPS_WAR_INSTALL_FOLDER="/var/lib/tomcat6/webapps"
 WPS_BIN_FOLDER="/usr/local/share/52nWPS"
-WPS_TAR_NAME="52nWPS-3.2.0.tar.gz"
+WPS_TAR_NAME="52nWPS-3.3.0.tar.gz"
 WPS_TAR_URL="http://52north.org/files/geoprocessing/OSGeoLiveDVD/"
 # when changing this, adjust the name in line 215, too,
 # and the quickstart, which links to this, too
