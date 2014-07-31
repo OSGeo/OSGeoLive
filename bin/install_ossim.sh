e4394f170bedd46d5ff3537938228f8c9e6e7c12
Hamish Bowman <hamish_b yahoo com>
2014-07-31 23:08:22 +0000

    make adjustment to the correct file :-)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11660 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_ossim.sh
+++ bin/install_ossim.sh
@@ -137,7 +137,7 @@ chown "$USER_NAME.$USER_NAME" "$USER_HOME/Desktop/imagelinker.desktop"
 sed -i -e 's/^Name=imagelinker/Name=Imagelinker/' \
    "$USER_HOME/Desktop/imagelinker.desktop"
 # keep out of unrelated menus
-sed -i -e 's/Network;Graphics;//' /usr/share/applications/merkaartor.desktop
+sed -i -e 's/Network;Graphics;//' /usr/share/applications/imagelinker.desktop
 
 cp /usr/share/applications/ossimplanet.desktop "$USER_HOME/Desktop/"
 chown "$USER_NAME.$USER_NAME" "$USER_HOME/Desktop/ossimplanet.desktop"
