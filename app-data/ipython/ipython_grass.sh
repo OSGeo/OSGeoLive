5572f2243ed67c2676ad5f4c42ca0fb391056c49
Massimo Di Stefano <massimodisasha@yahoo.it>
2014-08-01 17:33:36 +0000

    added support data for ipython notebook (wrapper script to start grass session from inside an ipython notebook and relative launchers)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11668 7da46fd8-bd2a-0410-a152-cf9810cf5b10

---
+++ app-data/ipython/ipython_grass.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# Script to start minimal GRASS session for ipython
+#
+
+# Conditionally add $LD_LIBRARY_PATH and $PYTHONPATH only if they already exist.
+# An empty ":" at the end can add `pwd` to the path, which is generally frowned upon.
+if [ -n "$LD_LIBRARY_PATH" ] ; then
+   LD_LIBRARY_PATH="/usr/lib/grass64/lib:$LD_LIBRARY_PATH"
+else
+   LD_LIBRARY_PATH="/usr/lib/grass64/lib"
+fi
+
+if [ -n "$PYTHONPATH" ] ; then
+   PYTHONPATH="/usr/lib/grass64/etc/python:$PYTHONPATH"
+else
+   PYTHONPATH="/usr/lib/grass64/etc/python"
+fi
+
+GISBASE="/usr/lib/grass64"
+PATH="$GISBASE/bin:$GISBASE/scripts:$PATH"
+GIS_LOCK="$$"
+GISRC="/home/$USER/.grassrc6"
+export LD_LIBRARY_PATH PYTHONPATH GISBASE PATH GIS_LOCK GISRC
+
+# note: $GISDBASE is generally a g.gisenv variable stored in .grassrc6, not a shell variable anymore
+GISDBASE="/home/$USER/grassdata"
+export GISDBASE
+
+GRASS_TRANSPARENT=TRUE
+GRASS_TRUECOLOR=TRUE
+GRASS_PNG_COMPRESSION=9
+GRASS_PNG_AUTO_WRITE=TRUE
+export GRASS_TRANSPARENT GRASS_TRUECOLOR GRASS_PNG_COMPRESSION GRASS_PNG_AUTO_WRITE
+
+mkdir -p /home/user/ossim/workspace
+
+ipython notebook --port=12345 --no-browser \
+   --notebook-dir=/home/user/ossim/workspace/geo-notebook \
+   --ip='*'
+
