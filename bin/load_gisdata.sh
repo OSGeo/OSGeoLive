fc4e72292592bf93d96bc60c6db532a9534819c0
Hamish Bowman <hamish_b yahoo com>
2014-08-11 22:24:41 +0000

    don't copy zip file onto disc
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11829 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/load_gisdata.sh
+++ bin/load_gisdata.sh
@@ -159,7 +159,7 @@ mv HYP_50M_SR_W.tif HYP_50M_SR_W_orig.tif
 gdal_translate HYP_50M_SR_W_orig.tif HYP_50M_SR_W.tif \
   -co compress=jpeg -co photometric=ycbcr -co tiled=yes
 
-rm HYP_50M_SR_W_orig.tif
+rm HYP_50M_SR_W_orig.tif "$RFILE"
 
 mv HYP_* "$NE2_DATA_FOLDER"
 
