7d5efbe6ac03f97c57c03c5f082950df3995f7f3
Hamish Bowman <hamish_b yahoo com>
2014-08-14 23:51:42 +0000

    avoid deegree starting firefox as root (#1389), and other deegree cleanups
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@11865 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- app-conf/deegree/deegree_start.sh
+++ app-conf/deegree/deegree_start.sh
@@ -9,11 +9,17 @@
 ## Credits: Judit Mays, Johannes Kuepper, Danilo Bretschneider
 #########################
 
-export DEEGREE_WORKSPACE_ROOT="/usr/local/share/deegree"
+DEEGREE_WORKSPACE_ROOT="/usr/local/share/deegree"
+export DEEGREE_WORKSPACE_ROOT
 DEE_DIR="/usr/local/lib/deegree-webservices-3.3.10"
 
+# live disc's username is "user"
+if [ -z "$USER_NAME" ] ; then
+   USER_NAME="user"
+fi
+
 ## start tomcat (and deegree webapps)
-cd $DEE_DIR
+cd "$DEE_DIR"
 mkdir -p logs
 ./bin/catalina.sh start
 
@@ -21,4 +27,6 @@ mkdir -p logs
 sleep 5
 
 ## open firefox with deegree 3 console
-firefox -new-tab http://localhost:8033
+sudo -u "$USER_NAME" \
+   firefox -new-tab http://localhost:8033
+
