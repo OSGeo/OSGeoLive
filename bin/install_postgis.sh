d6d4a9ebea83c18602ca98775fee9c4f6e9abcf0
Brian M Hamlin <screenlight@mac.com>
2014-12-22 07:40:43 +0000

    more comment
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@12042 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_postgis.sh
+++ bin/install_postgis.sh
@@ -31,8 +31,8 @@ USER_HOME="/home/$USER_NAME"
 PG_VERSION="9.3"
 
 #debug:
-#echo "#DEBUG The locale settings are currently:"
-#locale
+echo "#DEBUG The locale settings are currently:"
+locale
 
 # DB is created in the current locale, which was reset to "C". Put it
 #  back to UTF so the templates will be created using UTF8 encoding.
@@ -40,8 +40,9 @@ unset LC_ALL
 update-locale LC_ALL=en_US.UTF-8
 
 # another debug
-#echo "#DEBUG The locale settings updated:"
-#locale
+echo "#DEBUG The locale settings updated:"
+locale
+echo "------------------------------------"
 
 ##-- TODO pgdg repo ?
 apt-get install --yes postgis "postgresql-$PG_VERSION-postgis-2.1"
