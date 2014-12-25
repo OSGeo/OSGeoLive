df39e420f946bd4505eea47ed0bfaf901fe90c4f
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2014-12-25 17:37:18 +0000

    switching to GRASS7 for nightly builds
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@12080 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/inchroot_nightly.sh
+++ bin/inchroot_nightly.sh
@@ -117,7 +117,8 @@ export USER_NAME
 ./install_mapbender3.sh
 ./install_geokettle.sh
 ./install_gmt.sh
-./install_grass.sh
+# ./install_grass.sh
+./install_grass7.sh
 ./install_qgis.sh
 ./install_qgis_mapserver.sh
 ./install_saga.sh
