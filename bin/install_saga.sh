2cbb469e4ee56800195f9e3cef0c9e12155ea238
Johan Van de Wauw <johan.vandewauw@gmail.com>
2015-01-18 22:34:07 +0000

    Remove old cruft (commented lines)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@12181 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/install_saga.sh
+++ bin/install_saga.sh
@@ -5,7 +5,6 @@
 # Created by Johan Van de Wauw on 2010-07-02
 # Copyright (c) 2009 The Open Source Geospatial Foundation.
 # This script licensed under the GNU LGPL version >= 2.1.
-# lucid: saga 2.0.4 is provided in ubuntugis and ubuntugis/unstable
 
 ./diskspace_probe.sh "`basename $0`" begin
 ####
@@ -15,8 +14,6 @@ if [ -z "$USER_NAME" ] ; then
    USER_NAME="user"
 fi
 USER_HOME="/home/$USER_NAME"
-
-#add-apt-repository --yes ppa:johanvdw/sagacvs
 
 apt-get -q update
 apt-get --assume-yes install saga libsaga
@@ -36,17 +33,9 @@ ln -s /usr/local/share/saga "$USER_HOME"/saga
 ln -s /usr/local/share/saga /etc/skel/saga
  
  
-# # Desktop icon
-# # temporarily disabled: a custom .desktop file with a 64x64 logo is used - to be fixed in the package
-# wget -nv http://zadeh.ugent.be/~johan/saga/saga_gui_64x64.png \
-#    --output-document=/usr/share/pixmaps/saga_gui_64x64.png
-# wget -nv http://zadeh.ugent.be/~johan/saga/saga_gui.desktop \
-#    --output-document=/usr/share/applications/saga_gui.desktop
-
+# Desktop icon
 cp /usr/share/applications/saga.desktop "$USER_HOME/Desktop/"
 chown "$USER_NAME:$USER_NAME" "$USER_HOME/Desktop/saga.desktop"
-
-#add-apt-repository --yes --remove ppa:johanvdw/sagacvs
 
 ####
 "$BUILD_DIR"/diskspace_probe.sh "`basename $0`" end
