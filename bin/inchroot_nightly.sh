a0335e5422d4706a999a1a571585ec87db6c169c
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2015-02-11 19:57:57 +0000

    inchroot cleanup
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@12327 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/inchroot_nightly.sh
+++ bin/inchroot_nightly.sh
@@ -117,7 +117,6 @@ export USER_NAME
 ./install_mapbender3.sh
 ./install_geokettle.sh
 ./install_gmt.sh
-# ./install_grass.sh
 ./install_grass7.sh
 ./install_qgis.sh
 ./install_qgis_mapserver.sh
@@ -132,7 +131,6 @@ export USER_NAME
 ./install_zygrib.sh
 ./install_liblas.sh
 ./install_gpsdrive.sh
-# ./install_mapfish.sh
 ./install_openlayers.sh
 ./install_leaflet.sh
 ./install_R.sh
