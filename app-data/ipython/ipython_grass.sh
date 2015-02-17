9ee79c9d6e6fa9989a1eec5a3284f4778740f276
Massimo Di Stefano <massimodisasha@yahoo.it>
2015-02-17 04:35:49 +0000

    fixed grass environment (was pointing to grass 64 instead of new available grass70)
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@12366 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- app-data/ipython/ipython_grass.sh
+++ app-data/ipython/ipython_grass.sh
@@ -6,22 +6,22 @@
 # Conditionally add $LD_LIBRARY_PATH and $PYTHONPATH only if they already exist.
 # An empty ":" at the end can add `pwd` to the path, which is generally frowned upon.
 if [ -n "$LD_LIBRARY_PATH" ] ; then
-   LD_LIBRARY_PATH="/usr/lib/grass64/lib:$LD_LIBRARY_PATH"
+   LD_LIBRARY_PATH="/usr/lib/grass70/lib:$LD_LIBRARY_PATH"
 else
-   LD_LIBRARY_PATH="/usr/lib/grass64/lib"
+   LD_LIBRARY_PATH="/usr/lib/grass70/lib"
 fi
 
 if [ -n "$PYTHONPATH" ] ; then
-   PYTHONPATH="/usr/lib/grass64/etc/python:$PYTHONPATH"
+   PYTHONPATH="/usr/lib/grass70/etc/python:$PYTHONPATH"
 else
-   PYTHONPATH="/usr/lib/grass64/etc/python"
+   PYTHONPATH="/usr/lib/grass70/etc/python"
 fi
 
 
-GISBASE="/usr/lib/grass64"
+GISBASE="/usr/lib/grass70"
 PATH="$GISBASE/bin:$GISBASE/scripts:$PATH"
 GIS_LOCK="$$"
-GISRC="/home/$USER/.grassrc6"
+GISRC="/home/$USER/.grass7/rc"
 export LD_LIBRARY_PATH PYTHONPATH GISBASE PATH GIS_LOCK GISRC
 
 # note: $GISDBASE is generally a g.gisenv variable stored in .grassrc6, not a shell variable anymore
