c85e23e85cc75c8b9ab7242fb8ff9a071f197014
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2015-02-24 14:23:07 +0000

    sync full iso build script with the used code from debug script
    git-svn-id: http://svn.osgeo.org/osgeo/livedvd/gisvm/trunk@12396 7da46fd8-bd2a-0410-a152-cf9810cf5b10

--- bin/build_full_iso.sh
+++ bin/build_full_iso.sh
@@ -4,6 +4,7 @@
 # Purpose: Creating OSGeoLive as an Ubuntu customization
 # 	   https://help.ubuntu.com/community/LiveCDCustomization
 # Authors: Alex Mandel <tech_dev@wildintellect.com>
+#	   Angelos Tzotsos <tzotsos@gmail.com>
 #
 #
 #############################################################################
@@ -32,14 +33,9 @@
 # Checkout a copy of the svn, change to the bin folder
 # Run as 
 # sudo ./build_full_iso.sh /path/to/file.iso 2>&1 | tee ~/build_full_iso.log
-# Expects 1 argument, the path to the mini iso
 
-#DIR="/home/user/gisvm/bin"
-#SVN_DIR="/home/user/gisvm"
 VERSION=`cat ../VERSION.txt`
 PACKAGE_NAME="osgeo-live"
-#cd "$SVN_DIR"
-#REVISION=`svn info | grep "Revision" | sed 's/Revision: //'`
 CUR_DIR=`pwd`
 
 if [ $1 ]; then
@@ -50,13 +46,23 @@ else
     MINI_ISO_NAME="$PACKAGE_NAME-mini-$VERSION.iso"
 fi
 ISO_NAME="$PACKAGE_NAME-$VERSION"
+IMAGE_NAME=OSGEOLIVE`echo "$VERSION" | sed -e 's/\.//' -e 's/rc.*//'`
 
 echo
-echo "Installing genisoimage"
+echo "==============================================================="
+echo "Start Building $ISO_NAME"
+echo "==============================================================="
+
+#Some initial cleaning
+#  when run as root, ~ is /root/.
+rm -rf ~/livecdtmp/edit
+rm -rf ~/livecdtmp/lzfiles
+
+echo
+echo "Installing squashfs and genisoimage"
 echo "==================================="
 
-sudo apt-get install genisoimage
-
+sudo apt-get install --yes squashfs-tools genisoimage lzip
 
 echo
 echo "Extract mini iso"
@@ -66,57 +72,73 @@ mkdir -p ~/livecdtmp
 cd ~/livecdtmp
 
 #Start with a fresh copy
-#Mount the Desktop .iso to mnt2 to not interfere with other builds
-mkdir mnt2
-sudo mount -o loop "$MINI_ISO_NAME" mnt2
+#Mount the Desktop .iso
+mkdir mnt
+sudo mount -o loop "$MINI_ISO_NAME" mnt
 
 #Extract .iso contents into dir 'extract-cd' 
 mkdir "extract-cd"
-rsync -a mnt2/ "extract-cd"
+rsync --exclude=/casper/filesystem.squashfs -a mnt/ "extract-cd"
 
 echo
-echo "Download Windows and Mac Installers"
-echo "==================================="
+echo "Extracting squashfs from OSGeoLive mini image"
+echo "============================================="
+#Extract the SquashFS filesystem 
+sudo unsquashfs mnt/casper/filesystem.squashfs
+sudo mv squashfs-root edit
+
+echo
+echo "Download Windows and Mac Installers in chroot"
+echo "============================================="
 
 # First remove index.htm files if they exist, otherwise you won't see the
 # directory of files.
+sudo chroot edit rm -f /var/www/html/WindowsInstallers/index.html
+sudo chroot edit rm -f /var/www/html/MacInstallers/index.html
+sudo chroot edit rmdir /var/www/html/WindowsInstallers
+sudo chroot edit rmdir /var/www/html/MacInstallers
+sudo chroot edit ln -s /media/cdrom/WindowsInstallers /var/www/html/WindowsInstallers
+sudo chroot edit ln -s /media/cdrom/MacInstallers  /var/www/html/MacInstallers
+
 cd extract-cd
-
-rm -f ./var/www/html/WindowsInstallers/index.html
-rm -f ./var/www/html/MacInstallers/index.html
-rmdir ./var/www/html/WindowsInstallers
-rmdir ./var/www/html/MacInstallers
-
 sh "$CUR_DIR"/load_win_installers.sh
 sh "$CUR_DIR"/load_mac_installers.sh
-
-ln -s /media/cdrom/WindowsInstallers ./var/www/html/WindowsInstallers
-ln -s /media/cdrom/MacInstallers  ./var/www/html/MacInstallers
-
-cd ..
+cd ~/livecdtmp
 
 echo
 echo "Regenerating manifest..."
 echo "======================================"
 #Regenerate manifest 
-#chmod +w extract-cd/casper/filesystem.manifest
-#sudo cp extract-cd/casper/filesystem.manifest \
-#   extract-cd/casper/filesystem.manifest-desktop
-#sudo sed -i '/ubiquity/d' \
-#   extract-cd/casper/filesystem.manifest-desktop
-#sudo sed -i '/casper/d' \
-#   extract-cd/casper/filesystem.manifest-desktop
+chmod +w extract-cd/casper/filesystem.manifest
+sudo chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > \
+   extract-cd/casper/filesystem.manifest
+sudo cp extract-cd/casper/filesystem.manifest \
+   extract-cd/casper/filesystem.manifest-desktop
+sudo sed -i '/ubiquity/d' \
+   extract-cd/casper/filesystem.manifest-desktop
+sudo sed -i '/casper/d' \
+   extract-cd/casper/filesystem.manifest-desktop
+
+echo
+echo "Compressing filesystem..."
+echo "======================================"
+#Compress filesystem
+sudo rm extract-cd/casper/filesystem.squashfs
+sudo mksquashfs edit extract-cd/casper/filesystem.squashfs
 
 echo
 echo "Calculating new filesystem size..."
 echo "======================================"
 #Update the filesystem.size file, which is needed by the installer:
 # TODO: get it to run as sudo no sudo su
-#chmod +w extract-cd/casper/filesystem.size
-#printf $(sudo du -sx --block-size=1 edit | cut -f1) > \
-#   extract-cd/casper/filesystem.size
-#chmod -w extract-cd/casper/filesystem.size
+chmod +w extract-cd/casper/filesystem.size
+printf $(sudo du -sx --block-size=1 edit | cut -f1) > \
+   extract-cd/casper/filesystem.size
+chmod -w extract-cd/casper/filesystem.size
 
+#this is now compressed in squashfs so we delete to save VM disk space
+cd ~/livecdtmp
+sudo rm -rf edit
 
 echo
 echo "Calculating new md5 sums..."
@@ -141,6 +163,11 @@ echo "======================================"
 #Clear things up and prepare for next build
 cd ~/livecdtmp
 sudo rm -rf extract-cd
-sudo umount mnt2
-sudo rm -rf mnt2
+sudo umount mnt
+sudo rm -rf mnt
+
+echo
+echo "==============================================================="
+echo "Finished building $ISO_NAME"
+echo "==============================================================="
 
