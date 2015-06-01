415d9ec0814ae1350ad2747ddb2a8905d449ac15
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2015-06-01 22:04:08 +0300

    Cleanup diskspace_probe.sh

--- bin/diskspace_probe.sh
+++ bin/diskspace_probe.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
-#   diskspace_probe.sh
-#      by Hamish Bowman, 23 June 2013
-#   Copyright (c) 2013  Hamish Bowman, and The Open Source Geospatial Foundation
-#   Licensed under the GNU LGPL >=2.1.
-#   Previously this code existed in OSGeo Live DVD version 4-6's main.sh
+#############################################################################
 #
-# PURPOSE: Show how much disk space is free at the start and end of each install script.
+# Purpose: Show how much disk space is free at the start and end of each 
+#     install script.
+#
+# Author:  Hamish Bowman, 23 June 2013
+#
+#############################################################################
+# Copyright (c) 2013  Hamish Bowman
+# Copyright (c) 2013-2015 Open Source Geospatial Foundation (OSGeo)
+#
+# Licensed under the GNU LGPL version >= 2.1.
+# Previously this code existed in OSGeo Live DVD version 4-6's main.sh
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+#############################################################################
 #
 # USAGE:   diskspace_probe.sh  <project_name> [begin|end]
 #		if "begin" or "end" is not given it just does the
 #		  df (for mid-script debug, etc).
 #
-
-
-#debug: run this on the chroot:
+# debug: run this on the chroot:
 # df -h --print-type
-
+#############################################################################
 
 if [ $# -lt 1 ] || [ $# -gt 2 ] ; then
    echo "USAGE:   diskspace_probe.sh <project_name> [begin|end]" 1>&2
