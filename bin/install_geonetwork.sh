e260e68c9a7b4c6ff46f3a7386c25e52e3f9b765
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2015-08-25 10:58:20 +0300

    Revert "GeoNetwork / Update to version 3.0.0."
    This reverts commit 9e1f09baf1aa6f178fc4324a81db6d23afe8299b.

--- bin/install_geonetwork.sh
+++ bin/install_geonetwork.sh
@@ -26,24 +26,24 @@
 #
 # About:
 # =====
-# This script will install geonetwork into OSGeo live
-# stable version: v3.0.0
-# based on Jetty + GeoNetwork + H2
+# This script will install geonetwork into Xubuntu
+# stable version: v2.10.4 (23 Dec 2014) (also the manuals)
+# based on Jetty + Geoserver + H2
 # Installed at /usr/local/lib/geonetwork
-# Port number = 8880
+# Port number =8880
 #
 # To start geonetwork
 # cd /usr/local/lib/geonetwork/bin
-# ./startup.sh 
+# ./start-geonetwork.sh 
 #
 # To stop geonetwork
 # cd /usr/local/lib/geonetwork/bin
-# ./shutdown.sh
+# ./stop-geonetwork.sh
 #
 # To enter geonetwork, start browser with url:
 # http://localhost:8880/geonetwork
 #
-# GeoNetwork version 3.0.0 runs with java 7 or higher.
+# GeoNetwork version 2.10.4 runs with java-sun-1.6 or higher.
 # It can be installed into servlet containers: jetty and tomcat. Jetty is   
 # bundled with the installer.
 
@@ -57,8 +57,8 @@ if [ -z "$USER_NAME" ] ; then
 fi
 USER_HOME="/home/$USER_NAME"
 
-GEONETWORK_VERSION=3.0.0-0
-GEONETWORK_VERSION_FOLDER=3.0.0
+GEONETWORK_VERSION=2.10.4-0
+GEONETWORK_VERSION_FOLDER=2.10.4
 
 TMP="/tmp/build_geonetwork"
 INSTALL_FOLDER="/usr/local/lib"
@@ -90,15 +90,37 @@ else
      -O geonetwork-install-$GEONETWORK_VERSION.jar
 fi
 
+# get geonetwork doco - not just yet - has to be uploaded
+#if [ -f "GeoNetwork_opensource_v264_Manual.pdf" ]
+#then
+#   echo "GeoNetwork_opensource_v264_Manual.pdf has already been downloaded."
+#else
+#   wget --progress=dot:binary \
+#      http://transact.dl.sourceforge.net/project/geonetwork/Documentation/v2.6.4/GeoNetwork_opensource_v264_Manual.pdf
+#fi
+
+
 ## Get Install config files ##
-# Those files contains information about default ports
-# and need to be updated to properly setup the node on the OSGeo live machine
+# Download XML file that defines install location
+# Download jetty.xml file to listen on all addresses and change Port to 8880 
+#   config-gui.xml file to find default GeoServer layers Port 8880
+#   start-geonetwork.sh file with mods to work from any directory
+#   stop-geonetwork.sh file with mods to work from any directory
+#   iso-19139-basins-in-africa.mef file - changed port number 8880
+#   iso-19139-physiographic.mef file - changed port number 8880
+
 FILES="
 install.xml
 jetty.xml
-startup.sh
-shutdown.sh
+jetty-webapps.xml
+config-gui.xml
+mapviewer.wmc
+start-geonetwork.sh
+stop-geonetwork.sh
 data-db-default.sql
+index-fields.xsl
+iso-19139-basins-in-africa.mef
+iso-19139-physiographic.mef
 "
 
 for FILE in $FILES ; do
@@ -109,15 +131,25 @@ done
 
 ## Install Application ##
 if [ -d "$GEONETWORK_FOLDER" ] ; then
-   ( cd "$GEONETWORK_FOLDER/bin"; ./shutdown.sh )
+   ( cd "$GEONETWORK_FOLDER/bin"; ./stop-geonetwork.sh )
 fi
 java -jar geonetwork-install-$GEONETWORK_VERSION.jar install.xml
 
 
 cp -f jetty.xml "$GEONETWORK_FOLDER/jetty/etc/jetty.xml"
+cp -f jetty-webapps.xml "$GEONETWORK_FOLDER/jetty/etc/jetty-webapps.xml"
+cp -f config-gui.xml "$GEONETWORK_FOLDER/web/geonetwork/WEB-INF/config-gui.xml"
+cp -f mapviewer.wmc "$GEONETWORK_FOLDER/web/geonetwork/maps/mapviewer.wmc"
+# HTML5UI is using path with port number so we need to modify the settings in SQL
+# Needed until https://github.com/geonetwork/core-geonetwork/issues/138
 cp -f data-db-default.sql "$GEONETWORK_FOLDER/web/geonetwork/WEB-INF/classes/setup/sql/data/."
-cp -f startup.sh "$GEONETWORK_FOLDER/bin/startup.sh"
-cp -f shutdown.sh "$GEONETWORK_FOLDER/bin/shutdown.sh"
+cp -f start-geonetwork.sh "$GEONETWORK_FOLDER/bin/start-geonetwork.sh"
+cp -f stop-geonetwork.sh "$GEONETWORK_FOLDER/bin/stop-geonetwork.sh"
+# copy special mef files into place so that port numbers for files available
+# for download are on 8880 when sample data are loaded
+cp -f iso-19139-*.mef "$GEONETWORK_FOLDER/web/geonetwork/WEB-INF/data/config/schema_plugins/iso19139/sample-data"
+# index iso19139 with download=on and dynamic=on
+cp -f index-fields.xsl "$GEONETWORK_FOLDER/web/geonetwork/WEB-INF/data/config/schema_plugins/iso19139/index-fields.xsl"
 
 # fix permissions on installed software
 #   what's really needed here? the logs for sure, the rest are guesses
