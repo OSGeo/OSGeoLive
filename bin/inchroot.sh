c8d742f40b92a611c745fe12cca13edc8240ab93
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2015-12-16 16:57:02 +0200

    Disabled ossim until plugins rebuilt against new gdal

--- bin/inchroot.sh
+++ bin/inchroot.sh
@@ -162,7 +162,7 @@ export USER_NAME
 ./install_openlayers.sh
 ./install_leaflet.sh
 ./install_R.sh
-./install_ossim.sh "$ARCH"
+# ./install_ossim.sh "$ARCH"
 ./install_osgearth.sh
 ./install_spatialite.sh
 ./install_zoo-project.sh
