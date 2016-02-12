f2235c9f12d0bd71a5863deb3754a9e84ddaeee3
epifanio <epiesasha@gmail.com>
2016-02-11 21:34:23 -0500

    revert changes, moving the updated files to PR: https://github.com/OSGeo/OSGeoLive/pull/66

--- app-conf/R/installRpackages.r
+++ app-conf/R/installRpackages.r
@@ -6,24 +6,19 @@ core <- c("classInt", "DCluster", "deldir", "geoR", "gstat", "maptools",
 
 #non-spatial <- c("RPostgresql","RSQLite","RODBC")
 
-#packagelist <- core
+packagelist <- core
 
-#for (i in packagelist) {
-#	#Generic Version followed by Australian Repos
-#	#install.packages(i, repos= "http://cran.r-project.org", lib = "/usr/local/lib/R/site-library/" , dependencies = TRUE)
-#	#For AU builds
-#	#install.packages(i, repos= "http://cran.ms.unimelb.edu.au/", lib = "/usr/local/lib/R/site-library/")
-#	#For EU builds
-#	#install.packages(i, repos= "http://stat.ethz.ch/CRAN/", lib = "/usr/local/lib/R/site-library/")
-#	#For US builds
-#	install.packages(i, repos= "http://cran.rstudio.com/", lib = "/usr/local/lib/R/site-library/")
-#	output <- paste("Finished installing",i,sep=" ")
-#	print(output)
-#}
-
-install.packages(c('repr', 'IRkernel', 'IRdisplay'),
-                 repos = c('http://irkernel.github.io/', 'http://cran.us.r-project.org'))
-IRkernel::installspec()
-
+for (i in packagelist) {
+	#Generic Version followed by Australian Repos
+	#install.packages(i, repos= "http://cran.r-project.org", lib = "/usr/local/lib/R/site-library/" , dependencies = TRUE)
+	#For AU builds
+	#install.packages(i, repos= "http://cran.ms.unimelb.edu.au/", lib = "/usr/local/lib/R/site-library/")
+	#For EU builds
+	#install.packages(i, repos= "http://stat.ethz.ch/CRAN/", lib = "/usr/local/lib/R/site-library/")
+	#For US builds
+	install.packages(i, repos= "http://cran.rstudio.com/", lib = "/usr/local/lib/R/site-library/")
+	output <- paste("Finished installing",i,sep=" ")
+	print(output)
+}
 
 q()
