81cad69c1c2315b97dbfc7d59abfa396c6774e7e
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-03-01 14:48:39 +0200

    Provide more time to GeoNetwork startup process

--- app-conf/geonetwork/startup.sh
+++ app-conf/geonetwork/startup.sh
@@ -14,6 +14,6 @@ export JETTY_HOME=.
  
 ${JAVA_HOME}/bin/java -Xms512m -Xmx1024m -Xss2M -XX:MaxPermSize=128m -DSTOP.PORT=8879 -Djeeves.filecharsetdetectandconvert=enabled -Dmime-mappings=../web/geonetwork/WEB-INF/mime-types.properties -Djava.awt.headless=true -DSTOP.KEY=geonetwork -jar start.jar > logs/output.log 2>&1 &
 
-(sleep 10; echo "25"; sleep 10; echo "50"; sleep 10; echo "75"; sleep 10; echo "100") | zenity --progress --auto-close --text "GeoNetwork starting"
+(sleep 15; echo "25"; sleep 15; echo "50"; sleep 15; echo "75"; sleep 15; echo "100") | zenity --progress --auto-close --text "GeoNetwork starting"
 
 firefox http://localhost:8880/geonetwork/
