5ea4b9299e7847c5fc7d4ffbe6b7130a2986d4cc
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-05-11 20:08:15 +0300

    Starting port to Xenial, disabling all installers until they are reviewed

--- bin/inchroot.sh
+++ bin/inchroot.sh
@@ -127,61 +127,61 @@ export USER_NAME
 ./install_jupyter.sh
 ./install_django.sh
 
-./install_geoserver.sh
-./install_geomajas.sh
-./install_geonetwork.sh
-./install_deegree.sh
-./install_52nWPS.sh
-./install_kosmo.sh "$ARCH"
-./install_udig.sh "$ARCH"
-./install_openjump.sh
-./install_postgis.sh
-./install_osm.sh
-./load_postgis.sh
-./install_pgrouting.sh
-./install_sahana.sh
-./install_ushahidi.sh
-./install_mapserver.sh
-./install_mapbender3.sh
-./install_geokettle.sh
-./install_gmt.sh
-./install_grass.sh
-./install_qgis.sh
-./install_qgis_mapserver.sh
-./install_saga.sh
-./install_mapnik.sh
-./install_mapproxy.sh
-./install_mapslicer.sh
-./install_marble.sh
-./install_opencpn.sh
-./install_prune.sh
-./install_viking.sh
-./install_zygrib.sh
-./install_liblas.sh
-./install_openlayers.sh
-./install_leaflet.sh
-./install_R.sh
-./install_ossim.sh "$ARCH"
-./install_osgearth.sh
-./install_spatialite.sh
-./install_zoo-project.sh
-./install_52nSOS.sh
-./install_otb.sh
-./install_rasdaman.sh
-./install_tinyows.sh
-./install_pycsw.sh
-./install_geomoose.sh
-./install_mb-system.sh
-./install_gvsig.sh "$ARCH"
-./install_tilemill.sh
-./install_eoxserver.sh
-./install_cartaro.sh
-./install_iris.sh
-./install_ncWMS.sh
-./install_geonode.sh
-./install_cesium.sh
-./install_istsos.sh
-./install_javaworldwind.sh
+# ./install_geoserver.sh
+# ./install_geomajas.sh
+# ./install_geonetwork.sh
+# ./install_deegree.sh
+# ./install_52nWPS.sh
+# ./install_kosmo.sh "$ARCH"
+# ./install_udig.sh "$ARCH"
+# ./install_openjump.sh
+# ./install_postgis.sh
+# ./install_osm.sh
+# ./load_postgis.sh
+# ./install_pgrouting.sh
+# ./install_sahana.sh
+# ./install_ushahidi.sh
+# ./install_mapserver.sh
+# ./install_mapbender3.sh
+# ./install_geokettle.sh
+# ./install_gmt.sh
+# ./install_grass.sh
+# ./install_qgis.sh
+# ./install_qgis_mapserver.sh
+# ./install_saga.sh
+# ./install_mapnik.sh
+# ./install_mapproxy.sh
+# ./install_mapslicer.sh
+# ./install_marble.sh
+# ./install_opencpn.sh
+# ./install_prune.sh
+# ./install_viking.sh
+# ./install_zygrib.sh
+# ./install_liblas.sh
+# ./install_openlayers.sh
+# ./install_leaflet.sh
+# ./install_R.sh
+# ./install_ossim.sh "$ARCH"
+# ./install_osgearth.sh
+# ./install_spatialite.sh
+# ./install_zoo-project.sh
+# ./install_52nSOS.sh
+# ./install_otb.sh
+# ./install_rasdaman.sh
+# ./install_tinyows.sh
+# ./install_pycsw.sh
+# ./install_geomoose.sh
+# ./install_mb-system.sh
+# ./install_gvsig.sh "$ARCH"
+# ./install_tilemill.sh
+# ./install_eoxserver.sh
+# ./install_cartaro.sh
+# ./install_iris.sh
+# ./install_ncWMS.sh
+# ./install_geonode.sh
+# ./install_cesium.sh
+# ./install_istsos.sh
+# ./install_javaworldwind.sh
 
 ./load_gisdata.sh
 ./install_docs.sh "$BUILD_MODE"
