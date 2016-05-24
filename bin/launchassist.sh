a6b8ad2140d36f455b825014a28eb0d97d6bdce2
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-05-25 00:24:54 +0300

    Removing more unused scripts

--- bin/launchassist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh/
-#launchassist
-#Takes script/app to launch as an arg an makes sure it's run as sudo.
-#Seems to alleviate strange behavior of desktop icons not launching apps properly
-PASSWORD=user
-echo "Launching $1"
-echo $PASSWORD | sudo -S $1
