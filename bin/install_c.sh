8d30d090b6fac9c2a741677b886e9dd51d0b5ac7
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-05-25 17:31:25 +0300

    Moved parts of setup.sh to platform specific installers - C and Python first

---
+++ bin/install_c.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#############################################################################
+#
+# Purpose: This script will install C development tools and libraries to use in
+# OSGeoLive.
+#
+#############################################################################
+# Copyright (c) 2016 Open Source Geospatial Foundation (OSGeo)
+#
+# Licensed under the GNU LGPL.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+#############################################################################
+
+./diskspace_probe.sh "`basename $0`" begin
+BUILD_DIR=`pwd`
+
+apt-get -q update
+
+# Install C development packages
+apt-get install --yes build-essential cmake pkg-config
+
+# removed from list:
+# pbuilder fakeroot devscripts debhelper svn-buildpackage lintian dpkg-dev 
+
+
+
+"$BUILD_DIR"/diskspace_probe.sh "`basename $0`" end
---
+++ bin/install_python.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#############################################################################
+#
+# Purpose: This script will install python development tools and libraries to use in
+# OSGeoLive.
+#
+#############################################################################
+# Copyright (c) 2016 Open Source Geospatial Foundation (OSGeo)
+#
+# Licensed under the GNU LGPL.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 2.1 of the License,
+# or any later version.  This library is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details, either
+# in the "LICENSE.LGPL.txt" file distributed with this software or at
+# web page "http://www.fsf.org/licenses/lgpl.html".
+#############################################################################
+
+./diskspace_probe.sh "`basename $0`" begin
+BUILD_DIR=`pwd`
+
+apt-get -q update
+
+# install the python .deb maker
+apt-get install --yes python-all-dev
+
+# removed from list: python-stdeb
+
+"$BUILD_DIR"/diskspace_probe.sh "`basename $0`" end
