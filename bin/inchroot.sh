06f3e3f8eb6857877bbb0f66c92e75863e0a6133
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-05-31 01:55:08 +0300

    Disabling broken installers: mb-system and opencpn

--- bin/inchroot.sh
+++ bin/inchroot.sh
@@ -143,7 +143,7 @@ export USER_NAME
 ./install_mapserver.sh
 ./install_tinyows.sh
 ./install_gmt.sh
-./install_mb-system.sh
+# ./install_mb-system.sh
 ./install_mapnik.sh
 ./install_otb.sh
 ./install_liblas.sh
@@ -153,7 +153,7 @@ export USER_NAME
 ./install_qgis_mapserver.sh
 # ./install_zoo-project.sh
 ./install_marble.sh
-./install_opencpn.sh
+# ./install_opencpn.sh
 ./install_viking.sh
 ./install_zygrib.sh
 
