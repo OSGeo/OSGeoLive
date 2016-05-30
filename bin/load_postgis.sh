cc9d9a0b6246567b03595a10a0ba82d354dd2fb4
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-05-31 00:05:22 +0300

    PGRouting reviewed

--- bin/load_postgis.sh
+++ bin/load_postgis.sh
@@ -71,13 +71,13 @@ sudo -u $POSTGRES_USER psql osm_local -c 'create extension postgis;'
 
 # Kosmo, gpsdrive, please update your API calls ....
 cp "$BUILD_DIR"/../app-conf/postgis/legacy*.sql \
-  /usr/share/postgresql/9.4/contrib/postgis-2.2/
+  /usr/share/postgresql/9.5/contrib/postgis-2.2/
 
 sed -i -e 's/postgis-2.0/postgis-2.2/' \
-  /usr/share/postgresql/9.4/contrib/postgis-2.2/legacy*.sql
+  /usr/share/postgresql/9.5/contrib/postgis-2.2/legacy*.sql
 
 sudo -u $POSTGRES_USER psql osm_local \
-  -f /usr/share/postgresql/9.4/contrib/postgis-2.2/legacy_minimal.sql
+  -f /usr/share/postgresql/9.5/contrib/postgis-2.2/legacy_minimal.sql
 
 #sudo -u $POSTGRES_USER createdb osm_local_smerc
 #sudo -u $POSTGRES_USER psql osm_local_smerc -c 'create extension postgis;'
