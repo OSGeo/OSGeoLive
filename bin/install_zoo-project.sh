1550656955a82b5800f841a1b3bb8b0bea7834de
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-06-12 01:36:01 +0300

    Updated install_zoo-project.sh (#1710)

--- bin/install_zoo-project.sh
+++ bin/install_zoo-project.sh
@@ -40,15 +40,33 @@ apt-get --assume-yes install libmozjs185-1.0 zoo-kernel zoo-services zoo-api lib
 
 # Download ZOO Project deb file.
 wget -N --progress=dot:mega \
-   "http://zoo-project.org/dl/examples.7z"
+   -O "$TMP_DIR"/examples.7z
+   "http://download.osgeo.org/livedvd/data/zoo/examples.7z"
 
 7zr x examples.7z 
 cp -r examples /var/www/html/zoo-demo
 chmod -R 755 /var/www/html/zoo-demo
 
-# TODO: Add desktop file
-# cp /usr/share/applications/zoo-project.desktop "$USER_HOME/Desktop/"
-# chown "$USER_NAME:$USER_NAME" "$USER_HOME/Desktop/zoo-project.desktop"
+wget --progress=dot:mega \
+  -O /usr/share/icons/zoo-icon.png \
+  "http://download.osgeo.org/livedvd/data/zoo/zoo-icon.png"
+
+# Add desktop file
+cat << EOF > /usr/share/applications/zoo-project.desktop
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=ZOO Project
+Comment=ZOO Project Demo
+Categories=Application;Education;Geography;
+Exec=firefox http://localhost/zoo-demo
+Icon=/usr/share/icons/zoo-icon.png
+Terminal=false
+StartupNotify=false
+EOF
+
+cp /usr/share/applications/zoo-project.desktop "$USER_HOME/Desktop/"
+chown "$USER_NAME:$USER_NAME" "$USER_HOME/Desktop/zoo-project.desktop"
 
 # Reload Apache
 /etc/init.d/apache2 force-reload
