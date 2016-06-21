50ba09c68a90372487355ed57994b69db80e5e3f
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-06-21 21:46:13 +0300

    ZOO Services split into many deb files (#1710)

--- bin/install_zoo-project.sh
+++ bin/install_zoo-project.sh
@@ -36,7 +36,7 @@ if [ ! -d "$TMP_DIR" ] ; then
 fi
 cd "$TMP_DIR"
 
-apt-get --assume-yes install zoo-kernel zoo-services zoo-api
+apt-get --assume-yes install zoo-kernel zoo-service-ogr zoo-api
 
 # Download ZOO Project deb file.
 wget -N --progress=dot:mega \
