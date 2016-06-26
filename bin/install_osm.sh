5629ebb8c989f5da86971a9ea9b06720ecbbc355
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-06-27 01:08:03 +0300

    Nik4 properly packaged now

--- bin/install_osm.sh
+++ bin/install_osm.sh
@@ -42,7 +42,7 @@ mkdir /usr/local/share/osm
 
 apt-get install --assume-yes josm gpsd gpsd-clients \
    merkaartor xmlstarlet imposm osmosis python-osmapi \
-   osmctools osmium-tool  
+   osmctools osmium-tool nik4
 
 
 ##-----------------------------------------------
@@ -69,14 +69,6 @@ mkdir -p "$USER_HOME"/.josm/plugins
 mv *jar "$USER_HOME"/.josm/plugins/
 
 chown $USER_NAME.$USER_NAME "$USER_HOME"/.josm -R
-
-##-----------------------------------------------
-## v9.5 add util nik4.py
-wget -c --tries=3 --progress=dot:mega \
-  "https://github.com/Zverik/Nik4/raw/3415338e53ed67318c7b4c9f9d8402156d2ee4da/nik4.py"
-chmod a+x nik4.py
-mv nik4.py /usr/local/bin/nik4.py
-ls -l /usr/local/bin/
 
 #### desktop icons
 echo "MimeType=application/x-openstreetmap+xml;" \
