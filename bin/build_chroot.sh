77ff1a55d02ba4856244bc06e11bb3a50ad712cb
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2017-02-19 20:50:54 +0200

    Upgrading to new LTS 16.04.2

--- bin/build_chroot.sh
+++ bin/build_chroot.sh
@@ -132,7 +132,7 @@ cd ~/livecdtmp
 #mv ubuntu-9.04-desktop-i386.iso ~/livecdtmp
 UBU_MIRROR="http://cdimage.ubuntu.com"
 UBU_RELEASE="16.04"
-ISO_RELEASE="16.04.1"
+ISO_RELEASE="16.04.2"
 UBU_ISO="lubuntu-${ISO_RELEASE}-desktop-$ARCH.iso"
 wget -c --progress=dot:mega \
    "$UBU_MIRROR/lubuntu/releases/$UBU_RELEASE/release/$UBU_ISO"
@@ -205,7 +205,7 @@ echo "======================================"
 
 #Method 2 hardcode default kernel from Lubuntu
 #need to repack the initrd.lz to pick up the change to casper.conf and kernel update
-sudo chroot edit mkinitramfs -c lzma -o /initrd.lz 4.4.0-31-generic
+sudo chroot edit mkinitramfs -c lzma -o /initrd.lz 4.8.0-36-generic
 
 #continue
 mkdir lzfiles
