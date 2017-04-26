64c203716500b999daf8f8eb50d35fed3416e80c
Pascal Like <antonio.cerciello@gmail.com>
2017-04-26 13:59:11 +0200

    GN - Upgrade to 3.2.1

--- app-conf/geonetwork/shutdown.sh
+++ app-conf/geonetwork/shutdown.sh
@@ -3,7 +3,6 @@ JAVA_HOME="/usr/lib/jvm/default-java"
 dir=`dirname $0`
 cd $dir
 
-cd ../jetty
-${JAVA_HOME}/bin/java -DSTOP.PORT=8879 -DSTOP.KEY=geonetwork -jar start.jar --stop
+-${JAVA_HOME}/bin/java -DSTOP.PORT=8879 -DSTOP.KEY=geonetwork -jar start.jar --stop
 
 zenity --info --text "GeoNetwork stopped"
