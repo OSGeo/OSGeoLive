22a6044a8b08fcd14e92ed31129baca1ba579569
Pascal Like <antonio.cerciello@gmail.com>
2017-05-02 17:56:00 +0200

    Update shutdown.sh

--- app-conf/geonetwork/shutdown.sh
+++ app-conf/geonetwork/shutdown.sh
@@ -1,8 +1,22 @@
-#!/bin/sh
-JAVA_HOME="/usr/lib/jvm/default-java"
-dir=`dirname $0`
-cd $dir
+#!/usr/bin/env bash
+export JAVA_HOME="/usr/lib/jvm/default-java"
+export JETTY_HOME=../jetty
+export JETTY_FOREGROUND=0
+export JETTY_BASE=$JETTY_HOME
+cd $JETTY_HOME
 
--${JAVA_HOME}/bin/java -DSTOP.PORT=8879 -DSTOP.KEY=geonetwork -jar start.jar --stop
+for i in "$@"
+do
+case $i in
+    -f*)
+    JETTY_FOREGROUND=1
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
+
+$JAVA_HOME/bin/java -DSTOP.PORT=8879 -DSTOP.KEY=geonetwork -jar start.jar --stop
 
 zenity --info --text "GeoNetwork stopped"
