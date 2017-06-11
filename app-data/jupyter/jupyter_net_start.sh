19b3391f7e976bf08907a4928733938b048b5d82
Brian M Hamlin <screenlight@mac.com>
2017-06-10 18:28:09 -0700

    comment out GRASS vars in jupyter net start

--- app-data/jupyter/jupyter_net_start.sh
+++ app-data/jupyter/jupyter_net_start.sh
@@ -8,25 +8,31 @@ if [ -z "$USER_NAME" ] ; then
 fi
 USER_HOME="/home/$USER_NAME"
 
-export LD_LIBRARY_PATH=/usr/lib/grass72/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=/usr/lib/grass72/etc/python:$PYTHONPATH
-export GISBASE=/usr/lib/grass72/
-export PATH=/usr/lib/grass72/bin/:$GISBASE/bin:$GISBASE/scripts:$PATH
 
-export GIS_LOCK=$$
+## 10jun17
+##  TODO: review for new GRASS setup on live11
+##--------------------------------------------------------------
+#export LD_LIBRARY_PATH=/usr/lib/grass72/lib:$LD_LIBRARY_PATH
+#export PYTHONPATH=/usr/lib/grass72/etc/python:$PYTHONPATH
+#export GISBASE=/usr/lib/grass72/
+#export PATH=/usr/lib/grass72/bin/:$GISBASE/bin:$GISBASE/scripts:$PATH
+
+#export GIS_LOCK=$$
 
 #mkdir -p /home/$USER/Envs/grass7data
-mkdir -p $USER_HOME/.grass7
-export GISRC=$USER_HOME/.grass7/rc
+#mkdir -p $USER_HOME/.grass7
+#export GISRC=$USER_HOME/.grass7/rc
 
-export GISDBASE=/home/user/grassdata/
+#export GISDBASE=/home/user/grassdata/
 
-export GRASS_TRANSPARENT=TRUE
-export GRASS_TRUECOLOR=TRUE
-export GRASS_PNG_COMPRESSION=9
-export GRASS_PNG_AUTO_WRITE=TRUE
+#export GRASS_TRANSPARENT=TRUE
+#export GRASS_TRUECOLOR=TRUE
+#export GRASS_PNG_COMPRESSION=9
+#export GRASS_PNG_AUTO_WRITE=TRUE
 
-export OSSIM_PREFS_FILE=/usr/share/ossim/ossim_preference
+#export OSSIM_PREFS_FILE=/usr/share/ossim/ossim_preference
+#------------------------------------------------------------
+
 
 jupyter notebook --port=8883 --no-browser \
    --notebook-dir="$USER_HOME/jupyter/notebooks" \
