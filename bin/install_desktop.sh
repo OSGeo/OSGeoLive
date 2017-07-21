2582ec7536ace01e4574c865e61534262b750753
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2017-07-21 09:18:20 +0300

    Switching back to darker desktop font

--- bin/install_desktop.sh
+++ bin/install_desktop.sh
@@ -56,8 +56,9 @@ sed -i -e 's|^bg=.*|bg=/usr/share/lubuntu/wallpapers/osgeo-desktop.png|' \
 sed -i -e 's|^wallpaper_mode=.*|wallpaper_mode=stretch|' \
        -e 's|^wallpaper=.*|wallpaper=/usr/share/lubuntu/wallpapers/osgeo-desktop.png|' \
        -e 's|^desktop_bg=.*|desktop_bg=#000000|' \
-       -e 's|^desktop_fg=.*|desktop_fg=#FFFFFF|' \
+       -e 's|^desktop_fg=.*|desktop_fg=#232323|' \
        -e 's|^show_trash=.*|show_trash=0|' \
+       -e 's|^desktop_shadow=.*|desktop_shadow=#A09A8F|' \
    /etc/xdg/pcmanfm/lubuntu/pcmanfm.conf
 
 ## Desktop shadow configuration ^^
