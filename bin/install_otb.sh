6c841e248a80aaa0172482abf704d54e49db461d
Luca Delucchi <lucadeluge@gmail.com>
2018-03-22 15:43:50 +0100

    fixed otb header

--- bin/install_otb.sh
+++ bin/install_otb.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#############################################################################
+#
+# Purpose: This script will install Orfeo Tooblox including Monteverdi2 and 
+# OTB apps, assumes script is run with sudo privileges.
+#
+#############################################################################
 # Copyright (c) 2009-2016 The Open Source Geospatial Foundation.
 # Licensed under the GNU LGPL version >= 2.1.
 #
@@ -12,11 +18,8 @@
 # in the "LICENSE.LGPL.txt" file distributed with this software or at
 # web page "http://www.fsf.org/licenses/lgpl.html".
 #
-# About:
-# =====
-# This script will install Orfeo Tooblox including Monteverdi2 and OTB apps,
-#  assumes script is run with sudo privileges.
-#
+#############################################################################
+
 # Running:
 # =======
 # monteverdi2
