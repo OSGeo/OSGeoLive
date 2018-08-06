2dec1ae327027d011c6745424246311b7eca259f
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2018-08-06 16:00:24 +0300

    Reverting headers on GeoNetwork conf files, since no feedback was provided

--- app-conf/geonetwork/startup.sh
+++ app-conf/geonetwork/startup.sh
@@ -1,26 +1,4 @@
 #!/usr/bin/env bash
-
-#############################################################################
-#
-# Purpose: This script is used to startup geonetwork
-#
-#############################################################################
-
-# Copyright (c) 2009-2018 Open Source Geospatial Foundation (OSGeo)
-#
-# Licensed under the GNU LGPL.
-#
-# This library is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 2.1 of the License,
-# or any later version.  This library is distributed in the hope that
-# it will be useful, but WITHOUT ANY WARRANTY, without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Lesser General Public License for more details, either
-# in the "LICENSE.LGPL.txt" file distributed with this software or at
-# web page "http://www.fsf.org/licenses/lgpl.html".
-#############################################################################
-
 export JAVA_HOME="/usr/lib/jvm/default-java"
 export JETTY_HOME=../jetty
 export JETTY_FOREGROUND=0
