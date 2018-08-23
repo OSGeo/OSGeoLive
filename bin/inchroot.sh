b5b238398df1d3d0abc3a882dc8ec41b09c4b7e3
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2018-08-23 18:11:44 +0300

    ZOO packages are now available for Bionic (#2063)

--- bin/inchroot.sh
+++ bin/inchroot.sh
@@ -150,7 +150,7 @@ export USER_NAME
 ./install_grass.sh
 ./install_qgis.sh
 ./install_qgis_server.sh
-# ./install_zoo-project.sh "$ARCH"
+./install_zoo-project.sh "$ARCH"
 ./install_marble.sh
 ./install_opencpn.sh
 ./install_zygrib.sh
