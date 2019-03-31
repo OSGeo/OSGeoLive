b11515215ea9bbeb1d1a36f196e9d393827c6057
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2019-03-31 21:54:29 +0300

    VirtualBox guest additions for the Hardware Enablement included in 18.04.2

--- bin/setup.sh
+++ bin/setup.sh
@@ -92,7 +92,8 @@ apt-get install --yes wget less zip unzip bzip2 p7zip \
 # Install virtualbox guest additions
 # If running on virtualbox this will allow us to use full-screen/usb2/...
 # If running outside virtualbox the drivers will not be loaded
-apt-get install --yes virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+# apt-get install --yes virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+apt-get install --yes virtualbox-guest-dkms-hwe virtualbox-guest-utils-hwe virtualbox-guest-x11-hwe
 
 ##-------
 # add /usr/local/lib to /etc/ld.so.conf if needed, then run ldconfig
