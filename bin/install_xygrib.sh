6a38f150b1b9b38098c1513718bd7a8d85801774
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2020-09-16 22:17:05 +0300

    Large gshhs files got removed from package

--- bin/install_xygrib.sh
+++ bin/install_xygrib.sh
@@ -28,9 +28,9 @@ USER_HOME="/home/$USER_NAME"
 apt-get --assume-yes install xygrib
 
 # zygrib-maps package is 170mb (contains NOAA's GSHHS coastline)
-rm /usr/share/maps/gshhs/gshhs_0.rim
-rm /usr/share/maps/gshhs/gshhs_1.rim
-rm /usr/share/maps/gshhs/wdb_rivers_f.b
+# rm /usr/share/maps/gshhs/gshhs_0.rim
+# rm /usr/share/maps/gshhs/gshhs_1.rim
+# rm /usr/share/maps/gshhs/wdb_rivers_f.b
 
 # copy icon to Desktop
 cp /usr/share/applications/xygrib.desktop \
