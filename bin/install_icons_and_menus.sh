2171509b9b7415cf0a88eae0d699a2be10e57cae
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2020-09-20 16:47:40 +0300

    Add pygeoapi to web services menu

--- bin/install_icons_and_menus.sh
+++ bin/install_icons_and_menus.sh
@@ -48,7 +48,7 @@ NAV_APPS="marble opencpn josm osm_online xygrib gpsprune"
 #Server apps part 1 (web-enabled GIS; interactive/WPS)
 WEB_SERVICES="deegree-* geoserver-* *geonetwork* mapserver mapcache mapproxy-*
               qgis-mapserver zoo-project 52n* eoxserver* ncWMS-* pycsw istsos
-              pywps t-rex actinia*"
+              pywps t-rex actinia* pygeoapi"
 #disabled: mapguide*
 
 #Server apps part 2 (web based viewers; data only flows down to user)
