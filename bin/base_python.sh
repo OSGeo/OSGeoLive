c42581f8b0806df24fcca8e9ce81489984a87758
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2020-10-03 21:37:40 +0300

    Adding fiona and rasterio command line interfaces

--- bin/base_python.sh
+++ bin/base_python.sh
@@ -37,12 +37,12 @@ apt-get install --yes python3-django
 # apt-mark hold python3-django
 
 # Install Geospatial Python libraries
-apt-get install --yes python3-gdal python3-shapely python3-rasterio python3-rtree \
-    python3-fiona python3-matplotlib python3-tk python3-geopandas \
+apt-get install --yes python3-gdal python3-shapely python3-rasterio rasterio \
+    python3-fiona fiona python3-matplotlib python3-tk python3-geopandas \
     python3-netcdf4 python3-geojson python3-scipy python3-pandas \
     python3-pyshp python3-descartes python3-geographiclib \
     python3-cartopy python3-seaborn python3-networkx python3-branca python3-pysal \
-    python3-mappyfile
+    python3-mappyfile python3-rtree 
 
 ##======================================================
 # clean cartopy  12beta  rm 6MB, add 3MB
