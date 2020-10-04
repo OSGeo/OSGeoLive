fa1c7d8323f6191d0a9955912bdf2375f974901a
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2020-10-04 13:32:49 +0300

    pygeoapi configuration added

--- bin/install_pygeoapi.sh
+++ bin/install_pygeoapi.sh
@@ -29,34 +29,67 @@ if [ -z "$USER_NAME" ] ; then
    USER_NAME="user"
 fi
 USER_HOME="/home/$USER_NAME"
+BIN="/usr/local/bin"
+PYGEOAPI_DIR="/usr/local/share/pygeoapi"
 
+mkdir -p "$PYGEOAPI_DIR"
 
 echo 'Installing pygeoapi ...'
 
 apt-get install --yes python3-pygeoapi
 
-echo 'Downloading pycsw logo ...'
+# copy pygeoapi configuration
+cp "$BUILD_DIR/../app-conf/pygeoapi/pygeoapi-config.yml" "$PYGEOAPI_DIR/pygeoapi-config.yml"
+cp "$BUILD_DIR/../app-conf/pygeoapi/ne_110m_lakes.geojson" "$PYGEOAPI_DIR/ne_110m_lakes.geojson"
+
+echo 'Downloading pygeoapi logo ...'
 wget -c --progress=dot:mega \
    -O /usr/local/share/icons/pygeoapi.png \
       "https://github.com/geopython/pygeoapi.io/raw/master/docs/img/pygeoapi-icon.png"
 
+echo 'Creating Scripts/Links ...'
+mkdir -p "$BIN"
+cat << EOF > "$BIN/pygeoapi_start.sh"
+#!/bin/sh
+export PYGEOAPI_CONFIG=/usr/local/share/pygeoapi/pygeoapi-config.yml
+export PYGEOAPI_OPENAPI=/usr/local/share/pygeoapi/pygeoapi-openapi.yml
+pygeoapi generate-openapi-document -c $PYGEOAPI_CONFIG > $PYGEOAPI_OPENAPI
+pygeoapi serve
+EOF
+
 echo 'Installing desktop launcher ...'
 
-cat << EOF > /usr/share/applications/pygeoapi.desktop
+cat << EOF > /usr/share/applications/pygeoapi-start.desktop
 [Desktop Entry]
 Type=Application
 Encoding=UTF-8
-Name=pygeoapi
+Name=Start pygeoapi
 Comment=pygeoapi OGC API server
-Exec=xdg-open http://localhost/pycsw/tests/index.html
+Exec=qterminal -e pygeoapi_start.sh
 Icon=pygeoapi
 Terminal=false
 StartupNotify=false
 Categories=Application;Education;Geography
 EOF
 
-cp /usr/share/applications/pygeoapi.desktop "$USER_HOME/Desktop/"
-chown "$USER_NAME:$USER_NAME" "$USER_HOME/Desktop/pygeoapi.desktop"
+cp /usr/share/applications/pygeoapi-start.desktop "$USER_HOME/Desktop/"
+chown "$USER_NAME:$USER_NAME" "$USER_HOME/Desktop/pygeoapi-start.desktop"
+
+cat << EOF > /usr/share/applications/pygeoapi-intro.desktop
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=pygeoapi Introduction
+Comment=pygeoapi OGC API server
+Exec=xdg-open http://localhost:5000/ http://localhost/osgeolive/en/quickstart/pygeoapi_quickstart.html
+Icon=pygeoapi
+Terminal=false
+StartupNotify=false
+Categories=Application;Education;Geography
+EOF
+
+cp /usr/share/applications/pygeoapi-intro.desktop "$USER_HOME/Desktop/"
+chown "$USER_NAME:$USER_NAME" "$USER_HOME/Desktop/pygeoapi-intro.desktop"
 
 ####
 ./diskspace_probe.sh "`basename $0`" end
