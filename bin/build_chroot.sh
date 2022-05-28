9f9645d12665e546b5733579d0e7d379a3a637ba
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2022-05-28 10:55:13 +0300

    Fixing mkisofs to work without ISOLINUX (#2348)

--- bin/build_chroot.sh
+++ bin/build_chroot.sh
@@ -344,7 +344,7 @@ echo "======================================"
 cd extract-cd
 sudo rm md5sum.txt
 find -type f -print0 | sudo xargs -0 md5sum | \
-   grep -v isolinux/boot.cat | sudo tee md5sum.txt
+   grep -v boot.catalog | sudo tee md5sum.txt
 
 echo
 echo "Creating iso..."
@@ -353,16 +353,16 @@ echo "======================================"
 #isohybrid used only in 64bit architecture
 if [ "$ARCH" = "amd64" ] ; then
    sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -quiet -b \
-      isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
+      boot/grub/i386-pc/eltorito.img -c boot.catalog -no-emul-boot \
       -boot-load-size 4 -boot-info-table \
-      -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
+      -eltorito-alt-boot -e EFI/boot/grubx64.efi -no-emul-boot \
       -o ../"$ISO_NAME.iso" .
-   sudo isohybrid -u ../"$ISO_NAME.iso"
+   # sudo isohybrid -u ../"$ISO_NAME.iso"
 else
    sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -quiet -b \
-      isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
+      boot/grub/i386-pc/eltorito.img -c boot.catalog -no-emul-boot \
       -boot-load-size 4 -boot-info-table -o ../"$ISO_NAME.iso" .
-   sudo isohybrid ../"$ISO_NAME.iso"
+   # sudo isohybrid ../"$ISO_NAME.iso"
 fi
 
 echo
