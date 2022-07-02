25f1439ffcaf03c0b553569d9bfa942eb7311ba6
Brian M Hamlin <maplabs@light42.com>
2022-07-02 17:42:09 +0000

    fix deegree_start for utah-lite workspace

--- app-conf/deegree/deegree_start.sh
+++ app-conf/deegree/deegree_start.sh
@@ -23,7 +23,7 @@
 
 DEEGREE_WORKSPACE_ROOT="/usr/local/share/deegree"
 export DEEGREE_WORKSPACE_ROOT
-DEE_DIR="/usr/local/lib/deegree-webservices-3.4.31"
+DEE_DIR="/usr/local/share/deegree/deegree-workspace-utah-light/"
 
 # live disc's username is "user"
 if [ -z "$USER_NAME" ] ; then
