f342e79d1daec9609cc10049deccbbcf4eab9dfb
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2022-08-05 18:38:43 +0300

    Revert "Get the latest zoo examples from git"
    This reverts commit 2ec36d3eb3261031d54be42bde19a16461d5d3f7.

--- bin/install_zoo-project.sh
+++ bin/install_zoo-project.sh
@@ -62,13 +62,11 @@ apt-get --assume-yes install zoo-kernel zoo-service-ogr \
 
 # Download and setup ZOO Project demo files.
 cd "$TMP_DIR"
-git clone --depth=1 https://github.com/ZOO-Project/examples.git zoo-demo
-# wget -N --progress=dot:mega \
-   # -O "$TMP_DIR"/examples-livedvd.tar.bz2 \
-   # "http://download.osgeo.org/livedvd/data/zoo/examples-livedvd.tar.bz2"
+wget -N --progress=dot:mega \
+   -O "$TMP_DIR"/examples-livedvd.tar.bz2 \
+   "http://download.osgeo.org/livedvd/data/zoo/examples-livedvd.tar.bz2"
 
-# tar xf examples-livedvd.tar.bz2
-rm -rf zoo-demo/.git
+tar xf examples-livedvd.tar.bz2
 cp -r zoo-demo /var/www/html/zoo-demo
 chmod -R 755 /var/www/html/zoo-demo
 # cp zoo-demo/main.cfg /etc/zoo-project/
