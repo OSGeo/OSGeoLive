247e55e441af8c10427e4aa9672fdaeb568fabe5
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2022-08-06 18:55:31 +0300

    Update version to 15.0rc1

--- bin/install_desktop.sh
+++ bin/install_desktop.sh
@@ -72,7 +72,7 @@ sed -i -e 's|^background=.*|background=/usr/share/lubuntu/wallpapers/osgeo-deskt
 
 # Set the installer desktop icon to OSGeoLive
 sed -i -e 's|Lubuntu|OSGeoLive|' \
-       -e 's|22.04 LTS|15.0beta2|' \
+       -e 's|22.04 LTS|15.0rc1|' \
    /usr/share/applications/lubuntu-calamares.desktop
 
 #Add the launchhelp script which allows other apps to provide sudo
