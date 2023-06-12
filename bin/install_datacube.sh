3bfa67e35c434f5321361c1b5901cd09ba1a1755
Brian M Hamlin <maplabs@light42.com>
2023-06-12 15:37:20 -0700

    rewrite paths for data, add a demo layer

--- bin/install_datacube.sh
+++ bin/install_datacube.sh
@@ -28,11 +28,11 @@ USER_HOME="/home/$USER_NAME"
 
 TMP="/tmp/build_datacube"
 BIN="/usr/local/bin"
-DATA_FOLDER="/usr/local/share/data"
-ODC_DATA_FOLDER="$DATA_FOLDER/odc"
+DDIR="/usr/local/share/data/odc"
 
 apt-get install --yes python3-datacube python3-odc-geo python3-odc-stac
 
+mkdir -p ${DDIR} --verbose
 mkdir -p "$TMP"
 cd "$TMP"
 
@@ -40,7 +40,7 @@ sudo -u $USER_NAME createdb datacube
 sudo -u $USER_NAME psql datacube -c 'create extension postgis'
 sudo -u $USER_NAME psql datacube -c 'create extension hstore'
 
-# reset
+# -- reset install -----
 # rm -rf .config/datacube
 # psql -c 'drop database datacube'
 
@@ -50,8 +50,8 @@ DCONF=/home/${USER_NAME}/.config/datacube
 
 mkdir -p ${DCONF}
 chown -R ${USER_NAME} ${DCONF}
-mkdir -p ${ODC_DATA_FOLDER}
 
+echo "#--" >> ${USER_HOME}/.bashrc
 echo "export DCONF=${USER_HOME}/.config/datacube" >> ${USER_HOME}/.bashrc
 echo "export DATACUBE_CONFIG_PATH=${DCONF}/datacube.conf" >> ${USER_HOME}/.bashrc
 
@@ -60,7 +60,8 @@ cat << EOF > ${DCONF}/datacube.conf
 [datacube]
 db_database: datacube
 
-# A blank host will use a local socket. Specify a hostname (such as localhost) to use TCP.
+# A blank PG host will use a local socket.
+#  Specify a hostname (such as localhost) to use TCP.
 db_hostname:
 
 # Credentials are optional: you might have other Postgres authentication configured.
@@ -101,23 +102,41 @@ EOF
 sudo -u $USER_NAME  datacube -v -C ${DCONF}/datacube.conf  \
     system init
 
+cp -f ${BUILD_DIR}/../app-data/odc/*.yaml ${DCONF}/
+
+
+##----------------------------------------------------------
+##  demo: product description; ingest; data file(s)
+
+wget -c  -O ${DDIR}/esa_10m_2021_prizren.tif \
+     https://download.osgeo.org/livedvd/data/odc/esa_10m_2021_prizren.tif
+
+wget -c  -O ${DDIR}/srtm_prizren.tif \
+     http://download.osgeo.org/livedvd/data/odc/srtm_prizren.tif
+
+wget -c  -O ${DCONF}/tsrtm2_dem.odc-product.yaml \
+     http://download.osgeo.org/livedvd/data/odc/tsrtm2_dem.odc-product.yaml
+
+wget -c  -O ${DCONF}/tsrtm2-sample0.yaml \
+     http://download.osgeo.org/livedvd/data/odc/tsrtm2-sample0.yaml
+
 ##----------------------------------------------------------
 ##  add PRODUCT schema definitions to this local datacube
 sudo -u $USER_NAME datacube -C ${DCONF}/datacube.conf \
     product add ${DCONF}/landsat-clip.yaml
 
-cp -f ${BUILD_DIR}/../app-data/odc/*.yaml ${DCONF}/
-
 sudo -u $USER_NAME datacube -C ${DCONF}/datacube.conf \
     product add ${DCONF}/esa_worldcover_2021.odc-product.yaml
 
-##----------------------------------------------------------
-##  add demo dataset from a geoTIFF
-wget -c  -O ${ODC_DATA_FOLDER}/esa_10m_2021_prizren.tif \
-     https://download.osgeo.org/livedvd/data/odc/esa_10m_2021_prizren.tif
+sudo -u $USER_NAME datacube -C ${DCONF}/datacube.conf \
+    product add ${DCONF}/tsrtm2_dem.odc-product.yaml
 
+#--
 sudo -u $USER_NAME datacube -C ${DCONF}/datacube.conf \
     dataset add ${DCONF}/esa-sample0.yaml
+
+sudo -u $USER_NAME datacube -C ${DCONF}/datacube.conf \
+    dataset add ${DCONF}/tsrtm2-sample0.yaml
 
 ####
 "$BUILD_DIR"/diskspace_probe.sh "`basename $0`" end
