7937a4e8d770f5fad2db48d294dde2c73be213de
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2023-08-08 10:31:33 +0300

    Update version to 16.0

--- bin/install_desktop.sh
+++ bin/install_desktop.sh
@@ -74,7 +74,7 @@ sed -i -e 's|^background=.*|background=/usr/share/lubuntu/wallpapers/osgeo-deskt
 
 # Set the installer desktop icon to OSGeoLive
 sed -i -e 's|Lubuntu|OSGeoLive|' \
-       -e 's|22.04 LTS|16.0rc1|' \
+       -e 's|22.04 LTS|16.0|' \
    /usr/share/applications/lubuntu-calamares.desktop
 
 #Add the launchhelp script which allows other apps to provide sudo
