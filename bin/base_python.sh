5228480f58b9994514d36eada0d614d076275e62
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2024-08-16 15:35:36 +0300

    Review base Python installer

--- bin/base_python.sh
+++ bin/base_python.sh
@@ -5,7 +5,7 @@
 # OSGeoLive.
 #
 #############################################################################
-# Copyright (c) 2016-2023 Open Source Geospatial Foundation (OSGeo) and others.
+# Copyright (c) 2016-2024 Open Source Geospatial Foundation (OSGeo) and others.
 #
 # Licensed under the GNU LGPL.
 #
@@ -26,19 +26,19 @@ BUILD_DIR=`pwd`
 apt-get -q update
 
 # # Install Django
-apt-get install --yes python3-django
+# apt-get install --yes python3-django
 
 # # Hold Django version to avoid upgrades from upstream
 # apt-mark hold python3-django
 
 # Install Geospatial Python libraries
 apt-get install --yes python3-gdal python3-shapely python3-rasterio rasterio \
-    python3-fiona fiona python3-matplotlib python3-tk python3-geopandas \
+    python3-fiona fiona python3-matplotlib python3-geopandas \
     python3-netcdf4 python3-geojson python3-scipy python3-pandas \
-    python3-pyshp python3-descartes python3-geographiclib \
-    python3-cartopy python3-seaborn python3-networkx python3-branca \
-    python3-rtree python3-folium python3-lark python3-mappyfile \
-    python3-pysal python3-geoalchemy2 python3-datacube
+    python3-pyshp python3-geographiclib python3-geoalchemy2 \
+    python3-cartopy python3-folium
+
+# apt-get install --yes python3-mappyfile
 
 # Add a symlink for rio
 ln -s /usr/bin/rasterio /usr/local/bin/rio
