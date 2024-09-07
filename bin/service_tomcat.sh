9a9c3517a9a757f7c7fabbf7701d15d93ce1a792
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2024-09-07 12:39:20 +0300

    Update tomcat installer to version 10

--- bin/service_tomcat.sh
+++ bin/service_tomcat.sh
@@ -4,7 +4,7 @@
 # Purpose: This script will install tomcat 9
 #
 #############################################################################
-# Copyright (c) 2009-2022 Open Source Geospatial Foundation (OSGeo) and others.
+# Copyright (c) 2009-2024 Open Source Geospatial Foundation (OSGeo) and others.
 #
 # Licensed under the GNU LGPL version >= 2.1.
 #
@@ -26,7 +26,7 @@
 #
 # To manually launch:
 # ===================
-# sudo service tomcat9 start
+# sudo service tomcat10 start
 #############################################################################
 
 ./diskspace_probe.sh "`basename $0`" begin
@@ -40,7 +40,7 @@ fi
 groupadd -g 998 tomcat
 # useradd tomcat -u 998 -g 998 --gecos "Apache Tomcat" -m -d /var/lib/tomcat -s /usr/sbin/nologin
 
-apt-get install --yes tomcat9 tomcat9-admin
+apt-get install --yes tomcat10 tomcat10-admin
 
 #Add the following lines to <tomcat-users> in /etc/tomcat7/tomcat-users.xml
 #<role rolename="manager"/>
@@ -48,24 +48,24 @@ apt-get install --yes tomcat9 tomcat9-admin
 
 
 cp ../app-conf/tomcat/tomcat-users.xml \
-   /etc/tomcat9/tomcat-users.xml
+   /etc/tomcat10/tomcat-users.xml
 
-chown tomcat:tomcat /etc/tomcat9/tomcat-users.xml
+chown tomcat:tomcat /etc/tomcat10/tomcat-users.xml
 
 # something screwed up with the ISO permissions:
-chgrp tomcat /usr/share/tomcat9/bin/*.sh
+chgrp tomcat /usr/share/tomcat10/bin/*.sh
 adduser "$USER_NAME" tomcat
 
 # systemctl or service are not available anymore in chroot
 # service tomcat9 stop
 
 # Assign 1GB of RAM to default tomcat. Use -XX:+UseG1GC with Java 8.
-sed -i -e 's|-Djava.awt.headless=true|-Djava.awt.headless=true -XX:+UseG1GC -Xmx1024m|' /etc/default/tomcat9
-sed -i -e 's|-Djava.awt.headless=true|-Djava.awt.headless=true -XX:+UseG1GC -Xmx1024m|' /lib/systemd/system/tomcat9.service
+# sed -i -e 's|-Djava.awt.headless=true|-Djava.awt.headless=true -XX:+UseG1GC -Xmx1024m|' /etc/default/tomcat9
+# sed -i -e 's|-Djava.awt.headless=true|-Djava.awt.headless=true -XX:+UseG1GC -Xmx1024m|' /lib/systemd/system/tomcat9.service
 
 # Workaround for https://bugs.launchpad.net/ubuntu/+source/tomcat9/+bug/1972829
-apt-get install --yes libecj-java
-ln -s /usr/share/java/ecj.jar /var/lib/tomcat9/lib
+# apt-get install --yes libecj-java
+# ln -s /usr/share/java/ecj.jar /var/lib/tomcat9/lib
 
 ####
 ./diskspace_probe.sh "`basename $0`" end
