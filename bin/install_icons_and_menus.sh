0b87ac805c36e89aa16f544172272acf2d6e64be
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2024-09-07 15:03:48 +0300

    Review icons and menus

--- bin/install_icons_and_menus.sh
+++ bin/install_icons_and_menus.sh
@@ -9,7 +9,7 @@
 # Author: Hamish Bowman
 #
 #############################################################################
-# Copyright (c) 2013-2022 Open Source Geospatial Foundation (OSGeo) and others.
+# Copyright (c) 2013-2024 Open Source Geospatial Foundation (OSGeo) and others.
 #
 # Licensed under the GNU LGPL version >= 2.1.
 # 
@@ -518,7 +518,7 @@ cp /usr/share/icons/gnome/32x32/categories/gnome-globe.png /usr/share/icons/
 #  `grep -l 'Geography;' /usr/share/applications/*.desktop` \
 #  /usr/local/share/applications/*.desktop
 #if all are dupes, just nuke it:
-sed -i '57,67d' /etc/xdg/menus/lxqt-applications.menu
+# sed -i '57,67d' /etc/xdg/menus/lxqt-applications.menu
 
 bash /usr/local/share/osgeo-desktop/desktop-truster.sh
 
