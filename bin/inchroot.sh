9a2b3ee4a32f998af8c85eef0351bebe1dc536ca
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2024-09-22 16:33:10 +0300

    Enable data loading and some more installers

--- bin/inchroot.sh
+++ bin/inchroot.sh
@@ -175,7 +175,7 @@ export USER_NAME
 
 ## Java stack
 ./install_geoserver.sh
-# ./install_geonetwork.sh
+# ./install_geonetwork.sh # install_vm_only
 ./install_deegree.sh
 # ./install_udig.sh "$ARCH" # install_vm_only
 ./install_openjump.sh
@@ -199,14 +199,14 @@ export USER_NAME
 ./install_openlayers.sh
 ./install_leaflet.sh
 ./install_cesium.sh
-# ./install_geoext.sh
+./install_geoext.sh
 # ./install_rasdaman.sh # install_vm_only
-# ./install_geostyler.sh
+./install_geostyler.sh
 # ./install_re3gistry.sh # install_vm_only
 # ./install_etf.sh # install_vm_only
 
 ## Docs, Data and extras
-# ./load_gisdata.sh
+./load_gisdata.sh
 ./install_docs.sh "$BUILD_MODE"
 # ./install_edutools.sh
 
