52afbfd2dfc1593297aa7de90efdaae3b638436d
Sandro Santilli <strk@kbt.io>
2025-11-01 08:20:05 +0100

    Install required packages, don't run sudo inside the script

--- bin/tools/setup_package_build_env.sh
+++ bin/tools/setup_package_build_env.sh
@@ -5,11 +5,17 @@ BASEDIST=noble
 # TODO: find closest mirror from https://launchpad.net/ubuntu/+archivemirrors
 MIRROR=${MIRROR-http://nl.archive.ubuntu.com/ubuntu/}
 
-sudo cowbuilder --create \
-                --distribution=${BASEDIST} \
-                --basepath=/var/cache/pbuilder/base-${BASEDIST}-osgeolive.cow \
-                --hookdir=/var/cache/pbuilder/hook.d/ \
-                --mirror=${MIRROR} \
-                --othermirror="deb [ trusted=yes ] http://ppa.launchpad.net/osgeolive/nightly/ubuntu ${BASEDIST} main" \
-                --components="main universe"
+# Make sure the required tools are installed
+apt install \
+  ubuntu-keyring \
+  cowbuilder
+
+# Run cowbuilder
+cowbuilder --create \
+           --distribution=${BASEDIST} \
+           --basepath=/var/cache/pbuilder/base-${BASEDIST}-osgeolive.cow \
+           --hookdir=/var/cache/pbuilder/hook.d/ \
+           --mirror=${MIRROR} \
+           --othermirror="deb [ trusted=yes ] http://ppa.launchpad.net/osgeolive/nightly/ubuntu ${BASEDIST} main" \
+           --components="main universe"
 
