63703c7e5d59dfe9fa5becbfaded19859ad37c3e
Sandro Santilli <strk@kbt.io>
2025-11-04 11:51:22 +0100

    Ensure the script is run from the top dir of a repo with Debian Salsa remote
    Thanks Bas Couwenberg for suggestion

--- bin/tools/package_build.sh
+++ bin/tools/package_build.sh
@@ -3,6 +3,23 @@
 # TODO: allow overriding via env variable
 BASEDIST=noble
 
-# TODO: check you're in a git repository of a debian packaging project (how?)
+# Check you're in a git repository of a debian packaging project (how?)
+which git > /dev/null || {
+  echo "👎 this script needs 'git' to be installed" >&2
+  exit 1
+}
+
+GIT_TOPLEVEL=$(git rev-parse --show-toplevel) || exit 1
+test ${PWD} = "${GIT_TOPLEVEL}" || {
+  echo "👎 This script needs to be run in the root path of a git repository" >&2
+  exit 1
+}
+
+if [ "$(git remote -v | grep -c salsa.debian.org)" != 0 ]; then
+  echo "👍 We are in the top-level dir of a git repository having Debian Salsa remote"
+else
+  echo "👎 The git repository needs to have a 'salsa.debian.org' remote" >&2
+  exit 1
+fi
 
 gbp buildpackage --git-pbuilder --git-dist=${BASEDIST}-osgeolive
