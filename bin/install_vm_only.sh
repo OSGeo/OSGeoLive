8bab819e9a74ba315866792ec13cec07fd3a2a0a
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2026-01-06 16:34:58 +0200

    Update vm only packages setup

--- bin/install_vm_only.sh
+++ bin/install_vm_only.sh
@@ -63,45 +63,30 @@ USER_HOME="/home/$USER_NAME"
 # rm ./bootstrap.sh
 
 apt-get -q update
-apt-mark unhold linux-image-generic
 apt-get --yes upgrade
-
-# Install linux headers
-apt-get install --yes build-essential linux-headers-generic
 
 # Adding VBox guest additions
 # apt-get install --yes virtualbox-guest-x11
 
 # Adding development packages that were removed from iso to save disk space
 apt-get --yes install build-essential git gnupg devscripts debhelper \
-  gcc-11 g++-11 gfortran-11 pbuilder pristine-tar git-buildpackage devscripts \
-  grass-dev libgdal-dev libproj-dev libgeos-dev python3-dev python3-pip \
-  cmake libotb-dev npm nodejs python3-dask python3-sklearn python3-cfgrib
+  pbuilder pristine-tar git-buildpackage devscripts cmake python3-pip python3-virtualenv
 
-# Adding Python2
-# apt-get install --yes python-all-dev
+# apt-get --yes grass-dev libgdal-dev libproj-dev libgeos-dev python3-dev \
+#   libotb-dev npm nodejs python3-sklearn python3-cfgrib
 
 # Adding back LibreOffice and other packages that were removed from iso to save disk space
-apt-get --yes install libreoffice libreoffice-common libreoffice-core 2048-qt noblenote trojita \
-  transmission-common k3b vlc libllvm14 fonts-dejavu fonts-dejavu-extra xfonts-scalable \
-  xfonts-100dpi xfonts-75dpi xfonts-jmk fonts-lyx unifont fonts-noto-core fonts-noto \
-  fonts-noto-cjk fonts-noto-mono
+apt-get --yes install libreoffice libreoffice-common libreoffice-core 2048-qt noblenote \
+  transmission-common vlc libllvm18 fonts-noto-cjk
+  # fonts-dejavu fonts-dejavu-extra xfonts-scalable fonts-noto-mono \
+  # xfonts-100dpi xfonts-75dpi xfonts-jmk fonts-lyx unifont fonts-noto-core fonts-noto
 
 # Remove patches needed for the live session only
 rm "$USER_HOME"/.config/autostart/apache-fixer.desktop
 rm "$USER_HOME"/.config/autostart/desktop-truster.desktop
 
-# Install R Studio
-# cd ~
-# apt-get --yes install libclang-dev
-# wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2022.07.0-548-amd64.deb
-# dpkg -i rstudio-server-2022.07.0-548-amd64.deb
-# rm rstudio-server-2022.07.0-548-amd64.deb
-# systemctl disable rstudio-server
-# TODO: Install Atom or VS Code
-
 # Install docker engine
-apt-get --yes install docker.io docker-compose
+apt-get --yes install docker.io docker-compose-v2
 usermod -aG docker user
 # TODO: Install extra documentation
 
